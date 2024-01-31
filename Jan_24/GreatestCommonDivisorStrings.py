10b448c7ed19ef0332eadb4d932449f13d9604cc
Nimish <nimish.v.shinde60@gmail.com>
2024-01-31 19:00:11 +0530

    LeetCode 75 days challenge - Day 2 Optimal GCD Strings

--- Jan_24/GreatestCommonDivisorStrings.py
+++ Jan_24/GreatestCommonDivisorStrings.py
@@ -1,11 +1,12 @@
+import math
 class Solution:
-    def gcd(self, a, b):
-        if a == b:
-            return a
-        if a > b:
-            return self.gcd(a - b, b)
-        else:
-            return self.gcd(a, b - a)
+    # def gcd(self, a, b):
+    #     if a == b:
+    #         return a
+    #     if a > b:
+    #         return self.gcd(a - b, b)
+    #     else:
+    #         return self.gcd(a, b - a)
 
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         # if str1 == str2:
@@ -30,7 +31,7 @@ class Solution:
         #         Optimal Solution
         if str1 + str2 != str2 + str1:
             return ""
-        gcdNum = self.gcd(len(str1), len(str2))
+        gcdNum = math.gcd(len(str1), len(str2))
         return str1[:gcdNum]
 
 
