dc11342dcead6d4ee162d24f6525585c36f8a792
Nimish <nimish.v.shinde60@gmail.com>
2024-01-31 18:59:07 +0530

    LeetCode 75 days challenge - Day 2 Optimal GCD Strings

--- Jan_24/GreatestCommonDivisorStrings.py
+++ Jan_24/GreatestCommonDivisorStrings.py
@@ -1,25 +1,40 @@
 class Solution:
-    def gcdOfStrings(self, str1: str, str2: str) -> str:
-
-        if str1 == str2:
-            return str1
-
-        if len(str1) > len(str2):
-            big = str1
-            small = str2
+    def gcd(self, a, b):
+        if a == b:
+            return a
+        if a > b:
+            return self.gcd(a - b, b)
         else:
-            big = str2
-            small = str1
+            return self.gcd(a, b - a)
 
-        multiples = len(small)
-        while multiples > 0:
-            if len(big) % multiples == 0 and len(small) % multiples ==0:
-                curr = small[:multiples]
-                if curr * (len(big)//multiples) == big and curr * (len(small)//multiples) == small:
-                    return curr
-            multiples -= 1
-        return ""
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # if str1 == str2:
+        #     return str1
+        #
+        # if len(str1) > len(str2):
+        #     big = str1
+        #     small = str2
+        # else:
+        #     big = str2
+        #     small = str1
+        #
+        # multiples = len(small)
+        # while multiples > 0:
+        #     if len(big) % multiples == 0 and len(small) % multiples ==0:
+        #         curr = small[:multiples]
+        #         if curr * (len(big)//multiples) == big and curr * (len(small)//multiples) == small:
+        #             return curr
+        #     multiples -= 1
+        # return ""
+
+        #         Optimal Solution
+        if str1 + str2 != str2 + str1:
+            return ""
+        gcdNum = self.gcd(len(str1), len(str2))
+        return str1[:gcdNum]
+
+
 
 
 check = Solution()
-print(check.gcdOfStrings('AAAA', 'AAA'))
+print(check.gcdOfStrings('ABCABCABC', 'ABCABC'))
