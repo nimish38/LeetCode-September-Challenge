ecc6b486e1d79eaf09457c967825fc8efbba28fe
Nimish <nimish.v.shinde60@gmail.com>
2024-02-04 21:49:34 +0530

    LeetCode 75 days challenge - Day 4 Alternate numbers - brute force

--- Jan_24/FLowerBeds.py
+++ Jan_24/FLowerBeds.py
@@ -1,15 +1,23 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed, n):
-        i = 1
-        while i < len(flowerbed) - 1 and n > 0:
+
+        if flowerbed[0]==0 and flowerbed[1] == 0:
+            flowerbed[0] = 1
+            n -= 1
+
+        i = 2
+        while i < len(flowerbed) - 2 and n > 0:
             if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
                 flowerbed[i] = 1
                 n -= 1
             i += 1
+
+        if flowerbed[len(flowerbed) - 1] == 0 and flowerbed[len(flowerbed) - 2] == 0:
+            n -= 1
 
         if n:
             return False
         return True
 
 a= Solution()
-print(a.canPlaceFlowers([0, 1, 0], 1))
+print(a.canPlaceFlowers([0,0,1,0,1], 1))
