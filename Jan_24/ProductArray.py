4fb6b4b07bcb81461032475c4f2df625dd92969a
Nimish <nimish.v.shinde60@gmail.com>
2024-02-08 19:52:05 +0530

    LeetCode 75 days challenge - Day 6 Prefix Suffix Product Array

--- Jan_24/ProductArray.py
+++ Jan_24/ProductArray.py
@@ -1,25 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums):
-        cnt = nums.count(0)
-        if cnt > 1:
-            return [0]*len(nums)
-        else:
-            prod = 1
-            for val in nums:
-                if val:
-                    prod *= val
-            if cnt == 1:
-                idx = nums.index(0)
-                nums = [0] * (len(nums) - 1)
-                nums.insert(idx, prod)
-                return nums
-            else:
-                for i in range(len(nums)):
-                    if nums[i]:
-                        nums[i] = prod // nums[i]
-                    else:
-                        nums[i] = prod
-                return nums
+        prefixProd = [1]*len(nums)
+        suffixProd = [1]*len(nums)
+        res = []
+        cnt = 1
+        sf = 1
+        for i in range(1, len(nums)):
+            cnt *= nums[i-1]
+            prefixProd[i] = cnt
+
+            sf *= nums[len(nums) - i]
+            suffixProd[len(nums) - i - 1] = sf
+
+        for i in range(len(nums)):
+            res.append(prefixProd[i] * suffixProd[i])
+
+        return res
 
 a= Solution()
-print(a.productExceptSelf([-1,1,0,-3,3]))
+print(a.productExceptSelf([1,2,3,4,5]))
