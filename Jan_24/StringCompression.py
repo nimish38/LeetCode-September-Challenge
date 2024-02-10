2cabfc3346340ef788fd4aed383311a814afd714
Nimish <nimish.v.shinde60@gmail.com>
2024-02-10 20:10:39 +0530

    LeetCode 75 days challenge - Day 6 String Compress Inplace

--- Jan_24/StringCompression.py
+++ Jan_24/StringCompression.py
@@ -1,25 +1,29 @@
 class Solution:
     def compress(self, chars):
-        if len(chars):
-            cnt, prevChar, changed = 1, chars[0], 0
-            for i in range(1, len(chars)):
-                if chars[i] != prevChar:
-                    chars[changed] = prevChar
-                    prevChar = chars[i]
-                    changed += 1
-                    if cnt > 1:
-                        chars[changed] = cnt
-                        changed += 1
-                        cnt = 1
-                else:
-                    cnt += 1
-            if cnt > 1:
-                chars[changed] = prevChar
-                changed += 1
-                chars[changed] = cnt
-                print(chars[:changed + 1])
-                return changed + 1
+        if len(chars) == 1:
             return 1
+        cnt, prevChar, changed = 1, chars[0], 0
+        for i in range(1, len(chars)):
+            if chars[i] != prevChar:
+                chars[changed] = prevChar
+                prevChar = chars[i]
+                changed += 1
+                if cnt > 1:
+                    for ele in str(cnt):
+                        chars[changed] = ele
+                        changed += 1
+                    cnt = 1
+            else:
+                cnt += 1
+
+        chars[changed] = prevChar
+        changed += 1
+        if cnt > 1:
+            for ele in str(cnt):
+                chars[changed] = ele
+                changed += 1
+        return changed
+
 
 a = Solution()
-print(a.compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]))
+print(a.compress(["a","b", "c"]))
