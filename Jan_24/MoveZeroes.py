13c3ca6127ebdb9fbc24cb566aad03517a974364
Nimish <nimish.v.shinde60@gmail.com>
2024-02-11 14:01:54 +0530

    LeetCode 75 days challenge - Day 6 Move Zeroes Optimal with pointer

--- Jan_24/MoveZeroes.py
+++ Jan_24/MoveZeroes.py
@@ -3,18 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zero, i = [], 0
-        while i < len(nums):
-            if nums[i] == 0:
-                zero.append(i)
-            else:
-                if len(zero) > 0:
-                    nums[zero[0]] = nums[i]
-                    del zero[0]
-                    nums[i] = 0
-                    zero.append(i)
-            i += 1
+        nonZero = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[nonZero] = nums[i]
+                nonZero += 1
+
+        for i in range(nonZero, len(nums)):
+            nums[i] = 0
         print(nums)
 
 a = Solution()
-a.moveZeroes([0])
+a.moveZeroes([0,1])
