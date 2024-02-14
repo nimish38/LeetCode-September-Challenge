621c15138a586c348a71a3312b97f5696a07651f
Nimish <nimish.v.shinde60@gmail.com>
2024-02-14 20:16:20 +0530

    LeetCode 75 days challenge - Day 6 Max K Sum Pairs Linear Counter collection

--- Jan_24/KSumPairs.py
+++ Jan_24/KSumPairs.py
@@ -1,19 +1,14 @@
+from collections import Counter
 class Solution:
     def maxOperations(self, nums, k):
-        val, cnt = dict(), 0
-        for num in nums:
-            diff = k - num
-            if diff > 0 and diff in val.keys():
-                cnt += 1
-                if val[diff] > 1:
-                    val[diff] -= 1
-                else:
-                    del val[diff]
-            else:
-                if num in val.keys():
-                    val[num] += 1
-                else:
-                    val[num] = 1
-        return cnt
+        res, d = 0, Counter(nums)
+
+        for val1, val1Cnt in d.items():
+            val2 = k - val1
+            # to get rid of duplicates consider only pairs where val1 >= val2
+            if val1 < val2 or val2 not in d: continue
+            res += min(val1Cnt, d[val2]) if val1 != val2 else val1Cnt // 2
+
+        return res
 
 print(Solution().maxOperations([2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2], 3))
