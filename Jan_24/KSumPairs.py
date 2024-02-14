2152fcb3850250a2252cdfb3428ae6983d27517c
Nimish <nimish.v.shinde60@gmail.com>
2024-02-14 20:11:23 +0530

    LeetCode 75 days challenge - Day 6 Max K Sum Pairs Linear using Dict

--- Jan_24/KSumPairs.py
+++ Jan_24/KSumPairs.py
@@ -1,18 +1,19 @@
 class Solution:
     def maxOperations(self, nums, k):
-        nums.sort()
-        i, cnt, j = 0, 0, len(nums) - 1
-        while j >= 0 and nums[j] >= k:
-            j -= 1
-        while i < j:
-            if nums[i] + nums[j] == k:
+        val, cnt = dict(), 0
+        for num in nums:
+            diff = k - num
+            if diff > 0 and diff in val.keys():
                 cnt += 1
-                i += 1
-                j -= 1
-            elif nums[i] + nums[j] > k:
-                j -= 1
+                if val[diff] > 1:
+                    val[diff] -= 1
+                else:
+                    del val[diff]
             else:
-                i += 1
+                if num in val.keys():
+                    val[num] += 1
+                else:
+                    val[num] = 1
         return cnt
 
-print(Solution().maxOperations([1,2,3,4], 5))
+print(Solution().maxOperations([2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2], 3))
