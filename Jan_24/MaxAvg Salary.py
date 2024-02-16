464d1bd71a6fb29ce003ce448e168e3fa42a52ce
Nimish <nimish.v.shinde60@gmail.com>
2024-02-16 19:45:53 +0530

    Max Average Subarray Optimal

--- Jan_24/MaxAvg Salary.py
+++ Jan_24/MaxAvg Salary.py
@@ -1,9 +1,11 @@
 class Solution:
     def findMaxAverage(self, nums, k):
-        maxSum = float('-inf')
-        for i in range(len(nums) - k + 1):
-            cur = sum(nums[i:i+k])
-            if cur > maxSum:
-                maxSum = cur
+        maxSum, curSum, last = sum(nums[:k]), sum(nums[:k]), nums[0]
+        for i in range(1, len(nums) - k + 1):
+            curSum = curSum - last + nums[i + k - 1]
+            if curSum > maxSum:
+                maxSum = curSum
+            last = nums[i]
         return maxSum/k
-print(Solution().findMaxAverage([5], 1))
+
+print(Solution().findMaxAverage([1, 12, -5, -6, 50, 3], 4))
