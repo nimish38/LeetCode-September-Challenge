a6634f8dba6274fb88da56c2c794694932b2c960
Nimish <nimish.v.shinde60@gmail.com>
2024-02-19 18:55:03 +0530

    Highest Altitude Prefix Sum Memory Optimisation

--- Jan_24/HighestAltitude.py
+++ Jan_24/HighestAltitude.py
@@ -1,10 +1,10 @@
 class Solution:
     def largestAltitude(self, gain):
-        highest = curr = 0
-        for height in gain:
-            curr += height
-            if curr > highest:
-                highest = curr
+        highest = max(0, gain[0])
+        for i in range(1, len(gain)):
+            gain[i] += gain[i-1]
+            if gain[i] > highest:
+                highest = gain[i]
         return highest
 
-print(Solution().largestAltitude([-4,-3,-2,-1,4,3,2]))
+print(Solution().largestAltitude([-5,1,5,0,-7]))
