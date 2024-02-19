327c6a61cf96f57c21f4c3b32b9bbd3d8dfce92d
Nimish <nimish.v.shinde60@gmail.com>
2024-02-19 19:47:24 +0530

    Pivot Element two array

--- Jan_24/PivotElement.py
+++ Jan_24/PivotElement.py
@@ -1,11 +1,14 @@
 class Solution:
     def pivotIndex(self, nums):
-        leftSum, rightSum = 0, sum(nums)
-        for idx, ele in enumerate(nums):
-            rightSum -= ele
-            if rightSum == leftSum:
-                return idx
-            leftSum += ele
+        n = len(nums)
+        leftSum, rightSum = [0]*n, [0]*n
+        for i in range(1, n):
+            leftSum[i] = leftSum[i - 1] + nums[i - 1]
+            rightSum[n - i - 1] = rightSum[n - i] + nums[n - i]
+
+        for i in range(n):
+            if leftSum[i] == rightSum[i]:
+                return i
         return -1
 
 print(Solution().pivotIndex([2,1,-1]))
