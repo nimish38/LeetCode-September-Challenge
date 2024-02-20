d4433eec61d830cdc84a52e3fda6c0ef94398d02
Nimish <nimish.v.shinde60@gmail.com>
2024-02-20 19:40:52 +0530

    Array Difference hashmap brute force

--- Jan_24/ArrayDifference.py
+++ Jan_24/ArrayDifference.py
@@ -1,8 +1,23 @@
 class Solution:
     def findDifference(self, nums1, nums2):
-        res, nums1, nums2 = [], set(nums1), set(nums2)
-        res.append(list(nums1 - nums2))
-        res.append(list(nums2 - nums1))
+        res, elem = [[], []], {}
+
+        for num in nums1:
+            if num not in elem:
+                elem[num] = False
+
+        for num in nums2:
+            if num in elem:
+                elem[num] = True
+            if num not in res[1]:
+                res[1].append(num)
+
+        for val in elem:
+            if not elem[val]:
+                res[0].append(val)
+
         return res
 
-print(Solution().findDifference([1,2,3,3], [1,1,2,2]))
+
+
+print(Solution().findDifference([1,2,3], [2,4,6]))
