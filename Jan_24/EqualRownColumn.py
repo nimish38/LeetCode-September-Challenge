9f68ac1a535ce841038567627dd62785583dc270
Nimish <nimish.v.shinde60@gmail.com>
2024-02-22 20:03:29 +0530

    Equal rows and columns count brute force

--- Jan_24/EqualRownColumn.py
+++ Jan_24/EqualRownColumn.py
@@ -1,25 +1,13 @@
 class Solution:
     def equalPairs(self, grid):
-        rows = {}
-        for row in grid:
-            row = list(map(str, row))
-            strrow = ' '.join(row)
-            if strrow in rows:
-                rows[strrow] += 1
-            else:
-                rows[strrow] = 1
-
         cnt = 0
         for i in range(len(grid)):
-            strcol = ''
+            strcol = []
             for j in range(len(grid)):
-                strcol += str(grid[j][i])
-                if j < len(grid) - 1:
-                    strcol += ' '
-            if strcol in rows:
-                cnt += rows[strcol]
+                strcol.append(grid[j][i])
+            cnt += grid.count(strcol)
         return cnt
 
-print(Solution().equalPairs([[11,1],[1,11]]))
+print(Solution().equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
 
 
