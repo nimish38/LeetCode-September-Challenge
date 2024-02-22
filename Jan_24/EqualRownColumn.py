940e990b32578fcb604cec5a1a41fc0926678976
Nimish <nimish.v.shinde60@gmail.com>
2024-02-22 20:29:56 +0530

    Equal rows and columns count optimal Counter

--- Jan_24/EqualRownColumn.py
+++ Jan_24/EqualRownColumn.py
@@ -1,12 +1,11 @@
+from collections import Counter
 class Solution:
     def equalPairs(self, grid):
-        cnt = 0
-        for i in range(len(grid)):
-            strcol = []
-            for j in range(len(grid)):
-                strcol.append(grid[j][i])
-            cnt += grid.count(strcol)
-        return cnt
+        # * is to unpack each list
+        transpose = Counter(zip(*grid))
+        # map is used to convert list to tuples as it is accepted by Counter
+        grid = Counter(map(tuple,grid))
+        return sum(transpose[t] * grid[t] for t in transpose)
 
 print(Solution().equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
 
