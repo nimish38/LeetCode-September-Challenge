990cb6548a50a6898f45a6790daaf610e1d506db
Nimish <nimish.v.shinde60@gmail.com>
2024-02-24 19:50:59 +0530

    Asteroids stack brute force

--- Jan_24/Asteroid Collision.py
+++ Jan_24/Asteroid Collision.py
@@ -1,43 +1,33 @@
 class Solution:
     def asteroidCollision(self, asteroids):
-        stack, top = [], -1
-        for ast in asteroids:
-            if top is -1:
+        stack, i, top = [], 0, -1
+
+        while i < len(asteroids) and asteroids[i] < 0:
+            stack.append(asteroids[i])
+            i += 1
+        top = len(stack) - 1
+
+        while i < len(asteroids):
+            ast = asteroids[i]
+            if ast > 0 or top is -1:
                 stack.append(ast)
                 top += 1
             else:
-                if ast < 0:
-                    if stack[top] < 0:
-                        stack.append(ast)
-                        top += 1
-                    else:
-                        while top >= 0 and stack[top] < abs(ast):
-                            stack.pop()
-                            top -= 1
-
-                        if top is -1:
-                            stack.append(ast)
-                            top += 1
-                        else:
-                            if stack[top] == abs(ast):
-                                stack.pop()
-                                top -= 1
+                if stack[top] < 0:
+                    stack.append(ast)
+                    top += 1
                 else:
-                    if stack[top] > 0:
+                    while top >= 0 and 0 < stack[top] < abs(ast):
+                        stack.pop()
+                        top -= 1
+                    if top is -1 or stack[top] < 0:
                         stack.append(ast)
                         top += 1
                     else:
-                        while top >= 0 and abs(stack[top]) < ast:
+                        if stack[top] == abs(ast):
                             stack.pop()
                             top -= 1
-
-                        if top is -1:
-                            stack.append(ast)
-                            top += 1
-                        else:
-                            if abs(stack[top]) == ast:
-                                stack.pop()
-                                top -= 1
+            i += 1
         return stack
 
-print(Solution().asteroidCollision([-2,-1,1,2]))
+print(Solution().asteroidCollision([-2,-1,1,2,-3]))
