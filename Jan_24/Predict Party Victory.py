ed66110e4db2eb696aa393e97376f8079dfc469c
Nimish <nimish.v.shinde60@gmail.com>
2024-02-28 21:14:20 +0530

    Cyclic queue brute force

--- Jan_24/Predict Party Victory.py
+++ Jan_24/Predict Party Victory.py
@@ -1,15 +1,25 @@
 class Solution:
+    def removeCyclic(self, arr, delete, i):
+        for _ in range(len(arr) + 1):
+            if arr[i] == delete:
+                del arr[i]
+                return
+            i = (i+1) % len(arr)
+
     def predictPartyVictory(self, senate: str) -> str:
         senate, i = list(senate), 0
         while len(set(senate)) == 2:
             if senate[i] == 'R':
-                senate.remove('D')
+                self.removeCyclic(senate,'D', i)
             else:
-                senate.remove('R')
-            i = (i + 1) % len(senate)
+                self.removeCyclic(senate, 'R', i)
+            if i + 1 >= len(senate):
+                i = 0
+            else:
+                i += 1
 
         if set(senate).pop() == 'R':
             return 'Radiant'
         return 'Dire'
 
-print(Solution().predictPartyVictory('RDD'))
+print(Solution().predictPartyVictory('DRRD'))
