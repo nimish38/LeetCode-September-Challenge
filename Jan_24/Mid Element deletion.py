2e04407b572c16b490b9ec19928cb539de1cd4b3
Nimish <nimish.v.shinde60@gmail.com>
2024-02-29 18:43:05 +0530

    Linked List 2 pointer deletion

---
+++ Jan_24/Mid Element deletion.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head):
+        fast, slow, prev = head, head, head
+        while fast.next.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        if fast.next:
+            prev = slow
+            slow = slow.next
+
+        prev.next = slow.next
+        slow.next = None
+        del slow
+        return head
