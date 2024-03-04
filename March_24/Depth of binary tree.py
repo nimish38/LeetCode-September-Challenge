0184ac4433da7dc7f873419cd5d3422b60c8d0ee
Nimish <nimish.v.shinde60@gmail.com>
2024-03-04 20:25:43 +0530

    Binary tree depth iterative Tree abstraction

--- March_24/Depth of binary tree.py
+++ March_24/Depth of binary tree.py
@@ -1,4 +1,35 @@
 # Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def insertBST(self, curr, new):
+        if new.val < curr.val:
+            if curr.left:
+                self.insert(curr.left, new)
+            else:
+                curr.left = new
+        else:
+            if curr.right:
+                self.insert(curr.right, new)
+            else:
+                curr.right = new
+
+    def addElement(self, node):
+        if not self.root:
+            self.root = node
+        else:
+            self.insert(self.root, node)
+    
+    def getHead(self):
+        return self.root
+
 class Solution:
     def maxDepth(self, root):
         if not root: return 0
@@ -15,3 +46,5 @@ class Solution:
                 cnt += 1
         return cnt
 
+BinaryTree = Tree()
+nodes = []
