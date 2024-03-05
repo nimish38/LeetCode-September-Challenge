e99de2155c380100be88debd6d7df6e79b768f50
Nimish <nimish.v.shinde60@gmail.com>
2024-03-05 19:45:52 +0530

    DFS using stack

---
+++ March_24/Leaf Similar Trees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leafs(self, root):
+        stack, res = [root], []
+        while stack:
+            item = stack.pop()
+            if not item.left and not item.right:
+                res.append(item)
+            else:
+                if item.right:
+                    stack.append(item.right)
+                if item.left:
+                    stack.append(item.left)
+        return res
+
+
+    def leafSimilar(self, root1, root2):
+        return self.leafs(root1) == self.leafs(root2)
