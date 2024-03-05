fbbc735c4670542f7dde667a963cf0cb00782c2d
Nimish <nimish.v.shinde60@gmail.com>
2024-03-05 20:07:30 +0530

    DFS using recursion

--- March_24/Leaf Similar Trees.py
+++ March_24/Leaf Similar Trees.py
@@ -1,19 +1,16 @@
 class Solution:
-    def leafs(self, root):
-        stack, res = [root], []
-        while stack:
-            item = stack.pop()
-            if not item.left and not item.right:
-                res.append(item.val)
-            else:
-                if item.right:
-                    stack.append(item.right)
-                if item.left:
-                    stack.append(item.left)
+    def leafs(self, item, res):
+        if not item.left and not item.right:
+            res.append(item.val)
+        else:
+            if item.left:
+                self.leafs(item.left, res)
+            if item.right:
+                self.leafs(item.right, res)
         return res
 
 
     def leafSimilar(self, root1, root2):
         # print(self.leafs(root1))
         # print(self.leafs(root2))
-        return self.leafs(root1) == self.leafs(root2)
+        return self.leafs(root1, []) == self.leafs(root2, [])
