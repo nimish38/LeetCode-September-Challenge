0bccd5366c3234b579e36b8c761084a48619deca
Nimish <nimish.v.shinde60@gmail.com>
2024-03-10 11:31:15 +0530

    Recursive function most optimal

--- March_24/Lowest Common Ancestor.py
+++ March_24/Lowest Common Ancestor.py
@@ -5,32 +5,14 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def getAncestors(self, node):
-        res = [node]
-        while self.lineage[node]:
-            res.append(self.lineage[node])
-            node = self.lineage[node]
-        return res
-
-
     def lowestCommonAncestor(self, root, p, q):
-        self.lineage, stack = {}, [(root, None)]
-        while p.val not in self.lineage or q.val not in self.lineage:
-            item = stack.pop()
-            if not item[1]:
-                self.lineage[item[0].val] = None
-            else:
-                self.lineage[item[0].val] = item[1].val
-            if item[0].right:
-                stack.append((item[0].right, item[0]))
-            if item[0].left:
-                stack.append((item[0].left, item[0]))
-
-        p = self.getAncestors(p.val)
-        q = self.getAncestors(q.val)
-        if len(p) == 0 or len(q) == 0:
+        if not root or root == p or root == q:
             return root
-        return [x for x in p if x in q][0]
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
 
 
 a = TreeNode(3)
@@ -41,4 +23,4 @@ a.left.right = TreeNode(2)
 a.left.right.left = TreeNode(7)
 a.left.right.right = TreeNode(4)
 
-print(Solution().lowestCommonAncestor(a, a.left, a.left.right.right))
+print(Solution().lowestCommonAncestor(a, a.left, a.left.right.right).val)
