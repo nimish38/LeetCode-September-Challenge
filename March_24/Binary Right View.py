5322fa4da863bc1418de5430eededc6f255170ad
Nimish <nimish.v.shinde60@gmail.com>
2024-03-10 12:30:54 +0530

    BFS with storing last node

--- March_24/Binary Right View.py
+++ March_24/Binary Right View.py
@@ -1,10 +1,28 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def rightSideView(self, root):
-        res = []
-        while root:
-            res.append(root.val)
-            if root.right:
-                root = root.right
-            else:
-                root = root.left
+        if not root:
+            return []
+        res, queue = [], [root]
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                item = queue.pop(0)
+                if i == n - 1:
+                    res.append(item.val)
+                if item.left:
+                    queue.append(item.left)
+                if item.right:
+                    queue.append(item.right)
         return res
+
+a = TreeNode(1)
+a.left = TreeNode(2)
+a.right = TreeNode(3)
+a.left.left = TreeNode(4)
+print(Solution().rightSideView(a))
