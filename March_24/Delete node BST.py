3d8dee7c14d350fd1e796293d0ce45033f70dc6e
Nimish <nimish.v.shinde60@gmail.com>
2024-03-13 20:24:38 +0530

    BST Deletion with iterative

--- March_24/Delete node BST.py
+++ March_24/Delete node BST.py
@@ -1,24 +1,52 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def findReplacement(self, node):
+    def findLefttmost(self, node):
         while node and node.left:
             node = node.left
         return node
 
+    def successor(self, node):
+        if not node.left:
+            return node.right
+        if not node.right:
+            return node.left
+        lastElem = self.findLefttmost(node.right)
+        lastElem.left = node.left
+        return node.right
+
     def deleteNode(self, root, key):
         if not root:
             return
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        elif root.val == key:
-            inorder_succ = self.findReplacement(root.right)
-            if not inorder_succ:
-                if root.left:
-                    root = root.left
+        if root.val == key:
+            return self.successor(root)
+        curr = root
+        while curr:
+            if key < curr.val and curr.left:
+                if curr.left.val == key:
+                    curr.left = self.successor(curr.left)
+                    break
                 else:
-                    return None
-            else:
-                root.val = inorder_succ.val
-                root.right = self.deleteNode(root.right, inorder_succ.val)
-        return root
+                    curr = curr.left
+
+            elif key > curr.val and curr.right:
+                if curr.right.val == key:
+                    curr.right = self.successor(curr.right)
+                    break
+                else:
+                    curr = curr.right
+        return curr
+
+a = TreeNode(5)
+a.left = TreeNode(3)
+a.right = TreeNode(6)
+a.left.left = TreeNode(2)
+a.left.right = TreeNode(4)
+a.right.right =TreeNode(7)
+
+res = Solution().deleteNode(a,3)
+print(res)
