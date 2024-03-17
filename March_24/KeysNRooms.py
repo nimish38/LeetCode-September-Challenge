8400033b039068b33e5005c030dc347be6722f4a
Nimish <nimish.v.shinde60@gmail.com>
2024-03-17 18:58:49 +0530

    Brute Force with nested loops

--- March_24/KeysNRooms.py
+++ March_24/KeysNRooms.py
@@ -1,14 +1,14 @@
 class Solution:
     def canVisitAllRooms(self, rooms):
         visited, stack = [0], [0]
-        while len(visited) < len(rooms) - 1 and stack:
+        while len(visited) < len(rooms) and stack:
             item = stack.pop()
             for ele in rooms[item]:
                 if ele not in visited:
                     visited.append(ele)
                     stack.append(ele)
 
-        return len(visited) == len(rooms) - 1
+        return len(visited) == len(rooms)
 
 
 print(Solution().canVisitAllRooms([[1,3],[3,0,1],[2],[0]]))
