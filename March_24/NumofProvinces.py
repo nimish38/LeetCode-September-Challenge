05d171606d88944d4c773ef39e8e4437cb0fc321
Nimish <nimish.v.shinde60@gmail.com>
2024-03-18 20:23:07 +0530

    Build adjacency matrix

--- March_24/NumofProvinces.py
+++ March_24/NumofProvinces.py
@@ -8,8 +8,17 @@ class Solution:
             for j in range(n):
                 if i != j and isConnected[i][j] and j not in visited:
                     visited[i].add(j)
-        print(visited)
 
+        # check transitive
+        i = 0
+        while i < len(visited):
+            j = 0
+            adj = list(visited[list(visited.keys())[i]])
+            while j < len(adj):
+                visited[i] = visited[i].union(visited[adj[j]])
+                del visited[adj[j]]
+                j += 1
+            i += 1
+        return len(visited)
 
-
-Solution().findCircleNum([[1,1,0],[1,1,0],[0,0,1]])
+print(Solution().findCircleNum([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]]))
