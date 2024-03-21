c8147f739b7c0863d22afb531d11ec2d08975b8b
Nimish <nimish.v.shinde60@gmail.com>
2024-03-21 20:40:39 +0530

    Graph building with DFS

--- March_24/Evaluate Division.py
+++ March_24/Evaluate Division.py
@@ -1,16 +1,26 @@
 from collections import defaultdict
 class Solution:
     def dfs(self, src, dest):
-        stack = [(src, -1)]
+        stack = [(src, -1, 1.0)]
         while stack:
             item = stack.pop()
-            
+            for neighbor, val in self.graph[item[0]]:
+                if neighbor == dest:
+                    return item[2] * val
+                elif neighbor == item[1]:
+                    continue
+                else:
+                    stack.append((neighbor, item[0], item[2] * val))
+        return -1.0
+
+
+
 
     def calcEquation(self, equations, values, queries):
         self.graph, i = defaultdict(list), 0
         for u, v in equations:
-            self.graph[u].append(values[i])
-            self.graph[v].append(1 / values[i])
+            self.graph[u].append((v, values[i]))
+            self.graph[v].append((u, 1 / values[i]))
             i += 1
 
         res = []
@@ -22,3 +32,6 @@ class Solution:
             else:
                 res.append(self.dfs(u, v))
 
+        return res
+
+print(Solution().calcEquation(equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]))
