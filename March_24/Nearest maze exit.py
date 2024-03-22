05cbd628f624446e49aa3e2a544c8e6633adef4b
Nimish <nimish.v.shinde60@gmail.com>
2024-03-22 20:23:26 +0530

    added exit possible check

--- March_24/Nearest maze exit.py
+++ March_24/Nearest maze exit.py
@@ -5,29 +5,35 @@ class Solution:
         stack = [(entrance, 0)]
         maze[entrance[0]][entrance[1]] = 'v'
 
+        if maze[0].count('.') + maze[row - 1].count('.') == 0:
+            exitfound = False
+            for i in range(1, row - 1):
+                if maze[i][0] == '.' or maze[i][col - 1] == '.':
+                    exitfound = True
+                    break
+        if not exitfound:
+            return - 1
+
         while stack:
             item = stack.pop(0)
             x, y = item[0][0], item[0][1]
             if maze[x][y] == '.' and (x == 0 or x == row - 1 or y == 0 or y == col - 1):
-                return item[1] + 1
+                return item[1]
             else:
                 if y > 0 and maze[x][y - 1] == '.':
-                    maze[x][y - 1] = 'v'
                     stack.append(([x, y - 1], item[1] + 1))
 
-                if y < row - 1 and maze[x][y + 1] == '.':
-                    maze[x][y + 1] = 'v'
+                if y < col - 1 and maze[x][y + 1] == '.':
                     stack.append(([x, y + 1], item[1] + 1))
 
                 if x > 0 and maze[x - 1][y] == '.':
-                    maze[x - 1][y] = 'v'
                     stack.append(([x - 1, y], item[1] + 1))
 
-                if x < col - 1 and maze[x + 1][y] == '.':
-                    maze[x + 1][y] = 'v'
+                if x < row - 1 and maze[x + 1][y] == '.':
                     stack.append(([x + 1, y], item[1] + 1))
+            maze[x][y] = 'v'
         return -1
 
 
 
-print(Solution().nearestExit(maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]))
+# print(Solution().nearestExit(maze = [[".","+"]], entrance = [0,0]))
