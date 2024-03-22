c6ed3a20f1844a96bbca2ede62a09074f8dd5c38
Nimish <nimish.v.shinde60@gmail.com>
2024-03-22 19:46:52 +0530

    bfs with quadri direction check

--- March_24/Nearest maze exit.py
+++ March_24/Nearest maze exit.py
@@ -4,6 +4,30 @@ class Solution:
         col = len(maze[0])
         stack = [(entrance, 0)]
         maze[entrance[0]][entrance[1]] = 'v'
-        print(stack)
 
-Solution().nearestExit(maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0])
+        while stack:
+            item = stack.pop(0)
+            x, y = item[0][0], item[0][1]
+            if maze[x][y] == '.' and (x == 0 or x == row - 1 or y == 0 or y == col - 1):
+                return item[1] + 1
+            else:
+                if y > 0 and maze[x][y - 1] == '.':
+                    maze[x][y - 1] = 'v'
+                    stack.append(([x, y - 1], item[1] + 1))
+
+                if y < row - 1 and maze[x][y + 1] == '.':
+                    maze[x][y + 1] = 'v'
+                    stack.append(([x, y + 1], item[1] + 1))
+
+                if x > 0 and maze[x - 1][y] == '.':
+                    maze[x - 1][y] = 'v'
+                    stack.append(([x - 1, y], item[1] + 1))
+
+                if x < col - 1 and maze[x + 1][y] == '.':
+                    maze[x + 1][y] = 'v'
+                    stack.append(([x + 1, y], item[1] + 1))
+        return -1
+
+
+
+print(Solution().nearestExit(maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]))
