95c5d7b3d67b30f214cc94b729ec7ae7ed419d33
Nimish <nimish.v.shinde60@gmail.com>
2024-03-24 13:49:34 +0530

    Quick select algorithm

--- March_24/Kth largest element.py
+++ March_24/Kth largest element.py
@@ -1,6 +1,30 @@
-import heapq
 class Solution:
     def findKthLargest(self, nums, k):
-        return heapq.nlargest(k, nums)[-1]
+        def QuickSelect(i, j):
+            pivot = i
+            i += 1
+            while i <= j:
+                if nums[i] < nums[pivot] < nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i += 1
+                    j -= 1
+                if nums[i] >= nums[pivot]:
+                    i += 1
+                if nums[j] <= nums[pivot]:
+                    j -= 1
+            nums[pivot], nums[j] = nums[j], nums[pivot]
+            return j
 
-print(Solution().findKthLargest(nums = [3,2,3,1,2,4,5,5,6], k = 4))
+        left, right, pivot_index = 0, len(nums) - 1, 0
+        while True:
+            pivot_index = QuickSelect(left, right)
+            if pivot_index == k - 1:
+                return nums[pivot_index]
+            elif pivot_index < k - 1:
+                left = pivot_index + 1
+            else:
+                right = pivot_index - 1
+
+
+
+# print(Solution().findKthLargest(nums = [1], k = 1))
