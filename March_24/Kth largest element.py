aa644be719e810ba52683134ed63993765493994
Nimish <nimish.v.shinde60@gmail.com>
2024-03-24 12:18:05 +0530

    using inbuilt heap prio queue

--- March_24/Kth largest element.py
+++ March_24/Kth largest element.py
@@ -1,5 +1,6 @@
+import heapq
 class Solution:
     def findKthLargest(self, nums, k):
-        return sorted(nums, reverse=True)[k - 1]
+        return heapq.nlargest(k, nums)[-1]
 
 print(Solution().findKthLargest(nums = [3,2,3,1,2,4,5,5,6], k = 4))
