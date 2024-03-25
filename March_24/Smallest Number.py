46d4834b3bda23dee9b0d8d555b1d56c78c23f71
Nimish <nimish.v.shinde60@gmail.com>
2024-03-25 13:09:13 +0530

    Correct use of heapq library

--- March_24/Smallest Number.py
+++ March_24/Smallest Number.py
@@ -1,19 +1,27 @@
 import heapq
 class SmallestInfiniteSet:
     def __init__(self):
-        self.cnt, self.added = 1, heapq
+        self.cnt, self.added = 1, []
+        heapq.heapify(self.added)
 
     def popSmallest(self) -> int:
         if len(self.added) > 0 and self.added[0] < self.cnt:
-            return self.added.heappop()
+            return heapq.heappop(self.added)
         self.cnt += 1
         return self.cnt - 1
 
     def addBack(self, num: int) -> None:
         if self.cnt > num:
-            self.added.heappush(num)
+            heapq.heappush(self.added, num)
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
-# obj = SmallestInfiniteSet()
-# param_1 = obj.popSmallest()
-# obj.addBack(num)
+obj = SmallestInfiniteSet()
+print(obj.popSmallest())
+obj.addBack(1)
+print(obj.popSmallest())
+print(obj.popSmallest())
+print(obj.popSmallest())
+print(obj.popSmallest())
+obj.addBack(3)
+print(obj.popSmallest())
+print(obj.popSmallest())
