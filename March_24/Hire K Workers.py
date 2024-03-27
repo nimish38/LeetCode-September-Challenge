f0609ccdf02b4ed0e354acf3a05db283487347d1
Nimish <nimish.v.shinde60@gmail.com>
2024-03-27 21:28:05 +0530

    overlapping error

--- March_24/Hire K Workers.py
+++ March_24/Hire K Workers.py
@@ -2,20 +2,23 @@ import heapq
 class Solution:
     def totalCost(self, costs, k, candidates):
         cost, first, last = 0, costs[:candidates], costs[len(costs) - candidates:]
+        i, j = candidates - 1, len(costs) - candidates
+        for var in range(j, i + 1):
+            last.pop(0)
         heapq.heapify(first)
         heapq.heapify(last)
-        i, j = candidates - 1, len(costs) - candidates
         while k > 0:
-            if first[0] <= last[0]:
+            if len(last) == 0 or  first[0] <= last[0]:
                 cost += heapq.heappop(first)
-                i += 1
-                heapq.heappush(first, costs[i])
+                if i < j:
+                    i += 1
+                    heapq.heappush(first, costs[i])
             else:
                 cost += heapq.heappop(last)
-                j -= 1
-                heapq.heappush(first, costs[j])
+                if j > i:
+                    j -= 1
+                    heapq.heappush(last, costs[j])
             k -= 1
-
         return cost
 
-print(Solution().totalCost(costs = [17,12,10,2,7,2,11,20,8], k = 3, candidates = 4))
+print(Solution().totalCost(costs = [1,2,4,1], k = 4, candidates = 3))
