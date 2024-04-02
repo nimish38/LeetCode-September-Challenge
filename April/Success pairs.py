e1fbe6f5da6dab4f9a5dcf3d746d594ac9837232
Nimish <nimish.v.shinde60@gmail.com>
2024-04-02 21:50:58 +0530

    Array bisection library

--- April/Success pairs.py
+++ April/Success pairs.py
@@ -1,27 +1,10 @@
-import math
+import bisect
 class Solution:
-    def successfulPairs(self, spells, potions, success):
-        def minLargest(key):
-            low, high, best = 0, n - 1, n
-            while low <= high:
-                mid = ((high - low) // 2) + low
-                if potions[mid] >= key:
-                    best = mid
-                    high = mid - 1
-                else:
-                    low = mid + 1
-            return n - best
-
-        potions.sort()
-        pairs, n = [], len(potions)
+    def successfulPairs(self, spells, potions, success: int):
+        res = []
+        potions = sorted(potions)
+        l = len(potions)
         for spell in spells:
-            mid = math.ceil(success / spell)
-            if mid < potions[0]:
-                pairs.append(n)
-            elif mid > potions[-1]:
-                pairs.append(0)
-            else:
-                pairs.append(minLargest(mid))
-        return pairs
-
+            res.append(l - bisect.bisect_left(potions, success/spell))
+        return res
 print(Solution().successfulPairs(spells = [5,1,3], potions = [1,2,3,4,5], success = 7))
