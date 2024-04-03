0ec7f123ff3ff747f1e66d54d4049b5c826e421b
Nimish <nimish.v.shinde60@gmail.com>
2024-04-03 19:09:55 +0530

    Brute Force linear search edge cases

--- April/Peak Element.py
+++ April/Peak Element.py
@@ -1,9 +1,15 @@
 class Solution:
     def findPeakElement(self, nums):
         if len(nums) < 3:
-            return max(nums)
+            return nums.index(max(nums))
         for i in range(1, len(nums) - 1):
             if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
                 return i
+
+        if nums[0] > nums[1]:
+            return 0
+
+        if nums[-1] > nums[-2]:
+            return len(nums) - 1
 
 print(Solution().findPeakElement(nums = [1,2,1,3,5,6,4]))
