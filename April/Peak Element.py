e0f2f287f256d54220d45c2d8f032bc8808dc463
Nimish <nimish.v.shinde60@gmail.com>
2024-04-03 21:21:32 +0530

    Binary search with edge cases

--- April/Peak Element.py
+++ April/Peak Element.py
@@ -1,15 +1,18 @@
 class Solution:
     def findPeakElement(self, nums):
-        if len(nums) < 3:
-            return nums.index(max(nums))
-        for i in range(1, len(nums) - 1):
-            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
-                return i
-
-        if nums[0] > nums[1]:
+        if len(nums) == 1 or nums[0] > nums[1]:
             return 0
-
         if nums[-1] > nums[-2]:
             return len(nums) - 1
+
+        low, high = 1, len(nums) - 2
+        while low <= high:
+            mid = low + (high - low)//2
+            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                return mid
+            elif nums[mid] > nums[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
 
 print(Solution().findPeakElement(nums = [1,2,1,3,5,6,4]))
