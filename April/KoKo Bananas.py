7de93b8c0164041ab6e98d4e310593cd0c4c43e1
Nimish <nimish.v.shinde60@gmail.com>
2024-04-04 20:19:48 +0530

    Apply binary search on 1 to max for bananas / hr

---
+++ April/KoKo Bananas.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles, h):
+
+        def canEat(bananas_per_hour):
+            cnt = 0
+            for pile in piles:
+                cnt += math.ceil(pile / bananas_per_hour)
+            return cnt <= h
+
+        low, high, best = 1, max(piles), -1
+        while low <= high:
+            mid = low + (high - low)//2
+            if canEat(mid):
+                best = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return best
+
+print(Solution().minEatingSpeed(piles = [30,11,23,4,20], h = 6))
