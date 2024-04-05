79d84e1fe102c501ca1c718b7414157fbeaaaf33
Nimish <nimish.v.shinde60@gmail.com>
2024-04-05 20:23:21 +0530

    Hardcode buttons performance improvement

--- April/Phone Letters.py
+++ April/Phone Letters.py
@@ -5,20 +5,21 @@ class Solution:
         if not digits:
             return []
 
-        buttons, letters, cnt = defaultdict(), string.ascii_lowercase, 0
-        for i in range(2, 10):
-            buttons[i] = list(letters[cnt: cnt + 3])
-            cnt += 3
-
-            if i == 7 or i == 9:
-                buttons[i].append(letters[cnt])
-                cnt += 1
-
+        buttons = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
         letters = []
-        letters.extend(buttons[int(digits[0])])
+        letters.extend(buttons[digits[0]])
         for i in range(1, len(digits)):
             for j in range(len(letters)):
-                for char in buttons[int(digits[i])]:
+                for char in buttons[digits[i]]:
                     letters.append(letters[j] + char)
             letters = letters[j + 1:]
         return letters
