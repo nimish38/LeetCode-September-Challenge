3545892580592db3f124582d5b87bb5bb803ab14
Nimish <nimish.v.shinde60@gmail.com>
2024-04-08 20:33:21 +0530

    edge cases check

--- April/Climbing Stairs.py
+++ April/Climbing Stairs.py
@@ -2,19 +2,21 @@ class Solution:
     def minCostClimbingStairs(self, cost):
 
         def climb(ind, curr_cost):
-            if ind == len(cost) and curr_cost < self.res:
-                res = curr_cost
-                return res
-            elif ind == len(cost) - 1 and curr_cost + cost[ind] < self.res:
-                res = curr_cost + cost[ind]
-                return res
+            if ind == n and curr_cost < self.res:
+                self.res = curr_cost
+                return
+            elif ind == n - 1 and curr_cost + cost[ind] < self.res:
+                self.res = curr_cost + cost[ind]
+                return
 
-            climb(ind + 1, curr_cost + cost[ind])
-            climb(ind + 2, curr_cost + cost[ind])
+            if ind < n - 1:
+                climb(ind + 1, curr_cost + cost[ind])
+                climb(ind + 2, curr_cost + cost[ind])
 
+        n = len(cost)
         self.res = float('inf')
         climb(0, 0)
         climb(1, 0)
         return self.res
 
-print(Solution().minCostClimbingStairs(cost = [10,15,20]))
+print(Solution().minCostClimbingStairs(cost = [1,100,1,1,1,100,1,1,100,1]))
