f6625791816789c929ce77ff9e6f435bd366ff04
Nimish <nimish.v.shinde60@gmail.com>
2024-04-08 21:11:42 +0530

    inplace iterative bottoms up

--- April/Climbing Stairs.py
+++ April/Climbing Stairs.py
@@ -1,15 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost):
-        def climb(ind):
-            if ind >= n:
-                return 0
-            if memorise[ind] != -1:
-                return memorise[ind]
-            memorise[ind] = cost[ind] + min(climb(ind + 1), climb(ind + 2))
-            return memorise[ind]
-
         n = len(cost)
-        memorise = [-1]*(n + 1)
-        return min(climb(0), climb(1))
+        for i in range(2, n):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return min(cost[-1], cost[-2])
 
 print(Solution().minCostClimbingStairs(cost = [1,100,1,1,1,100,1,1,100,1]))
