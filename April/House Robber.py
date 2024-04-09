8b7d9ecb900518d05071dbe9b5db251920081b39
Nimish <nimish.v.shinde60@gmail.com>
2024-04-09 13:11:32 +0530

    bottoms up approach iterative

--- April/House Robber.py
+++ April/House Robber.py
@@ -1,13 +1,8 @@
 class Solution:
     def rob(self, nums):
-        def steal(ind):
-            if ind >= len(nums):
-                return 0
-            if profit[ind] == -1:
-                profit[ind] = max(nums[ind] + steal(ind + 2), steal(ind + 1))
-            return profit[ind]
-        profit = [-1] * len(nums)
-        return steal(0)
-
+        profit = [0, nums[0]]
+        for i in range(2, len(nums) + 1):
+            profit.append(max(nums[i - 1] + profit[i - 2], profit[i - 1]))
+        return profit[-1]
 
 print(Solution().rob([2,7,9,3,1]))
