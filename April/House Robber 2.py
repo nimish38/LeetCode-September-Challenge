2906186a7bc7b8c932b16a6f8676df43f319c667
Nimish <nimish.v.shinde60@gmail.com>
2024-04-11 12:38:54 +0530

    bottom up with twin array

--- April/House Robber 2.py
+++ April/House Robber 2.py
@@ -1,16 +1,22 @@
 class Solution:
     def rob(self, nums):
         n = len(nums)
-        profit = [-1]*(n - 1)
-        sec_profit = [-1]*(n)
+        if n == 1: return nums[0]
+        if n == 2: return max(nums)
 
-        def dp(ind, end, arr):
-            if ind > end:
-                return 0
-            if arr[ind] == -1:
-                arr[ind] = max(nums[ind] + dp(ind + 2, end, arr), dp(ind + 1, end, arr))
-            return arr[ind]
+        def dp(start, end):
+            profit = [-1] * (n)
+            profit[start] = nums[start]
+            profit[start + 1] = max(nums[start], nums[start + 1])
+            i = start + 2
+            while(i <= end):
+                profit[i] = max(nums[i] + profit[i - 2], profit[i - 1])
+                i += 1
 
-        return max(dp(0, n-2, profit), dp(1, n-1, sec_profit))
+            if profit[-1] == -1:
+                return profit[-2]
+            return profit[-1]
+
+        return max(dp(0, n - 2), dp(1, n - 1))
 
 print(Solution().rob(nums = [1,2,3,1]))
