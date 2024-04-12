26c65f41414dc76624a546e6a6aa05277fa632e3
Nimish <nimish.v.shinde60@gmail.com>
2024-04-12 20:58:01 +0530

    recursive backtracking with memoization

--- April/Unique Paths.py
+++ April/Unique Paths.py
@@ -5,8 +5,15 @@ class Solution:
                 return 0
             if col == 0 or row == 0:
                 return 1
+            if memo[row][col] == -1:
+                memo[row][col] = findPaths(row, col - 1) + findPaths(row - 1, col)
+            return memo[row][col]
+        if m == 1 or n == 1:
+            return 1
+        memo = []
+        for _ in range(m):
+            memo.append([-1]*n)
 
-            return findPaths(row, col - 1) + findPaths(row - 1, col)
         return findPaths(m - 1, n - 1)
 
-print(Solution().uniquePaths(m = 3, n = 3))
+print(Solution().uniquePaths(m = 3, n = 7))
