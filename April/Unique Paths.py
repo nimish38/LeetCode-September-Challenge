efd165ad11902e3f6d5549a571f847cce24319cd
Nimish <nimish.v.shinde60@gmail.com>
2024-04-12 21:18:34 +0530

    bottom up iterative

--- April/Unique Paths.py
+++ April/Unique Paths.py
@@ -1,19 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int):
-        def findPaths(row, col):
-            if col == 0 and row == 0:
-                return 0
-            if col == 0 or row == 0:
-                return 1
-            if memo[row][col] == -1:
-                memo[row][col] = findPaths(row, col - 1) + findPaths(row - 1, col)
-            return memo[row][col]
-        if m == 1 or n == 1:
-            return 1
-        memo = []
-        for _ in range(m):
-            memo.append([-1]*n)
+        dp = [[1] * n for _ in range(m)]
 
-        return findPaths(m - 1, n - 1)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[m - 1][n - 1]
 
 print(Solution().uniquePaths(m = 3, n = 7))
