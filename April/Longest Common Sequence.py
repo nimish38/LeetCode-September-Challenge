93076b3e3005eb026d9d0cb2c15a7740eba6006a
Nimish <nimish.v.shinde60@gmail.com>
2024-04-13 13:18:34 +0530

    iterative bottom up

--- April/Longest Common Sequence.py
+++ April/Longest Common Sequence.py
@@ -2,18 +2,23 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str):
         m, n = len(text1), len(text2)
         memo = []
-        for _ in range(m):
-            memo.append([-1] * n)
-
-        def dpLcs(i, j):
-            if i == m or j == n:
-                return 0
-            if memo[i][j] == -1:
-                if text1[i] == text2[j]:
-                    memo[i][j] = 1 + dpLcs(i + 1, j + 1)
+        for i in range(m + 1):
+            temp = []
+            for j in range(n + 1):
+                if i == 0 or j == 0:
+                    temp.append(0)
                 else:
-                    memo[i][j] = max(dpLcs(i + 1, j), dpLcs(i, j + 1))
-            return memo[i][j]
-        return dpLcs(0, 0)
+                    temp.append(-1)
+            memo.append(temp)
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    memo[i][j] = 1 + memo[i - 1][j - 1]
+                else:
+                    memo[i][j] = max(memo[i][j - 1], memo[i - 1][j])
+        # print(memo)
+        return memo[m][n]
+
 
 print(Solution().longestCommonSubsequence(text1 = "abcde", text2 = "ace"))
