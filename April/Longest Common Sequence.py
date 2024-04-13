ff7bd34f1af85364a82c6488fec62871cbe2c201
Nimish <nimish.v.shinde60@gmail.com>
2024-04-13 12:42:35 +0530

    recursive top down DP with memoization

--- April/Longest Common Sequence.py
+++ April/Longest Common Sequence.py
@@ -1,12 +1,18 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str):
         m, n = len(text1), len(text2)
+        memo = []
+        for _ in range(m):
+            memo.append([-1] * n)
+
         def dpLcs(i, j):
             if i == m or j == n:
                 return 0
             if text1[i] == text2[j]:
                 return 1 + dpLcs(i + 1, j + 1)
-            return max(dpLcs(i + 1, j), dpLcs(i, j + 1))
+            if memo[i][j] == -1:
+                memo[i][j] = max(dpLcs(i + 1, j), dpLcs(i, j + 1))
+            return memo[i][j]
         return dpLcs(0, 0)
 
 print(Solution().longestCommonSubsequence(text1 = "abcde", text2 = "ace"))
