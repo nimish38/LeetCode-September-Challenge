54e9765a868ef661fc34486c4505ba52da521659
Nimish <nimish.v.shinde60@gmail.com>
2024-04-14 13:46:02 +0530

    recursive with transaction / hold checking

--- April/Stock Transfer.py
+++ April/Stock Transfer.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxProfit(self, prices, fee):
-        def market(purchase, curr_profit, ind):
+        def market(ind, need_buying):
             if ind == len(prices):
-                return curr_profit
+                return 0
 
-            # Transaction
-            if purchase == -1:
-                return max(market(prices[ind], curr_profit, ind + 1), market(purchase, curr_profit, ind + 1))
-            sale = 0
-            if prices[ind] - fee - purchase > 0:
-                sale = market(-1, curr_profit + prices[ind] - fee - purchase, ind + 1)
-            return max(sale, market(purchase, curr_profit, ind + 1))
+            profit = 0
+            # If it is buying
+            if need_buying:
+                sale = market(ind + 1, False) - prices[ind]
+                hold = market(ind + 1, True)
+                profit = max(profit, sale, hold)
+            # if it is selling
+            else:
+                sale = market(ind + 1, True) + prices[ind] - fee
+                hold = market(ind + 1, False)
+                profit = max(profit, sale, hold)
+            return profit
 
-        return market(-1, 0, 0)
+        return market(0, True)
 
 print(Solution().maxProfit(prices = [1,3,7,5,10,3], fee = 3))
