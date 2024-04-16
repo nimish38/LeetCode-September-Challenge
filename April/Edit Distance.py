4fea0442bb05064829ef8bc3c95c632ae7b25ef2
Nimish <nimish.v.shinde60@gmail.com>
2024-04-16 22:07:00 +0530

    iteratie approach bottoms up

--- April/Edit Distance.py
+++ April/Edit Distance.py
@@ -2,23 +2,21 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
         memo = []
-        for _ in range(m):
-            memo.append([-1] * n)
+        for _ in range(m + 1):
+            memo.append([-1] * (n + 1))
 
-        def match(i, j):
-            if i == m:
-                return n - j
-            if j == n:
-                return m - i
-            if memo[i][j] == -1:
-                if word1[i] == word2[j]:
-                    memo[i][j] = match(i + 1, j + 1)
+        for i in range(0, m + 1):
+            for j in range(0, n + 1):
+                if i == 0 or j == 0:
+                    memo[i][j] = i + j
+                elif word1[i - 1] == word2[j - 1]:
+                    memo[i][j] = memo[i - 1][j - 1]
                 else:
-                    insert = 1 + match(i, j + 1)
-                    delete = 1 + match(i + 1, j)
-                    replace = 1 + match(i + 1, j + 1)
+                    insert = 1 + memo[i][j - 1]
+                    delete = 1 + memo[i - 1][j]
+                    replace = 1 + memo[i - 1][j - 1]
                     memo[i][j] = min(insert, delete, replace)
-            return memo[i][j]
-        return match(0, 0)
+        return memo[m][n]
+
 
 print(Solution().minDistance(word1 = "horse", word2 = "ros"))
