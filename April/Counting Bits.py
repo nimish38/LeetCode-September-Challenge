d562e9517675f255d53c1afb912a679fce93a847
Nimish <nimish.v.shinde60@gmail.com>
2024-04-17 20:08:33 +0530

    math solution

--- April/Counting Bits.py
+++ April/Counting Bits.py
@@ -1,8 +1,13 @@
 class Solution:
     def countBits(self, n: int):
         res = [0]
+        if n == 0:
+            return res
         for i in range(1, n + 1):
-            res.append(str(bin(i)).count('1'))
+            if i % 2:
+                res.append(res[i // 2] + 1)
+            else:
+                res.append(res[i // 2])
         return res
 
 print(Solution().countBits(5))
