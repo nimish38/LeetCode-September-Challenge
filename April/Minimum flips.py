a5b98f7fe6c60cab50907304e9ac1e4d3d9d16bb
Nimish <nimish.v.shinde60@gmail.com>
2024-04-18 20:16:05 +0530

    bitwise operators

--- April/Minimum flips.py
+++ April/Minimum flips.py
@@ -1,22 +1,19 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int):
-        a, b, c = bin(a)[2:], bin(b)[2:], bin(c)[2:]
-        appen_len = max(len(a), len(b), len(c))
-
-        a = ('0' * (appen_len - len(a))) + a
-        b = ('0' * (appen_len - len(b))) + b
-        c = ('0' * (appen_len - len(c))) + c
-
         cnt = 0
-        for i in range(appen_len):
-            if c[i] == '1':
-                if a[i] == '0' and b[i] == '0':
+        while a > 0 and b > 0 and c > 0: 
+            if c & 1 == 1:
+                if a & 1 == 0 and b & 1 == 0:
                     cnt += 1
             else:
-                if a[i] == '1' and b[i] == '1':
+                if a & 1 == 1 and b & 1 == 1:
                     cnt += 2
-                elif a[i] == '1' or b[i] == '1':
+                elif a & 1 == 1 or b & 1 == 1:
                     cnt += 1
+
+            a = a >> 1
+            b = b >> 1
+            c = c >> 1
         return cnt
 
-print(Solution().minFlips(a = 1, b = 2, c = 3))
+print(Solution().minFlips(a = 2, b = 6, c = 5))
