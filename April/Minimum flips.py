07cccd0b17d52c72c3ecf316fc5c4ab0a4c99a5c
Nimish <nimish.v.shinde60@gmail.com>
2024-04-18 20:37:17 +0530

    bitwise operators intuition

--- April/Minimum flips.py
+++ April/Minimum flips.py
@@ -1,19 +1,7 @@
 class Solution:
     def minFlips(self, a: int, b: int, c: int):
-        cnt = 0
-        while a > 0 and b > 0 and c > 0: 
-            if c & 1 == 1:
-                if a & 1 == 0 and b & 1 == 0:
-                    cnt += 1
-            else:
-                if a & 1 == 1 and b & 1 == 1:
-                    cnt += 2
-                elif a & 1 == 1 or b & 1 == 1:
-                    cnt += 1
-
-            a = a >> 1
-            b = b >> 1
-            c = c >> 1
-        return cnt
+        xor = (a | b) ^ c
+        singles = xor & (a & b)
+        return bin(singles).count('1') + bin(xor).count('1')
 
 print(Solution().minFlips(a = 2, b = 6, c = 5))
