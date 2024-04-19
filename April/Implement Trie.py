4e4e3574fcf63ad333c8a1343ae8735795c40c46
Nimish <nimish.v.shinde60@gmail.com>
2024-04-19 20:20:03 +0530

    Trie Implementation with * delimit

--- April/Implement Trie.py
+++ April/Implement Trie.py
@@ -5,10 +5,10 @@ class Trie:
     def insert(self, word: str) -> None:
         curr = self.trie
         for letter in word:
-            if curr[letter]  not in curr:
+            if letter not in curr:
                 curr[letter] = {}
             curr = curr[letter]
-        curr[letter] = '*'
+        curr['*'] = '*'
 
     def search(self, word: str) -> bool:
         curr = self.trie
@@ -16,13 +16,22 @@ class Trie:
             if letter not in curr:
                 return False
             curr = curr[letter]
-        if curr[letter] == '*':
+        if '*' in curr:
             return True
+        return False
 
     def startsWith(self, prefix: str) -> bool:
         curr = self.trie
         for letter in prefix:
             if letter not in curr:
                 return False
+            curr = curr[letter]
         return True
 
+a = Trie()
+print(a.insert('apple'))
+print(a.search('apple'))
+print(a.search('app'))
+print(a.startsWith('app'))
+print(a.insert('app'))
+print(a.search('app'))
