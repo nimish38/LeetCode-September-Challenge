155b3e51fd0f9b361c13a7aaf92d8f5fc016a1aa
Nimish <nimish.v.shinde60@gmail.com>
2024-04-20 13:27:09 +0530

    Perform DFS on Trie

--- April/Search Suggestion.py
+++ April/Search Suggestion.py
@@ -10,6 +10,31 @@ class Trie:
             curr = curr[letter]
         curr['*'] = True
 
+    def getwords(self, let, curr):
+        stack = [(let, curr[let])], res = []
+        while stack:
+            item = stack.pop()
+            for char in item[1]:
+                if char == '*':
+                    res.append(item[1])
+                else:
+                    stack.append((item[0] + char, item[1][char]))
+        if len(res) > 3:
+            return res[:3]
+        return res
+
+    def suggestions(self, searchWord):
+        res = []
+        curr = self.trie
+        for letter in searchWord:
+            if letter in curr:
+                res.append(self.getwords(letter, curr))
+                curr = curr[letter]
+            else:
+                break
+        while len(res) < len(searchWord):
+            res.append([])
+
 class Solution:
     def suggestedProducts(self, products, searchWord):
         trie = Trie()
