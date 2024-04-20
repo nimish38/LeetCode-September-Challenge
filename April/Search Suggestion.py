6559dd5f24f2cc504186bc2d426fe473adc409fb
Nimish <nimish.v.shinde60@gmail.com>
2024-04-20 16:11:35 +0530

    Using python bisection method

--- April/Search Suggestion.py
+++ April/Search Suggestion.py
@@ -1,47 +1,14 @@
-class Trie:
-    def __init__(self):
-        self.trie = {}
-
-    def insert(self, word):
-        curr = self.trie
-        for letter in word:
-            if letter not in curr:
-                curr[letter] = {}
-            curr = curr[letter]
-        curr['*'] = True
-
-    def getwords(self, prefix, let, curr):
-        stack, res = [(let, curr[let])], []
-        while stack and len(res) < 3:
-            item = stack.pop()
-            for char in sorted(item[1], reverse=True):
-                if char == '*':
-                    res.append(prefix + item[0])
-                else:
-                    stack.append((item[0] + char, item[1][char]))
-        if len(res) > 3:
-            return res[:3]
-        return res
-
-    def suggestions(self, searchWord):
-        res, prefix = [], ''
-        curr = self.trie
-        for letter in searchWord:
-            if letter in curr:
-                res.append(self.getwords(prefix, letter, curr))
-                curr = curr[letter]
-                prefix += letter
-            else:
-                break
-        while len(res) < len(searchWord):
-            res.append([])
-        return res
-
+import bisect
 class Solution:
-    def suggestedProducts(self, products, searchWord):
-        trie = Trie()
-        for word in products:
-            trie.insert(word)
-        return trie.suggestions(searchWord)
+    def suggestedProducts(self, products, searchWord: str):
+        products.sort()
+        res = []
+        prefix = ""
+        for c in searchWord:
+            prefix += c
+            index = bisect.bisect_left(products, prefix)
+            res.append([w for w in products[index: index + 3] if w.startswith(prefix)])
+        return res
+
 
 print(Solution().suggestedProducts(products = ["havana"], searchWord = "havana"))
