5d02661a57216dd49ae106d5279b0ca9f4255575
Nimish <nimish.v.shinde60@gmail.com>
2024-04-21 14:01:09 +0530

    Sorting interval with end time

--- April/Non Overlapping.py
+++ April/Non Overlapping.py
@@ -1,25 +1,17 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals):
-        if len(intervals) < 2:
-            return 0
-        intervals.sort()
-        i, j, cnt = 0, 1, 0
+        intervals.sort(key= lambda x: x[1] )
+        res = 0
+        end = intervals[0][1]
 
-        while j < len(intervals):
-            curr_start, curr_end, next_start, next_end = intervals[i][0], intervals[i][1], intervals[j][0], intervals[j][1]
-            if curr_end > next_start:
-                if curr_end > next_end:
-                    cnt += 1
-                    i = j
-                else:
-                    cnt += 1
+        for s, e in intervals[1:]:
+            if s < end:
+                res += 1
             else:
-                i = j
-            j += 1
+                end = e
+        return res
 
-        return cnt
-
-print(Solution().eraseOverlapIntervals(intervals = [[1,2],[2,3],[3,4],[1,3]]))
+print(Solution().eraseOverlapIntervals(intervals = [[2,12],[3,7],[9,12]]))
 
 
 
