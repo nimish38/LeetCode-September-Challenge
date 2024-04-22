57baeb7b7e06488f9496dd6ce6f1fd02516ff10d
Nimish <nimish.v.shinde60@gmail.com>
2024-04-22 22:08:52 +0530

    Sort on end cordinate

--- April/Minimum Arrows.py
+++ April/Minimum Arrows.py
@@ -2,18 +2,13 @@ class Solution:
     def findMinArrowShots(self, points):
         if len(points) < 2:
             return 1
-        points.sort()
-        j, cnt = 1, 0
-        start, end = points[0][0], points[0][1]
-        while j < len(points):
-            if points[j][0] <= end:
-                start = max(start, points[j][0])
-                end = min(end, points[j][1])
-                j += 1
-            else:
+        points.sort(key=lambda x: x[1])
+        cnt = 0
+        last_arrow = points[0][1]
+        for j in range(1, len(points)):
+            if points[j][0] > last_arrow:
                 cnt += 1
-                start, end = points[j][0], points[j][1]
-                j += 1
+                last_arrow = points[j][1]
         return cnt + 1
 
 print(Solution().findMinArrowShots(points = [[9,12],[1,10],[4,11],[8,12],[3,9],[6,9],[6,7]]))
