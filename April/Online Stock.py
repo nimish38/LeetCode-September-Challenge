bd07df12fe0d21e2654c940b24e8b9ad32837c41
Nimish <nimish.v.shinde60@gmail.com>
2024-04-24 20:34:09 +0530

    Brute force TLE

--- April/Online Stock.py
+++ April/Online Stock.py
@@ -4,20 +4,20 @@ class StockSpanner:
 
     def next(self, price: int) -> int:
         cnt, i = 1, len(self.stack) - 1
-        while i > 0 and self.stack[i] <= price:
+        while i >= 0 and self.stack[i] <= price:
             cnt += 1
             i -= 1
         self.stack.append(price)
         return cnt
 
 s = StockSpanner()
-print(s.next(100))
-print(s.next(80))
-print(s.next(60))
-print(s.next(70))
-print(s.next(60))
-print(s.next(75))
-print(s.next(85))
+print(s.next(31))
+print(s.next(41))
+print(s.next(48))
+print(s.next(59))
+print(s.next(79))
+# print(s.next(75))
+# print(s.next(85))
 
 
 
