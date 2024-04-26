7144f638cb063f5be4d91985795488027f2800c4
Nimish <nimish.v.shinde60@gmail.com>
2024-04-26 19:44:21 +0530

    single pointer solution

--- April/Remove Element.py
+++ April/Remove Element.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeElement(self, nums, val):
-        i, j, cnt = 0, len(nums) - 1, 0
-        while i <= j:
-            if nums[i] == val:
-                nums[i], nums[j] = nums[j], nums[i]
-                j -= 1
+        cnt = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[cnt] = nums[i]
                 cnt += 1
-            else:
-                i += 1
-        # print(nums)
-        return len(nums) - cnt
+        return cnt
+
 
 print(Solution().removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
