8949877682885880e9d4a47708fece29566f1252
Nimish <nimish.v.shinde60@gmail.com>
2024-04-26 19:29:24 +0530

    2 pointer approach with count

--- April/Remove Element.py
+++ April/Remove Element.py
@@ -2,13 +2,13 @@ class Solution:
     def removeElement(self, nums, val):
         i, j, cnt = 0, len(nums) - 1, 0
         while i <= j:
-            if i == val:
+            if nums[i] == val:
                 nums[i], nums[j] = nums[j], nums[i]
                 j -= 1
                 cnt += 1
             else:
                 i += 1
-        print(nums)
+        # print(nums)
         return len(nums) - cnt
 
-print(Solution().removeElement(nums = [3,2,2,3], val = 3))
+print(Solution().removeElement(nums = [0,1,2,2,3,0,4,2], val = 2))
