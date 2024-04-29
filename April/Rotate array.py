5fff47da2810a3dd124e5ac11d5660afbedd8ccc
Nimish <nimish.v.shinde60@gmail.com>
2024-04-29 22:14:11 +0530

    Array reversal 3 parts

--- April/Rotate array.py
+++ April/Rotate array.py
@@ -4,18 +4,17 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        if k > n:
-            k %= n
+        k %= n
 
         def reverse(low, high):
             while low < high:
                 nums[low], nums[high] = nums[high], nums[low]
                 low += 1
                 high -= 1
-
-        reverse(0, n - k -1)
-        reverse(n - k, n - 1)
-        reverse(0, n - 1)
+        if k > 0:
+            reverse(0, n - k -1)
+            reverse(n - k, n - 1)
+            reverse(0, n - 1)
         print(nums)
 
 Solution().rotate(nums = [1,2,3,4,5,6,7], k = 5)
