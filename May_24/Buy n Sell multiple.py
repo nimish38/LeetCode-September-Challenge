ad5263ca61541fb49d2649dba1e6b138a324f3f7
Nimish <nimish.v.shinde60@gmail.com>
2024-05-05 13:32:31 +0530

    little optimisation

--- May_24/Buy n Sell multiple.py
+++ May_24/Buy n Sell multiple.py
@@ -2,8 +2,8 @@ class Solution:
     def maxProfit(self, prices):
         if len(prices) == 1:
             return 0
-        buy, profit = float('inf'), 0
-        for i in range(len(prices) - 1):
+        buy, profit, i = prices[0], 0, 0
+        for i in range(1, len(prices) - 1):
             if buy == -1 or buy > prices[i]:
                 buy = prices[i]
             elif prices[i] > buy and prices[i + 1] < prices[i]:
