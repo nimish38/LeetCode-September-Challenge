eda619684d5822888fb85d1bf9bb22496faa6fcb
Nimish <nimish.v.shinde60@gmail.com>
2024-05-05 12:53:02 +0530

    checking neighbors

--- May_24/Buy n Sell multiple.py
+++ May_24/Buy n Sell multiple.py
@@ -1,14 +1,13 @@
 class Solution:
     def maxProfit(self, prices):
-        buy, bestbuy, bestsell, profit = -1, float('inf'), -1, 0
-        for price in prices:
-            if buy == -1 or buy > price:
-                buy = price
-                bestbuy = min(buy, bestbuy)
-            elif price - buy > 0:
-                profit += price - buy
+        buy, profit = float('inf'), 0
+        for i in range(len(prices) - 1):
+            if buy == -1 or buy > prices[i]:
+                buy = prices[i]
+            elif prices[i] > buy and prices[i + 1] < prices[i]:
+                profit += prices[i] - buy
                 buy = -1
-                bestsell = max(bestsell, price)
-        return max(profit, bestsell - bestbuy)
-
-print(Solution().maxProfit(prices = [7,1,5,6,9,4]))
+        if buy != -1:
+            profit = max(profit, prices[i + 1] - buy)
+        return profit
+print(Solution().maxProfit(prices = [7,6,4,3,1]))
