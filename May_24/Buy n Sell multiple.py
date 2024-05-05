f97f995a21250efb4dfd7b5ec42e70e4ec57cd8a
Nimish <nimish.v.shinde60@gmail.com>
2024-05-05 13:42:37 +0530

    single variable check

--- May_24/Buy n Sell multiple.py
+++ May_24/Buy n Sell multiple.py
@@ -1,15 +1,9 @@
 class Solution:
     def maxProfit(self, prices):
-        if len(prices) == 1:
-            return 0
-        buy, profit, i = prices[0], 0, 0
-        for i in range(1, len(prices) - 1):
-            if buy == -1 or buy > prices[i]:
-                buy = prices[i]
-            elif prices[i] > buy and prices[i + 1] < prices[i]:
+        buy, profit = prices[0], 0
+        for i in range(1, len(prices)):
+            if buy < prices[i]:
                 profit += prices[i] - buy
-                buy = -1
-        if buy != -1:
-            profit = max(profit, profit + prices[i + 1] - buy)
+            buy = prices[i]
         return profit
 print(Solution().maxProfit(prices = [6,1,3,2,4,7]))
