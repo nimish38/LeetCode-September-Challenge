4b8592b46fb5f4165bdad0a1b408fc452eef12ab
Nimish <nimish.v.shinde60@gmail.com>
2024-05-06 21:59:03 +0530

    correction

--- May_24/Jump Game.py
+++ May_24/Jump Game.py
@@ -1,15 +1,17 @@
 class Solution:
     def canJump(self, nums):
+        if len(nums) == 1:
+            return True
         stack = [(0, nums[0])]
         while stack:
             ind, num = stack.pop()
             for i in range(ind + 1, ind + num + 1):
-                if i >= len(nums):
+                if i >= len(nums) - 1:
                     return True
                 stack.append((i, nums[i]))
         if stack:
             return True
         return False
 
-print(Solution().canJump(nums = [3,2,1,0,4]))
+print(Solution().canJump(nums = [2,0,0]))
 
