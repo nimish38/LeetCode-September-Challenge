6eb1b39dd1cbe4877eda0e1a6e132789dd394881
Nimish <nimish.v.shinde60@gmail.com>
2024-05-07 21:07:40 +0530

    Reverse Traversal searching 0

--- May_24/Jump Game.py
+++ May_24/Jump Game.py
@@ -1,17 +1,23 @@
 class Solution:
     def canJump(self, nums):
-        if len(nums) == 1:
-            return True
-        stack = [(0, nums[0])]
-        while stack:
-            ind, num = stack.pop()
-            for i in range(ind + 1, ind + num + 1):
-                if i >= len(nums) - 1:
-                    return True
-                stack.append((i, nums[i]))
-        if stack:
-            return True
-        return False
+        zeroFlag, dist = False, 0
+        for i in range(len(nums) - 1, -1, -1):
+            if zeroFlag:
+                if nums[i] > dist:
+                    zeroFlag = False
+                    dist = 0
+                else:
+                    dist += 1
+            else:
+                if nums[i] == 0:
+                    zeroFlag = True
+                    if i == len(nums) - 1:
+                        dist = 0
+                    else:
+                        dist = 1
 
-print(Solution().canJump(nums = [2,0,0]))
+        if zeroFlag:
+            return False
+        return True
 
+print(Solution().canJump(nums = [2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6]))
