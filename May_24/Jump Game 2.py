5c88812fb5e1705429682a7f3d66960e20720009
Nimish <nimish.v.shinde60@gmail.com>
2024-05-08 20:51:43 +0530

    index checks

--- May_24/Jump Game 2.py
+++ May_24/Jump Game 2.py
@@ -4,14 +4,15 @@ class Solution:
             return 0
         i, cnt = 0, 0
         while i < len(nums) - 1:
-            maxval, j = -1, i + 1
-            while j < i + nums[i]:
+            maxval, maxind, j = -1, -1, i + 1
+            while j < i + nums[i] + 1:
                 if j >= len(nums) - 1:
-                    break
-                maxval = max(maxval, j)
+                    return cnt + 1
+                if nums[j] > maxval:
+                    maxval, maxind = nums[j], j
                 j += 1
-            i = j
+            i = maxind
             cnt += 1
         return cnt
 
-print(Solution().jump(nums = [2,3,1,1,4]))
+print(Solution().jump(nums = [2,3,0,1,4]))
