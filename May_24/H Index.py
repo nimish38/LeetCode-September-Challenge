2d1ce72651f8b4396e3f6258b812389c7419eddc
Nimish <nimish.v.shinde60@gmail.com>
2024-05-09 20:45:46 +0530

    Not required

--- May_24/H Index.py
+++ May_24/H Index.py
@@ -1,9 +1,12 @@
 class Solution:
     def hIndex(self, citations):
         citations.sort()
-        i, n = len(citations) - 1, len(citations)
-        while i >= 0 and citations[i] >= n - i:
-                i -= 1
-        return n - i - 1
+        n, cnt = len(citations), 0
+        for i in range(n-1, -1, -1):
+            if citations[i] >= n-i:
+                cnt = n - i
+            else:
+                break
+        return cnt
 
 print(Solution().hIndex(citations = [3,0,6,1,5]))
