6df2257e11302091f8e0359ccfd3c31cbeb31515
Nimish <nimish.v.shinde60@gmail.com>
2024-05-11 11:58:55 +0530

    concurrent left and right prod

---
+++ May_24/Product of Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        res, i = [1] * n, 1
+        left, right = nums[0], nums[-1]
+        while i < n:
+            res[i] *= left
+            res[n - i - 1] *= right
+            left *= nums[i]
+            right *= nums[n - i -1]
+            i += 1
+        return res
+
+print(Solution().productExceptSelf(nums = [1,2,3,4]))
+
