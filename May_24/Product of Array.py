d33475c519def29602e01500afe8872c4e3eb647
Nimish <nimish.v.shinde60@gmail.com>
2024-05-11 12:12:21 +0530

    Multiple zero check

--- May_24/Product of Array.py
+++ May_24/Product of Array.py
@@ -1,13 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums):
         n = len(nums)
+        zeros = nums.count(0)
+        if zeros > 1:
+            return [0] * n
         res, i = [1] * n, 1
         left, right = nums[0], nums[-1]
         while i < n:
             res[i] *= left
             res[n - i - 1] *= right
             left *= nums[i]
-            right *= nums[n - i -1]
+            right *= nums[n - i - 1]
             i += 1
         return res
 
