6e2aa1ea7bb80fb177fade2c318821135f9cd817
Nimish <nimish.v.shinde60@gmail.com>
2024-05-14 21:49:24 +0530

    2 variable 1 pass

--- May_24/Candy.py
+++ May_24/Candy.py
@@ -1,14 +1,31 @@
 class Solution:
     def candy(self, ratings):
         n = len(ratings)
-        L2R = [1] * n
-        # check left neightbor
-        for i in range(1, n):
-            if ratings[i] > ratings[i - 1]:
-                L2R[i] = max(L2R[i], L2R[i - 1] + 1)
-        # check right neighbor
-            if ratings[n - i - 1] > ratings[n - i]:
-                L2R[n - i - 1] = max(L2R[n - i - 1], L2R[n - i] + 1)
-        return sum(L2R)
+        candies = n
+
+        i = 1;
+        while i < n:
+            if ratings[i] == ratings[i - 1]:
+                i += 1
+                continue
+
+            # // increasing slope
+            peak = 0
+            while ratings[i] > ratings[i - 1]:
+                peak += 1
+                candies += peak
+                i += 1
+                if i == n:
+                    return candies
+
+            # // decreasing slope
+            dip = 0
+            while i < n and ratings[i] < ratings[i - 1]:
+                dip += 1
+                candies += dip
+                i += 1
+
+            candies -= min(peak, dip)
+        return candies
 
 print(Solution().candy(ratings = [1,3,4,5,2]))
