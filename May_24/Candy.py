3940203ea8c6bdb31c996dbba2b13be71b4a82c7
Nimish <nimish.v.shinde60@gmail.com>
2024-05-14 20:58:01 +0530

    greedy solution with single array

--- May_24/Candy.py
+++ May_24/Candy.py
@@ -1,7 +1,7 @@
 class Solution:
     def candy(self, ratings):
         n = len(ratings)
-        L2R, R2L = [1] * n, [1] * n
+        L2R = [1] * n
         # check left neightbor
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
@@ -9,10 +9,7 @@ class Solution:
         # check right neighbor
         for i in range(n - 2, -1, -1):
             if ratings[i] > ratings[i + 1]:
-                R2L[i] = R2L[i + 1] + 1
-        cnt = 0
-        for i in range(n):
-            cnt += max(L2R[i], R2L[i])
-        return cnt
+                L2R[i] = max(L2R[i], L2R[i + 1] + 1)
+        return sum(L2R)
 
-print(Solution().candy(ratings = [1,0,2]))
+print(Solution().candy(ratings = [1,3,4,5,2]))
