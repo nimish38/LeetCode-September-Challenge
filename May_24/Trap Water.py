8000cf18dfc8ec79bb18c2036bb4b44bf580d812
Nimish <nimish.v.shinde60@gmail.com>
2024-05-15 20:52:20 +0530

    no need to add > 0 check

--- May_24/Trap Water.py
+++ May_24/Trap Water.py
@@ -4,7 +4,7 @@ class Solution:
         left_max, right_max = [], []
 
         for i in range(1, n - 1):
-            left_max.append(max(height[:i]))
+            left_max.append(max(height[:i + 1]))
             right_max.append(max(height[i:]))
 
         left_max.insert(0, height[0])
@@ -15,9 +15,8 @@ class Solution:
 
         water = 0
         for i in range(n):
-            cnt = min(left_max[i], right_max[i]) - height[i]
-            if cnt > 0:
-                water += cnt
+            water += min(left_max[i], right_max[i]) - height[i]
+
         return water
 
-print(Solution().trap(height = [4,2,0,3,2,5]))
+print(Solution().trap(height = [0,1,0,2,1,0,1,3,2,1,2,1]))
