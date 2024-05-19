6c2e4c4b4eaa1dc9b5a07fbe2062769a0d8f16d7
Nimish <nimish.v.shinde60@gmail.com>
2024-05-19 12:30:30 +0530

    Reverse traversal

--- May_24/Reverse words.py
+++ May_24/Reverse words.py
@@ -1,5 +1,19 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return ' '.join(s.split()[::-1])
+        res, i, curr = '', len(s) - 1, ''
+
+        while i >= 0:
+            if s[i] == ' ':
+                if curr:
+                    res += ' ' + curr
+                    curr = ''
+                else:
+                    continue
+            else:
+                curr = s[i] + curr
+            i -= 1
+
+        if curr:
+            res += ' ' + curr
 
 print(Solution().reverseWords(s = "the sky is blue"))
