a79226731dd42529bc37ed487fb6e088675d54fb
Nimish <nimish.v.shinde60@gmail.com>
2024-05-21 21:41:08 +0530

    bug fix

--- May_24/Find Index.py
+++ May_24/Find Index.py
@@ -1,10 +1,15 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n = len(needle) - 1
+        n = len(needle)
+
+        if n > len(haystack):
+            return -1
 
         for i in range(len(haystack)):
             if haystack[i] == needle[0]:
+                # print(haystack[i + 1: i + n], needle[1:])
                 if haystack[i + 1: i + n] == needle[1:]:
                     return i
-
         return -1
+
+print(Solution().strStr(haystack = "sadbutsad", needle = "sad"))
