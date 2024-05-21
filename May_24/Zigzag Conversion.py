85c9b59bb331a1de5e9de88809393d371746158d
Nimish <nimish.v.shinde60@gmail.com>
2024-05-21 21:21:31 +0530

    corner condition

--- May_24/Zigzag Conversion.py
+++ May_24/Zigzag Conversion.py
@@ -1,5 +1,7 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
         zigzag = [''] * numRows
 
         inc, cnt = True, 0
