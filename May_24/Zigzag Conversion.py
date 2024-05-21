7a2d60fb398b10a27f6ebe75d366ca89ed54b131
Nimish <nimish.v.shinde60@gmail.com>
2024-05-21 21:10:35 +0530

    mutidimensional array to linear array space

--- May_24/Zigzag Conversion.py
+++ May_24/Zigzag Conversion.py
@@ -1,27 +1,22 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        zigzag, n = [], len(s) // 2
-        for _ in range(numRows):
-            zigzag.append([''] * n)
+        zigzag = [''] * numRows
 
         inc, cnt = True, 0
         for char in s:
             if inc:
-                zigzag[cnt].append(char)
+                zigzag[cnt] += char
                 cnt += 1
                 if cnt == numRows:
                     inc = False
                     cnt -= 2
             else:
-                zigzag[cnt].append(char)
+                zigzag[cnt] += char
                 cnt -= 1
                 if cnt < 0:
                     inc = True
                     cnt += 2
 
-        res = ''
-        for i in range(numRows):
-            res += ''.join(zigzag[i])
-        return res
+        return ''.join(zigzag)
 
-print(Solution().convert(s = "A", numRows = 1))
+print(Solution().convert(s = "PAYPALISHIRING", numRows = 4))
