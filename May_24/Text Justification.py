53b305fe9531bb69702674926ba710ce89ea04b2
Nimish <nimish.v.shinde60@gmail.com>
2024-05-23 16:46:04 +0530

    Left margin spaces corrected

--- May_24/Text Justification.py
+++ May_24/Text Justification.py
@@ -3,7 +3,7 @@ class Solution:
         i, res = 0, []
         while i < len(words) :
             lettercount, curr, selectedWords = 0, '', []
-            while i < len(words) and lettercount + len(words[i]) < maxWidth:
+            while i < len(words) and lettercount + len(words[i]) <= maxWidth:
                 lettercount += len(words[i]) + 1
                 selectedWords.append(words[i])
                 i += 1
@@ -14,19 +14,21 @@ class Solution:
                 if i == len(words):
                     for k in selectedWords:
                         curr += k + ' '
-                    curr += (' ' * (maxWidth - lettercount))
+                    curr += (' ' * (maxWidth - lettercount - 1))
                 else:
+                    lettercount -= len(selectedWords) - 1
                     extras = maxWidth - lettercount
                     additional = extras // (len(selectedWords) - 1)
                     left_margin = extras % (len(selectedWords) - 1)
 
-                    curr += selectedWords[0] + ' ' + (' ' * (additional + left_margin))
-                    for j in range(1, len(selectedWords) - 1):
-                        curr += selectedWords[j] + ' ' + (' ' * additional)
+                    for j in range(0, len(selectedWords) - 1):
+                        curr += selectedWords[j] + (' ' * additional)
+                        if j < left_margin:
+                            curr += ' '
                     curr += selectedWords[-1]
             res.append(curr)
 
         return res
 
-print(Solution().fullJustify(words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16))
-print(["What   must   be","acknowledgment  ","shall be        "])
+print(Solution().fullJustify(words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20))
+print(["Science  is  what we","understand      well","enough to explain to","a  computer.  Art is","everything  else  we","do                  "])
