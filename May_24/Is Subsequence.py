d9d8d42f3966548c67e456ef3365cdd93baefc10
Nimish <nimish.v.shinde60@gmail.com>
2024-05-25 13:23:02 +0530

    Corner case

--- May_24/Is Subsequence.py
+++ May_24/Is Subsequence.py
@@ -1,10 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        m, n = len(s), len(t)
+        if m > n:
+            return False
         i, j = 0, 0
-        while i < len(s) and j < len(t):
+        while i < m and j < n:
             if s[i] == t[j]:
                 i += 1
             j += 1
-        return i == len(s)
+        return i == m
 
 print(Solution().isSubsequence(s = "axc", t = "ahbgdc"))
