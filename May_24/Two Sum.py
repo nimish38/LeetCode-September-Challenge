aa63f3ffa58c1fa117546ab46755e54491cd103f
Nimish <nimish.v.shinde60@gmail.com>
2024-05-26 12:55:46 +0530

    two pointer search

--- May_24/Two Sum.py
+++ May_24/Two Sum.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, numbers, target):
         i, j = 0, len(numbers) - 1
         while i < j:
-            curr = numbers[i] + numbers[j]
-            if curr == target:
+            if numbers[i] + numbers[j] == target:
                 return [i + 1, j + 1]
-            elif curr < target:
+            elif numbers[i] + numbers[j] < target:
                 i += 1
             else:
                 j -= 1
