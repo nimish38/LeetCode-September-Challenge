5395cefc6ccab962fb96b006c517d637d67c12ce
Nimish <nimish.v.shinde60@gmail.com>
2024-05-27 22:30:38 +0530

    optimised

--- May_24/Container with most water.py
+++ May_24/Container with most water.py
@@ -2,13 +2,15 @@ class Solution:
     def maxArea(self, height):
         i, j, area = 0, len(height) - 1, 0
         while i < j:
-            curr = min(height[i], height[j]) * (j - i)
-            if curr > area:
-                area = curr
-
             if height[i] < height[j]:
+                curr = height[i] * (j - i)
+                if curr > area:
+                    area = curr
                 i += 1
             else:
+                curr = height[j] * (j - i)
+                if curr > area:
+                    area = curr
                 j -= 1
         return area
 
