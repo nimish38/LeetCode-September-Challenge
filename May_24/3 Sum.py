7160b79a1f4fa878fe4f693b61384c05085eadc9
Nimish <nimish.v.shinde60@gmail.com>
2024-05-28 22:10:58 +0530

    sorting and 2 Sum with a loop

--- May_24/3 Sum.py
+++ May_24/3 Sum.py
@@ -1,11 +1,19 @@
 class Solution:
     def threeSum(self, nums):
         res, n = [], len(nums)
-        for i in range(len(nums) - 2):
+        nums.sort()
+        for i in range(n - 2):
             j, k = i + 1, n - 1
             while j < k:
-                if nums[j] + nums[k] + nums[i] == 0:
-                    res.append([i, j, k])
+                curr = nums[j] + nums[k] + nums[i]
+                if curr == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                elif curr < 0:
+                    j += 1
+                else:
+                    k -= 1
         return res
 
 print(Solution().threeSum(nums = [-1,0,1,2,-1,-4]))
