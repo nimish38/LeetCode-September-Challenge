8c8a1c412670ca68ee4c0c54e3c0275ed79d9aba
Nimish <nimish.v.shinde60@gmail.com>
2024-05-29 17:27:04 +0530

    move ahead if duplicate

--- May_24/3 Sum.py
+++ May_24/3 Sum.py
@@ -3,12 +3,17 @@ class Solution:
         res, n = [], len(nums)
         nums.sort()
         for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
             j, k = i + 1, n - 1
             while j < k:
                 curr = nums[j] + nums[k] + nums[i]
                 if curr == 0:
-                    if [nums[i], nums[j], nums[k]] not in res:
-                        res.append([nums[i], nums[j], nums[k]])
+                    while nums[j] == nums[j + 1]:
+                        j += 1
+                    while nums[k] == nums[k - 1]:
+                        k -= 1
+                    res.append([nums[i], nums[j], nums[k]])
                     j += 1
                     k -= 1
                 elif curr < 0:
