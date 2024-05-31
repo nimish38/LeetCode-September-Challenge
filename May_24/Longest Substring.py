dfa37719a7cbe682c3f46d982a260accdc70abfb
Nimish <nimish.v.shinde60@gmail.com>
2024-05-31 19:51:45 +0530

    remove from set until duplicate

--- May_24/Longest Substring.py
+++ May_24/Longest Substring.py
@@ -8,11 +8,11 @@ class Solution:
             if s[j] not in curr:
                 curr.add(s[j])
             else:
-                curr.remove(s[i])
-                curr.add(s[j])
-                i += 1
+                while s[i] != s[j]:
+                    curr.remove(s[i])
+                    i += 1
             res = max(res, len(curr))
             j += 1
         return res
 
-print(Solution().lengthOfLongestSubstring(s = ""))
+print(Solution().lengthOfLongestSubstring(s = "qrsvbspk"))
