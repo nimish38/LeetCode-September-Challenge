ecd24904dbdc4365c2721060e7a8f0f4df749b2a
Nimish <nimish.v.shinde60@gmail.com>
2024-06-01 13:23:05 +0530

    creating combinations

--- June_24/Substring with concat.py
+++ June_24/Substring with concat.py
@@ -1,12 +1,17 @@
 import itertools
 class Solution:
     def findSubstring(self, s: str, words):
-        perms = list(itertools.permutations(words))
+        combo = list(itertools.permutations(words))
+        perms = []
+        for lis in combo:
+            perms.append(''.join(lis))
+
         i, n, res = 0, len(perms[0]), []
         while i < len(s) - n + 1:
+            # print(s[i: i + n])
             if s[i: i + n] in perms:
                 res.append(i)
             i += 1
         return res
 
-print(Solution().findSubstring())
+print(Solution().findSubstring(s = "barfoothefoobar", words = ["foo","bar"]))
