384168165fbedaf924192046b5296c084262a05f
Nimish <nimish.v.shinde60@gmail.com>
2024-06-04 22:43:13 +0530

    using deafultdict with set

--- June_24/Is Valid Sudoku.py
+++ June_24/Is Valid Sudoku.py
@@ -1,34 +1,30 @@
+from collections import defaultdict
 class Solution:
-    def isValidSudoku(self, board):
-        row_set, col_set, box_set = set(), set(), set()
+    def isValidSudoku(board) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        grid = defaultdict(set)
         for i in range(9):
             for j in range(9):
-                if board[i][j] != '.':
-                    x = str(i) + '_ROW_' + str(board[i][j])
-                    if x in row_set:
+                val = board[i][j]
+                if val.isalnum():
+                    k = i // 3 * 3 + j // 3
+                    if val in row[i] or val in col[j] or val in grid[k]:
                         return False
-                    row_set.add(x)
+                    else:
+                        row[i].add(val)
+                        col[j].add(val)
+                        grid[k].add(val)
 
-                    y = str(j) + '_COL_' + str(board[i][j])
-                    if y in col_set:
-                        return False
-                    col_set.add(y)
-
-                    z = str(i//3) + str(j//3) + '_BOX_' + str(board[i][j])
-                    if z in box_set:
-                        return False
-                    box_set.add(z)
         return True
 
-
-
 print(Solution().isValidSudoku(board =
-[[".",".",".",".","5",".",".","1","."],
- [".","4",".","3",".",".",".",".","."],
- [".",".",".",".",".","3",".",".","1"],
- ["8",".",".",".",".",".",".","2","."],
- [".",".","2",".","7",".",".",".","."],
- [".","1","5",".",".",".",".",".","."],
- [".",".",".",".",".","2",".",".","."],
- [".","2",".","9",".",".",".",".","."],
- [".",".","4",".",".",".",".",".","."]]))
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
