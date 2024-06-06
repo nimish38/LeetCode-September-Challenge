24f9b77ab80732837f0035d6a05274175f7acc9c
Nimish <nimish.v.shinde60@gmail.com>
2024-06-06 22:01:49 +0530

    Creating transpose of matrix

--- June_24/Rotate Image.py
+++ June_24/Rotate Image.py
@@ -1,15 +1,10 @@
 class Solution:
     def rotate(self, matrix):
-        temp, n = [], len(matrix)
-        for j in range(n):
-            x = []
-            for i in range(n - 1, -1, -1):
-                x.append(matrix[i][j])
-            temp.append(x)
-
+        n = len(matrix)
         for i in range(n):
-            for j in range(n):
-                matrix[i][j] = temp[i][j]
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
         print(matrix)
 
 Solution().rotate(matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
