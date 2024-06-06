916689e655a311b15889fff53c6031bcaaa5d70d
Nimish <nimish.v.shinde60@gmail.com>
2024-06-06 22:03:51 +0530

    reversing rows

--- June_24/Rotate Image.py
+++ June_24/Rotate Image.py
@@ -5,6 +5,9 @@ class Solution:
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
+        for i in range(n):
+            matrix[i].reverse()
+
         print(matrix)
 
 Solution().rotate(matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])
