b9fca97dd2b75490a80b4a6381fe1d97b4522482
Nimish <nimish.v.shinde60@gmail.com>
2024-06-07 21:09:33 +0530

    Storing zeroes in first row n col

--- June_24/Set Matrix zeroes.py
+++ June_24/Set Matrix zeroes.py
@@ -6,15 +6,19 @@ class Solution:
                 if matrix[i][j] == 0:
                     matrix[i][0] = matrix[0][j] = 0
 
-        for r in range(m):
-            if matrix[r][0] == 0:
-                matrix[r] = [0] * n
-
-        for c in range(n):
+        for c in range(1, n):
             if matrix[0][c] == 0:
                 for i in range(1, m):
                     matrix[i][c] = 0
 
+        for r in range(1, m):
+            if matrix[r][0] == 0:
+                matrix[r] = [0] * n
+
+        if matrix[0][0] == 0:
+            matrix[0] = [0] * n
+            for i in range(m):
+                matrix[i][0] = 0
         print(matrix)
 
 Solution().setZeroes(matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]])
