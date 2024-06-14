459383c1724e501f0ae0d7000ffcc7dcde0e1f87
Nimish <nimish.v.shinde60@gmail.com>
2024-06-14 19:21:21 +0530

    Store index in map

--- June_24/Two Sum.py
+++ June_24/Two Sum.py
@@ -1,13 +1,23 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums, target: int):
         i, j =  0, len(nums) - 1
+        ind = defaultdict(list)
+
+        for k in range(len(nums)):
+            ind[nums[k]].append(k)
         nums.sort()
+
         while i < j:
             if nums[i] + nums[j] < target:
                 i += 1
             elif nums[i] + nums[j] > target:
                 j -= 1
             else:
-                return [i, j]
+                break
 
-print(Solution().twoSum(nums = [2,7,11,15], target = 13))
+        if nums[i] == nums[j]:
+            return ind[nums[i]]
+        return [ind[nums[i]][0], ind[nums[j]][0]]
+
+print(Solution().twoSum(nums = [3,2,3], target = 6))
