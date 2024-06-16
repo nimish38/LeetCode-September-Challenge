9a9656b872e1666eb3089538380b029ed3a1613a
Nimish <nimish.v.shinde60@gmail.com>
2024-06-16 13:01:30 +0530

    Dictionary Solution with count updated

--- June_24/Longest Consecutive Seq.py
+++ June_24/Longest Consecutive Seq.py
@@ -2,15 +2,25 @@ class Solution:
     def longestConsecutive(self, nums) -> int:
         if len(nums) == 0:
             return 0
-        nums = list(set(nums))
-        nums.sort()
-        cnt, res = 1, -1
-        for i in range(1, len(nums)):
-            if nums[i] - nums[i - 1] == 1:
-                cnt += 1
-            else:
-                res = max(res, cnt)
-                cnt = 1
-        return max(res, cnt)
+        val, res = {}, -1
+        for num in nums:
+            if num in val:
+                continue
+            lcont, rcont = 0, 0
+            if num - 1 in val:
+                lcont = val[num - 1]
+            if num + 1 in val:
+                rcont = val[num + 1]
+            val[num] = lcont + rcont + 1
+            if lcont:
+                while lcont > 0:
+                    val[num - lcont] = val[num]
+                    lcont -= 1
+            if rcont:
+                while rcont:
+                    val[num + rcont] = val[num]
+                    rcont -= 1
+            res = max(res, val[num])
+        return res
 
 print(Solution().longestConsecutive(nums = [0,3,7,2,5,8,4,6,0,1]))
