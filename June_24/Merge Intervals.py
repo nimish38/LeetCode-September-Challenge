ebcd99502311c3aeb5cce22532057a0e63a2cda6
Nimish <nimish.v.shinde60@gmail.com>
2024-06-18 22:05:47 +0530

    sort array with second value and then by first

--- June_24/Merge Intervals.py
+++ June_24/Merge Intervals.py
@@ -1,16 +1,18 @@
+import operator
 class Solution:
     def merge(self, intervals):
         if len(intervals) < 2:
             return intervals
-        intervals.sort(key=lambda x: x[1])
+
+        intervals = sorted(intervals, key=operator.itemgetter(1, 0))
         res, i, j = [], 0, 1
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:
                 j += 1
             else:
-                res.append([intervals[i][0], intervals[j - 1][1]])
+                res.append([min(intervals[i][0], intervals[j - 1][0]), intervals[j - 1][1]])
                 i, j = j, j + 1
-        res.append([intervals[i][0], intervals[j - 1][1]])
+        res.append([min(intervals[i][0],intervals[j - 1][0]), intervals[j - 1][1]])
         return res
 
-print(Solution().merge(intervals = [[1,3],[8,10],[2,6],[15,18]]))
+print(Solution().merge(intervals = [[1,4],[2,3]]))
