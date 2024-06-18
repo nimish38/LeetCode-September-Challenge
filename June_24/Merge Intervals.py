3d7a22dbbef9b3a5e3985c05abfe4a1b761e8a37
Nimish <nimish.v.shinde60@gmail.com>
2024-06-18 21:50:14 +0530

    sort array with second value

--- June_24/Merge Intervals.py
+++ June_24/Merge Intervals.py
@@ -2,6 +2,7 @@ class Solution:
     def merge(self, intervals):
         if len(intervals) < 2:
             return intervals
+        intervals.sort(key=lambda x: x[1])
         res, i, j = [], 0, 1
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:
@@ -11,3 +12,5 @@ class Solution:
                 i, j = j, j + 1
         res.append([intervals[i][0], intervals[j - 1][1]])
         return res
+
+print(Solution().merge(intervals = [[1,3],[8,10],[2,6],[15,18]]))
