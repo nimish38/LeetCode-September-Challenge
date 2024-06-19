a862405ef6c3d704d9f9c94ae2b8a8ab5b39d6a4
Nimish <nimish.v.shinde60@gmail.com>
2024-06-19 21:23:27 +0530

    calculate end and start

--- June_24/Merge Intervals.py
+++ June_24/Merge Intervals.py
@@ -4,17 +4,17 @@ class Solution:
         if len(intervals) < 2:
             return intervals
 
-        intervals = sorted(intervals, key=operator.itemgetter(1, 0))
-        res, i, j, start = [], 0, 1, intervals[0][0]
+        intervals = sorted(intervals, key=lambda x: x[0])
+        res, i, j, start, end = [], 0, 1,intervals[0][0], intervals[0][1]
         while j < len(intervals):
-            if intervals[j][0] <= intervals[i][1]:
-                start = min(start, intervals[j][0])
+            if intervals[j][0] <= end:
+                end = max(end, intervals[j][1])
                 i += 1
                 j += 1
             else:
-                res.append([start, intervals[j - 1][1]])
-                start, i, j = intervals[j][0], j, j + 1
-        res.append([start, intervals[j - 1][1]])
+                res.append([start, end])
+                start, end, i, j = intervals[j][0], intervals[j][1], j, j + 1
+        res.append([start, end])
         return res
 
-print(Solution().merge(intervals = [[1,4],[2,3]]))
+print(Solution().merge(intervals = [[2,3],[4,5],[6,7],[8,9],[1,10]]))
