a8c17c898ec5d6142afad702904e2882ec061b04
Nimish <nimish.v.shinde60@gmail.com>
2024-06-19 20:50:43 +0530

    handle multiple intervals merge

--- June_24/Merge Intervals.py
+++ June_24/Merge Intervals.py
@@ -1,18 +1,18 @@
-import operator
 class Solution:
     def merge(self, intervals):
         if len(intervals) < 2:
             return intervals
 
-        intervals = sorted(intervals, key=operator.itemgetter(1, 0))
-        res, i, j = [], 0, 1
+        intervals = sorted(intervals, key=lambda x: x[1])
+        res, i, j, start = [], 0, 1, intervals[0][0]
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:
+                i += 1
                 j += 1
             else:
-                res.append([min(intervals[i][0], intervals[j - 1][0]), intervals[j - 1][1]])
-                i, j = j, j + 1
-        res.append([min(intervals[i][0],intervals[j - 1][0]), intervals[j - 1][1]])
+                res.append([start, intervals[j - 1][1]])
+                start, i, j = intervals[j][0], j, j + 1
+        res.append([start, intervals[j - 1][1]])
         return res
 
-print(Solution().merge(intervals = [[1,4],[2,3]]))
+print(Solution().merge(intervals = [[1,3],[2,6],[8,10],[10,18], [11,21]]))
