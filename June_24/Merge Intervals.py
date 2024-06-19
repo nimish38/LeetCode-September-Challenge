8f97e3c4d9852f20fd5b205ced1a290a71609dc8
Nimish <nimish.v.shinde60@gmail.com>
2024-06-19 21:01:13 +0530

    check min start

--- June_24/Merge Intervals.py
+++ June_24/Merge Intervals.py
@@ -1,12 +1,14 @@
+import operator
 class Solution:
     def merge(self, intervals):
         if len(intervals) < 2:
             return intervals
 
-        intervals = sorted(intervals, key=lambda x: x[1])
+        intervals = sorted(intervals, key=operator.itemgetter(1, 0))
         res, i, j, start = [], 0, 1, intervals[0][0]
         while j < len(intervals):
             if intervals[j][0] <= intervals[i][1]:
+                start = min(start, intervals[j][0])
                 i += 1
                 j += 1
             else:
@@ -15,4 +17,4 @@ class Solution:
         res.append([start, intervals[j - 1][1]])
         return res
 
-print(Solution().merge(intervals = [[1,3],[2,6],[8,10],[10,18], [11,21]]))
+print(Solution().merge(intervals = [[1,4],[2,3]]))
