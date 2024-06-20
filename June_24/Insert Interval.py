4cc113ba1f93bcee7ed9d7248681596407bc2d82
Nimish <nimish.v.shinde60@gmail.com>
2024-06-20 17:15:30 +0530

    empty and full interval check

--- June_24/Insert Interval.py
+++ June_24/Insert Interval.py
@@ -1,5 +1,7 @@
 class Solution:
     def insert(self, intervals, newInterval):
+        if not intervals:
+            return [newInterval]
         res, start, end, flag = [], newInterval[0], newInterval[1], False
         for int in intervals:
             if not flag:
@@ -13,6 +15,8 @@ class Solution:
                     res[-1][1] = int[1]
                 elif end < int[0]:
                     res.append(int)
+        if not flag:
+            res.append(newInterval)
         return res
 
 print(Solution().insert(intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]))
