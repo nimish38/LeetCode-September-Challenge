c17bcd3080b2d38940ff17233ca174e41bc934b4
Nimish <nimish.v.shinde60@gmail.com>
2024-06-21 21:14:15 +0530

    Sort and find overlapping intervals

---
+++ June_24/Arrows to burst balloons.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMinArrowShots(self, points):
+        points.sort()
+        res, start, end = 0, points[0][0], points[0][1]
+        for pt in points:
+            if start <= pt[0] <= end or start <= pt[1] <= end:
+                start, end = max(start, pt[0]), min(pt[1], end)
+            else:
+                res, start, end = res + 1, pt[0], pt[1]
+        return res + 1
+
+print(Solution().findMinArrowShots(points = [[10,16],[2,8],[1,6],[7,12]]))
