7a0ee4f6ae0f50b41afadf08b9d6232bfebb7d1b
Nimish <nimish.v.shinde60@gmail.com>
2024-06-22 13:42:42 +0530

    empty stack check

--- June_24/Valid Parenthesis.py
+++ June_24/Valid Parenthesis.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        match = {']': '[', '(': ')', '{': '}'}
+        match = {']': '[', '}': '{', ')': '('}
         if s[0] in match:
             return False
         st = [s[0]]
         for i in range(1, len(s)):
             if s[i] in match:
-                if st[-1] != match[s[i]]:
+                if not st or st[-1] != match[s[i]]:
                     return False
                 st.pop()
             else:
                 st.append(s[i])
         return len(st) == 0
 
-print(Solution().isValid())
+print(Solution().isValid(s = "()[]{}}"))
