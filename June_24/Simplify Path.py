3ffb37853211747b66dfee9b48ae7951f41a4d24
Nimish <nimish.v.shinde60@gmail.com>
2024-06-23 13:06:57 +0530

    split and check

---
+++ June_24/Simplify Path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        st = []
+        for dir in path.split('/'):
+            if dir == '..':
+                if st:
+                    st.pop()
+            else:
+                st.append(dir)
+
+        return '/' + '/'.join(st)
+
+print(Solution().simplifyPath("/home/"))
