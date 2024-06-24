872175ababfc164b087d2ee53baede5a3997d025
Nimish <nimish.v.shinde60@gmail.com>
2024-06-24 21:33:59 +0530

    use builtin method for minimum

---
+++ June_24/Min Stack.py
@@ -0,0 +1,25 @@
+class MinStack:
+    def __init__(self):
+        self.st = []
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+
+    def pop(self) -> None:
+        self.st.pop()
+
+    def top(self) -> int:
+        return self.st[-1]
+
+    def getMin(self) -> int:
+        return min(self.st)
+
+
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin()) # return -3
+minStack.pop()
+print(minStack.top())   #// return 0
+print(minStack.getMin()) #// return -2
