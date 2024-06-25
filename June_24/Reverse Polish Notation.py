04f1f0411de9c7b5371970c7460cb93eb94fc231
Nimish <nimish.v.shinde60@gmail.com>
2024-06-25 22:13:55 +0530

    division with negative num fix

--- June_24/Reverse Polish Notation.py
+++ June_24/Reverse Polish Notation.py
@@ -1,13 +1,17 @@
 import operator
 class Solution:
     def evalRPN(self, tokens) -> int:
+        print(12 // -5)
         st, operators = [], {'+': operator.add, '-': operator.sub, '/': operator.floordiv, '*': operator.mul}
         for tok in tokens:
             if tok not in operators:
                 st.append(int(tok))
             else:
                 op2, op1 = st.pop(), st.pop()
-                st.append(operators[tok](op1, op2))
+                mul = 1
+                if tok == '/' and ((op1 < 0 and op2 > 0) or (op2 < 0 and op1 > 0)):
+                    mul = -1
+                st.append(mul * operators[tok](abs(op1), abs(op2)))
         return st[0]
 
-print(Solution().evalRPN(tokens = ["4","13","5","/","+"]))
+print(Solution().evalRPN(tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
