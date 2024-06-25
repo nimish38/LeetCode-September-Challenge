51ce44c1a2ccb765c7065df895e8bbe25c8e2ac0
Nimish <nimish.v.shinde60@gmail.com>
2024-06-25 21:52:41 +0530

    save function reference and call them when needed

--- June_24/Reverse Polish Notation.py
+++ June_24/Reverse Polish Notation.py
@@ -1,13 +1,13 @@
 import operator
 class Solution:
     def evalRPN(self, tokens) -> int:
-        st, operators = [], {'+': operator.add(), '-': operator.sub(), '/': operator.floordiv(), '*': operator.mul()}
+        st, operators = [], {'+': operator.add, '-': operator.sub, '/': operator.floordiv, '*': operator.mul}
         for tok in tokens:
             if tok not in operators:
                 st.append(int(tok))
             else:
                 op2, op1 = st.pop(), st.pop()
-                st.push(operators[tok](op1, op2))
+                st.append(operators[tok](op1, op2))
         return st[0]
 
-print(Solution().evalRPN())
+print(Solution().evalRPN(tokens = ["4","13","5","/","+"]))
