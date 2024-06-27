7738140637694d26036f61a497431e37d3ae5558
Nimish <nimish.v.shinde60@gmail.com>
2024-06-27 21:19:40 +0530

    handle negative at bracket case

--- June_24/Basic Calculator.py
+++ June_24/Basic Calculator.py
@@ -1,7 +1,7 @@
 import operator
 class Solution:
     def calculate(self, s: str) -> int:
-        st, operators, neg = [], {'+': operator.add, '-': operator.sub}, False
+        st, operators, neg, start = [], {'+': operator.add, '-': operator.sub}, False, 1
         for char in s:
             if char == ' ':
                 continue
@@ -12,15 +12,22 @@ class Solution:
                     st.append(char)
             elif char == ')':
                 emt = []
-                while st[-1] != '(':
+                while st[-1] != '(' and st[-1] != '-(':
                     emt.append(st.pop())
                 while len(emt) > 1:
                     op2, op, op1 = emt.pop(), emt.pop(), emt.pop()
                     emt.append(operators[op](op1, op2))
-                st.pop()
-                st.append(emt[0])
+                minus = st.pop()
+                if minus == '-(':
+                    st.append(-1 * emt[0])
+                else:
+                    st.append(emt[0])
             elif char == '(':
-                st.append(char)
+                if neg:
+                    st.append('-(')
+                    neg = False
+                else:
+                    st.append(char)
             else:
                 if neg:
                     st.append(-1 * int(char))
@@ -41,5 +48,5 @@ class Solution:
 
         return st[0]
 
-print(Solution().calculate(s = "-2+ 1"))
+print(Solution().calculate(s = "- (3 + (4 + 5))"))
 
