ff5fe009a777bd2494e44d6db717fe627fe122ff
Nimish <nimish.v.shinde60@gmail.com>
2024-07-04 20:39:08 +0530

    Fix next pointer

--- July_24/Copy Linked List.py
+++ July_24/Copy Linked List.py
@@ -4,28 +4,31 @@ class Node:
         self.next = next
         self.random = random
 
+
 class Solution:
     def copyRandomList(self, head):
         if not head: return None
-        links1, links2, i, prev = {}, {}, 0, None
+        links1, links2, i = {}, {}, 0
         while head:
             links1[head] = i
             new_node = Node(head.val)
             links2[i] = new_node
             head = head.next
             i += 1
-            if prev:
-                prev.next = new_node
-            else:
-                prev = new_node
 
         for ele in links1:
+            # print(ele.val, '-> ', links1[ele])
+            if ele.next:
+                index, target_index = links1[ele], links1[ele.next]
+                links2[index].next = links2[target_index]
             if ele.random:
                 index, target_index = links1[ele], links1[ele.random]
                 links2[index].random = links2[target_index]
             else:
                 links2[links1[ele]].random = None
 
+        # for i in links2:
+        #     print(links2[i].val ,'->', links2[i].next.val)
         return links2[0]
 
 
