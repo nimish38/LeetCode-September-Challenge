bafbd38130835677a29bfaff27bdfc25cea99e2c
Nimish <nimish.v.shinde60@gmail.com>
2024-07-05 21:52:06 +0530

    Insert new nodes in between original nodes

--- July_24/Copy Linked List.py
+++ July_24/Copy Linked List.py
@@ -8,24 +8,15 @@ class Node:
 class Solution:
     def copyRandomList(self, head):
         if not head: return None
-        new_head, prev, curr, corresponding = None, None, head, {}
+        # insert new nodes in middle
+        curr = head
         while curr:
-            new_node = Node(curr.val)
-            if new_head is None:
-                new_head = new_node
-            else:
-                prev.next = new_node
-            corresponding[curr] = new_node
-            prev = new_node
-            curr = curr.next
+            new_node = Node(curr.val, curr.next)
+            curr.next = new_node
+            curr = new_node.next
 
-        curr, new_curr = head, new_head
-        while curr:
-            if curr.random:
-                new_curr.random = corresponding[curr.random]
-            curr = curr.next
-            new_curr = new_curr.next
-        return new_head
+        
+
 
 
 print(Solution().copyRandomList([[7,None],[13,0],[11,4],[10,2],[1,0]]))
