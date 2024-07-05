908652b7125abf012c67116e19165b9a28aa2d80
Nimish <nimish.v.shinde60@gmail.com>
2024-07-05 21:21:59 +0530

    Store only corresponding nodes in map

--- July_24/Copy Linked List.py
+++ July_24/Copy Linked List.py
@@ -8,28 +8,24 @@ class Node:
 class Solution:
     def copyRandomList(self, head):
         if not head: return None
-        links1, links2, i = {}, {}, 0
-        while head:
-            links1[head] = i
-            new_node = Node(head.val)
-            links2[i] = new_node
-            head = head.next
-            i += 1
-
-        for ele in links1:
-            # print(ele.val, '-> ', links1[ele])
-            if ele.next:
-                index, target_index = links1[ele], links1[ele.next]
-                links2[index].next = links2[target_index]
-            if ele.random:
-                index, target_index = links1[ele], links1[ele.random]
-                links2[index].random = links2[target_index]
+        new_head, prev, curr, corresponding = None, None, head, {}
+        while curr:
+            new_node = Node(curr.val)
+            if new_head is None:
+                new_head = new_node
             else:
-                links2[links1[ele]].random = None
+                prev.next = new_node
+            corresponding[curr] = new_node
+            prev = new_node
+            curr = curr.next
 
-        # for i in links2:
-        #     print(links2[i].val ,'->', links2[i].next.val)
-        return links2[0]
+        curr, new_curr = head, new_head
+        while curr:
+            if curr.random:
+                new_curr.random = corresponding[curr.random]
+            curr = curr.next
+            new_curr = new_curr.next
+        return new_head
 
 
 print(Solution().copyRandomList([[7,None],[13,0],[11,4],[10,2],[1,0]]))
