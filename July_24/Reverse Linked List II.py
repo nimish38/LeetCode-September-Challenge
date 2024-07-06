2dcd64eb6b15e30acd57945ca7fef9abc05a51a0
Nimish <nimish.v.shinde60@gmail.com>
2024-07-06 19:43:43 +0530

    1 indexed

--- July_24/Reverse Linked List II.py
+++ July_24/Reverse Linked List II.py
@@ -1,12 +1,12 @@
 class Solution:
     def reverseBetween(self, head, left: int, right: int):
-        curr, i, vals = head, 0, []
+        curr, i, vals = head, 1, []
         while curr:
             if left <= i <= right:
                 vals.append(curr.val)
             curr = curr.next
             i += 1
-        curr, i = head, 0
+        curr, i = head, 1
         while curr:
             if left <= i <= right:
                 curr.val = vals.pop()
