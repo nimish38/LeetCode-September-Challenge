f1dd44ab3710eda18a3c49ee20bbd1ab8d70c19e
Nimish <nimish.v.shinde60@gmail.com>
2024-07-06 19:28:27 +0530

    store values in list and replace

---
+++ July_24/Reverse Linked List II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseBetween(self, head, left: int, right: int):
+        curr, i, vals = head, 0, []
+        while curr:
+            if left <= i <= right:
+                vals.append(curr.val)
+            curr = curr.next
+            i += 1
+        curr, i = head, 0
+        while curr:
+            if left <= i <= right:
+                curr.val = vals.pop()
+            curr = curr.next
+            i += 1
+        return head
+
+print(Solution().reverseBetween(head = [1,2,3,4,5], left = 2, right = 4))
