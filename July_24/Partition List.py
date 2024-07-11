6a69e04a67075d2da8c31c9a9e5cb1bdd0f0b823
Nimish <nimish.v.shinde60@gmail.com>
2024-07-11 21:48:52 +0530

    build linked list and dummy node

---
+++ July_24/Partition List.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def build_LL(self, head):
+        first, prev = None, None
+        for i in range(len(head)):
+            a = ListNode(head[i])
+            if i == 0:
+                first = prev = a
+            else:
+                prev.next = a
+                prev = a
+        return first
+
+    def partition(self, head, x: int):
+        if not head:
+            return head
+        head = self.build_LL(head)
+        dummy = ListNode(-420, head)
+
+        return dummy.next
+
+print(Solution().partition(head = [1,4,3,2,5,2], x = 3))
