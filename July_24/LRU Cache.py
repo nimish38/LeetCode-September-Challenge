9613340039b8337776b56397bcf36bbc5104b09f
Nimish <nimish.v.shinde60@gmail.com>
2024-07-13 15:24:00 +0530

    correction on put

--- July_24/LRU Cache.py
+++ July_24/LRU Cache.py
@@ -13,11 +13,23 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
+        i = 0
         for i in range(len(self.cache)):
             if self.cache[i][0] == key:
                 del self.cache[i]
                 break
-        if i == self.capacity:
+        if i == self.capacity - 1:
             del self.cache[0]
         self.cache.append((key, value))
 
+
+lRUCache = LRUCache(2)
+lRUCache.put(1, 1)
+lRUCache.put(2, 2)
+print(lRUCache.get(1))
+lRUCache.put(3, 3)
+print(lRUCache.get(2))
+lRUCache.put(4, 4)
+print(lRUCache.get(1))
+print(lRUCache.get(3))
+print(lRUCache.get(4))
