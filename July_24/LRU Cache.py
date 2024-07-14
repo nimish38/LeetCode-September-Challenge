99dc9e67192c4199a3ce988c283f1513370ad8cf
Nimish <nimish.v.shinde60@gmail.com>
2024-07-14 20:39:08 +0530

    Updated getter and setter with new DS

--- July_24/LRU Cache.py
+++ July_24/LRU Cache.py
@@ -16,10 +16,24 @@ class LRUCache:
         self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-
+        if key in self.cache:
+            val, node = self.cache[key]
+            self.remove_node(node)
+            self.push_back(key, val)
+            return val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        
+        if key in self.cache:
+            node = self.cache[key][1]
+            self.remove_node(node)
+            self.push_back(key, value)
+        else:
+            if len(self.cache) == self.capacity:
+                lru_node = self.head.next
+                self.remove_node(lru_node)
+            self.push_back(key, value)
+
 
 lRUCache = LRUCache(2)
 print(lRUCache.get(2))
