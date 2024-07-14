fcbc6de10e302a94675afdae273b79068327347e
Nimish <nimish.v.shinde60@gmail.com>
2024-07-14 20:24:49 +0530

    Double Linked list and map introduced

--- July_24/LRU Cache.py
+++ July_24/LRU Cache.py
@@ -1,28 +1,25 @@
+class Dll:
+    def __init__(self,key, val):
+        self.val = val
+        self.key = key
+        self.prev = None
+        self.next = None
+
+
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cache = []
+        self.cache = {}
         self.capacity = capacity
+        self.head = Dll(-1, -1)
+        self.tail = Dll(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, key: int) -> int:
-        for i in range(len(self.cache)):
-            if self.cache[i][0] == key:
-                val = self.cache[i][1]
-                del self.cache[i]
-                self.cache.append((key, val))
-                return val
-        return -1
+
 
     def put(self, key: int, value: int) -> None:
-        i, found = 0, False
-        for i in range(len(self.cache)):
-            if self.cache[i][0] == key:
-                del self.cache[i]
-                found = True
-                break
-        if i == self.capacity - 1 and not found and self.cache:
-            del self.cache[0]
-        self.cache.append((key, value))
-
+        
 
 lRUCache = LRUCache(2)
 print(lRUCache.get(2))
