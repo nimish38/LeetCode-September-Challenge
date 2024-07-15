3deed75a1c4c9ad53f8bed2bb0b52e262c572f05
Nimish <nimish.v.shinde60@gmail.com>
2024-07-15 21:46:09 +0530

    iterative solution with stack

--- July_24/Deptch of Binary Tree.py
+++ July_24/Deptch of Binary Tree.py
@@ -1,15 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def maxDepth(self, root):
         if not root:
             return 0
         queue, dep = [(root, 1)], 1
         while queue:
-            for i in range(len(queue)):
-                node, val = queue.pop(0)
-                if dep < val:
-                    dep = val
-                if node.left:
-                    queue.append((node.left, val + 1))
-                if node.right:
-                    queue.append((node.right, val + 1))
+            node, val = queue.pop()
+            if dep < val:
+                dep = val
+            if node.left:
+                queue.append((node.left, val + 1))
+            if node.right:
+                queue.append((node.right, val + 1))
         return dep
