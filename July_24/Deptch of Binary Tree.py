068b1b456c2e90d62fa15dccc3f580f28628fe96
Nimish <nimish.v.shinde60@gmail.com>
2024-07-15 21:42:33 +0530

    iterative solution with queue

--- July_24/Deptch of Binary Tree.py
+++ July_24/Deptch of Binary Tree.py
@@ -1,10 +1,15 @@
 class Solution:
-    def getDepth(self, node):
-        if not node:
-            return 0
-        return 1 + max(self.getDepth(node.left), self.getDepth(node.right))
-
     def maxDepth(self, root):
         if not root:
             return 0
-        return self.getDepth(root)
+        queue, dep = [(root, 1)], 1
+        while queue:
+            for i in range(len(queue)):
+                node, val = queue.pop(0)
+                if dep < val:
+                    dep = val
+                if node.left:
+                    queue.append((node.left, val + 1))
+                if node.right:
+                    queue.append((node.right, val + 1))
+        return dep
