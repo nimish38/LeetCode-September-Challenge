1e8d24df9c82863361b50250441701d69349387f
Nimish <nimish.v.shinde60@gmail.com>
2024-07-16 21:45:57 +0530

    recursively check nodes

--- July_24/Same Tree.py
+++ July_24/Same Tree.py
@@ -1,5 +1,13 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def similar(self, p, q):
+        if not p and not q:
+            return True
         if (not p and q) or (p and not q):
             return False
         if (p and q) and p.val != q.val:
@@ -9,3 +17,11 @@ class Solution:
     def isSameTree(self, p, q) -> bool:
         return self.similar(p, q)
 
+
+x, y = TreeNode(2), TreeNode(3)
+z = TreeNode(1, x, y)
+
+a, b = TreeNode(2), TreeNode(3)
+c = TreeNode(1, a, b)
+
+print(Solution().isSameTree(z, c))
