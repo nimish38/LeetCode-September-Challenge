f4725efbfe7de2ad07e302390c8c810b48e091c4
Nimish <nimish.v.shinde60@gmail.com>
2024-07-17 20:47:57 +0530

    recursive with swap

---
+++ July_24/Invert Binary Tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def invert(self, node):
+        if node.left or node.right:
+            node.left, node.right = node.right, node.left
+        if node.left:
+            self.invert(node.left)
+        if node.right:
+            self.invert(node.right)
+
+
+    def invertTree(self, root):
+        if not root:
+            return None
+        return self.invert(root)
