76527a0b089dc7704cab2bf4cda56d6eb817ca8e
Nimish <nimish.v.shinde60@gmail.com>
2024-07-17 21:01:59 +0530

    iterative with stack

--- July_24/Invert Binary Tree.py
+++ July_24/Invert Binary Tree.py
@@ -1,15 +1,9 @@
 class Solution:
-    def invert(self, node):
-        if node.left or node.right:
-            node.left, node.right = node.right, node.left
-        if node.left:
-            self.invert(node.left)
-        if node.right:
-            self.invert(node.right)
-        return node
-
-
     def invertTree(self, root):
-        if not root:
-            return None
-        return self.invert(root)
+        st = [root]
+        while st:
+            node = st.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                st.extend([node.left, node.right])
+        return root
