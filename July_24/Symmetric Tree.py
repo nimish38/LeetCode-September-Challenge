24dad821cf6f6746ff453e6753a7da7cf0361857
Nimish <nimish.v.shinde60@gmail.com>
2024-07-18 20:11:25 +0530

    iterative, using stack instead of recursive call

--- July_24/Symmetric Tree.py
+++ July_24/Symmetric Tree.py
@@ -1,11 +1,12 @@
-    class Solution:
-        def isSymmetric(self, root) -> bool:
-            st = [(root.left, root.right)]
-            while st:
-                p, q = st.pop()
-                if not p and not q:
-                    continue
-                if not p or not q or p.val != q.val:
-                    return False
-                st.extend([(p.left, q.right), (p.right, q.left)])
-            return True
+class Solution:
+    def isSymmetric(self, root) -> bool:
+        st = [(root.left, root.right)]
+        while st:
+            p, q = st.pop()
+            if not p and not q:
+                continue
+            if not p or not q or p.val != q.val:
+                return False
+            st.extend([(p.left, q.right), (p.right, q.left)])
+        return True
+    
