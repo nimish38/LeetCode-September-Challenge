41ea3a5dd2898117036525d48122fa1077216a61
Nimish <nimish.v.shinde60@gmail.com>
2024-07-19 21:00:33 +0530

    Recursively build right and left subtree

--- July_24/Tree from Preorder Inorder.py
+++ July_24/Tree from Preorder Inorder.py
@@ -4,9 +4,20 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    def buildTree(self, preorder, inorder = []):
-        # do the following
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = 0
+        while i < len(preorder) and inorder[i] != root.val:
+            i += 1
+        root.left = self.buildTree(preorder[1: i + 1], inorder[:i])
+        root.right = self.buildTree(preorder[i + 1:], inorder[i + 1:])
+        return root
+
+
 #   make first node from preorder as root
 #   find that node in inorder
 #   recursively call all left nodes in inorder as root.left
