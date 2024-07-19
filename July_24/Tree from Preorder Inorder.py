3c017f0a524a96a1a7543ffd29b576be64eb15f0
Nimish <nimish.v.shinde60@gmail.com>
2024-07-19 21:20:24 +0530

    Little optimisation

--- July_24/Tree from Preorder Inorder.py
+++ July_24/Tree from Preorder Inorder.py
@@ -7,20 +7,17 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder, inorder):
-        if not preorder or not inorder:
-            return None
         root = TreeNode(preorder[0])
         i = 0
         while i < len(preorder) and inorder[i] != root.val:
             i += 1
-        root.left = self.buildTree(preorder[1: i + 1], inorder[:i])
-        root.right = self.buildTree(preorder[i + 1:], inorder[i + 1:])
+        if i > 0:
+            root.left = self.buildTree(preorder[1: i + 1], inorder[:i])
+        if i < len(preorder) - 1:
+            root.right = self.buildTree(preorder[i + 1:], inorder[i + 1:])
         return root
 
 
-#   make first node from preorder as root
-#   find that node in inorder
-#   recursively call all left nodes in inorder as root.left
-# recursively call all right nodes in inorder as root.right
-#  repeat till empty
+x = Solution().buildTree(preorder = [3,9,20,15,7], inorder = [9,3,15,20,7])
+print(x)
 
