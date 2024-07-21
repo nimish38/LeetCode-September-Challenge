fbb279d4ca0797d01c309ee7569c7053f76002e0
Nimish <nimish.v.shinde60@gmail.com>
2024-07-21 20:37:01 +0530

    maintain var for inorder and postorder limits

--- July_24/Tree from Postorder Inorder.py
+++ July_24/Tree from Postorder Inorder.py
@@ -6,19 +6,18 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, inorder, postorder):
-        def construct(start, end):
-            if start > end:
+        def construct(in_start, in_end, post_start, post_end):
+            if post_start > post_end or in_start > in_end:
                 return
-            root, i = TreeNode(postorder[self.idx]), start
-            self.idx -= 1
+            root, i = TreeNode(postorder[post_end]), in_start
             while inorder[i] != root.val:
                 i += 1
-            root.right = construct(start, i - 1)
-            root.left = construct(i, end)
+            root.right = construct(i + 1, in_end, i, post_end - 1)
+            root.left = construct(in_start, i - 1, post_start, i - 1)
             return root
 
-        self.idx = len(inorder) - 1
-        return construct(0, len(inorder) - 1)
+        n = len(inorder) - 1
+        return construct(0, n, 0, n)
 
 x = Solution().buildTree(inorder = [9,3,15,20,7], postorder = [9,15,7,20,3])
 print(x.val)
