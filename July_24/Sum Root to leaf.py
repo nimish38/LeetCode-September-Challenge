ef4b03061945d7af9a4c43f983bb21f634817810
Nimish <nimish.v.shinde60@gmail.com>
2024-07-27 21:29:09 +0530

    perform sum in var instead of list

--- July_24/Sum Root to leaf.py
+++ July_24/Sum Root to leaf.py
@@ -1,13 +1,13 @@
 class Solution:
     def sumNumbers(self, root) -> int:
-        st, nums = [(root, str(root.val))], []
+        st, nums = [(root, str(root.val))], 0
         while st:
             node, curr = st.pop()
             if not node.left and not node.right:
-                nums.append(int(curr))
+                nums += int(curr)
             if node.left:
                 st.append((node.left, curr + str(node.left.val)))
             if node.right:
                 st.append((node.right, curr + str(node.right.val)))
-        return sum(nums)
+        return nums
 
