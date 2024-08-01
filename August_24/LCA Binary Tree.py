09597f957f9d546903fb34a77869705ce5feb3e4
Nimish <nimish.v.shinde60@gmail.com>
2024-08-01 19:46:56 +0530

    recursively find ancestor

---
+++ August_24/LCA Binary Tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left == right:
+            return root
+        return left or right
