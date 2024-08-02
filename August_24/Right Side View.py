ba1552045d4d2df810174cbce7abec3610e5f568
Nimish <nimish.v.shinde60@gmail.com>
2024-08-02 20:03:45 +0530

    build tree and test

--- August_24/Right Side View.py
+++ August_24/Right Side View.py
@@ -1,3 +1,10 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def rightSideView(self, root):
         if not root:
@@ -12,3 +19,8 @@ class Solution:
                 if node.right:
                     st.append(node.right)
         return res
+
+a, b, c = TreeNode(1), TreeNode(2), TreeNode(3)
+a.left, a.right = b, c
+b.left = TreeNode(4)
+print(Solution().rightSideView(a))
