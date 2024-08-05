18faa88fefd305208ac2fe51a3d7f206df9d00c2
Nimish <nimish.v.shinde60@gmail.com>
2024-08-05 21:55:12 +0530

    build tree

--- August_24/zigzag.py
+++ August_24/zigzag.py
@@ -1,4 +1,10 @@
 from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def zigzagLevelOrder(self, root):
         qu, res, l2r = deque([root]), [], True
@@ -21,3 +27,8 @@ class Solution:
             l2r = not l2r
             res.append(lvl)
         return res
+
+a, b, c = TreeNode(3), TreeNode(9), TreeNode(20)
+a.left, a.right = b, c
+c.left, c.right = TreeNode(15), TreeNode(7)
+print(Solution().levelOrder(a))
