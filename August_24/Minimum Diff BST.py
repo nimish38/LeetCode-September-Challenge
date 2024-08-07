51145c537b1c8a8309f130c99f1f9750225008a1
Nimish <nimish.v.shinde60@gmail.com>
2024-08-07 21:39:02 +0530

    build tree and test

--- August_24/Minimum Diff BST.py
+++ August_24/Minimum Diff BST.py
@@ -1,3 +1,9 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def getMinimumDifference(self, root):
         diff, st = float('inf'), [root]
@@ -9,5 +15,8 @@ class Solution:
             if node.right:
                 diff = min(diff, abs(node.val - node.right.val))
                 st.append(node.right)
-
         return diff
+
+a, b, c = TreeNode(4), TreeNode(2), TreeNode(6)
+a.left, a.right = b, c
+b.left, b.right = TreeNode(1), TreeNode(3)
