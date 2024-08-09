be763b91b7aad0a3745e4eb54b0c9065aa546096
Nimish <nimish.v.shinde60@gmail.com>
2024-08-09 20:45:48 +0530

    recursively check BST validity

--- August_24/Valid BST.py
+++ August_24/Valid BST.py
@@ -1,3 +1,9 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def isValidBST(self, root) -> bool:
         left, right = True, True
@@ -9,6 +15,14 @@ class Solution:
         if root.right:
             if root.right.val <= root.val :
                 return False
-            right = self.isValidBST(root.left)
+            right = self.isValidBST(root.right)
 
         return left and right
+
+# a, b, c = TreeNode(5), TreeNode(4), TreeNode(1)
+# a.right, a.left = b, c
+# b.left, b.right = TreeNode(3), TreeNode(2)
+
+a, b = TreeNode(0), TreeNode(1)
+a.right = b
+print(Solution().isValidBST(a))
