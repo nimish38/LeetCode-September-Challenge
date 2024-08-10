a3e97d339365969ea1c2aae99572bae8d228c0b7
Nimish <nimish.v.shinde60@gmail.com>
2024-08-10 13:16:30 +0530

    duplicate items check

--- August_24/Valid BST.py
+++ August_24/Valid BST.py
@@ -21,12 +21,14 @@ class Solution:
                 while curr:
                     st.append(curr)
                     curr = curr.left
-        return inorder == sorted(inorder)
+        if len(inorder) == len(set(inorder)) and inorder == sorted(inorder):
+            return True
+        return False
 
-# a, b, c = TreeNode(5), TreeNode(4), TreeNode(1)
-# a.right, a.left = b, c
-# b.left, b.right = TreeNode(3), TreeNode(2)
+a, b, c = TreeNode(5), TreeNode(4), TreeNode(1)
+a.right, a.left = b, c
+b.left, b.right = TreeNode(3), TreeNode(2)
 
-a, b = TreeNode(0), TreeNode(1)
-a.left, a.right = b
+# a, b = TreeNode(0), TreeNode(1)
+# a.right = b
 print(Solution().isValidBST(a))
