d29ecb6bf3f4498a4ae91d9de4d4995dfc4623d1
Nimish <nimish.v.shinde60@gmail.com>
2024-08-11 21:05:14 +0530

    test and check

--- August_24/NO of Island.py
+++ August_24/NO of Island.py
@@ -1,14 +1,18 @@
 class Solution:
     def numIslands(self, grid) -> int:
-        m,n = len(grid), len(grid[0])
-        self.cnt = 0
-        def checkneighbors(i, j):
-            if i > 0 and grid[i - 1][j] or j > 0 and grid[i][j-2]:
-                return
-            self.cnt += 1
-            return 
+        m, n, cnt = len(grid), len(grid[0]), 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]:
-                    checkneighbors(i, j)
-        return self.cnt
+                if grid[i][j] == '1':
+                    if ( i > 0 and grid[i - 1][j] == '1' ) or ( j > 0 and grid[i][j - 1] == '1'):
+                        continue
+                    else:
+                        cnt += 1
+        return cnt
+
+print(Solution().numIslands(grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]))
