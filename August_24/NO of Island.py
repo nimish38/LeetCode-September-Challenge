dfb03545258395a26a9a8a1a38505f32a5d973a6
Nimish <nimish.v.shinde60@gmail.com>
2024-08-11 21:22:15 +0530

    Perform BFS and then amrk visited

--- August_24/NO of Island.py
+++ August_24/NO of Island.py
@@ -1,13 +1,26 @@
 class Solution:
     def numIslands(self, grid) -> int:
         m, n, cnt = len(grid), len(grid[0]), 0
+
+        def dfs(i, j):
+            st = [(i, j)]
+            while st:
+                a, b = st.pop()
+                grid[a][b] = 'V'
+                if a > 0 and grid[a - 1][b] == '1':
+                    st.append((a - 1, b))
+                if a < m - 1 and grid[a + 1][b] == '1':
+                    st.append((a + 1, b))
+                if b > 0 and grid[a][b - 1] == '1':
+                    st.append((a, b - 1))
+                if b < n - 1 and grid[a][b + 1] == '1':
+                    st.append((a, b + 1))
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
-                    if ( i > 0 and grid[i - 1][j] == '1' ) or ( j > 0 and grid[i][j - 1] == '1'):
-                        continue
-                    else:
-                        cnt += 1
+                    dfs(i, j)
+                    cnt += 1
         return cnt
 
 print(Solution().numIslands(grid = [
