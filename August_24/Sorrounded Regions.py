00cbe4b0d30076f18eca7f385325f2c136526dbc
Nimish <nimish.v.shinde60@gmail.com>
2024-08-12 22:32:57 +0530

    Perform DFS on board

--- August_24/Sorrounded Regions.py
+++ August_24/Sorrounded Regions.py
@@ -2,12 +2,24 @@ class Solution:
     def solve(self, board) -> None:
         m, n = len(board), len(board[0])
 
+        def dfs(i, j):
+            st = [(i, j)]
+            while st:
+                a, b = st.pop()
+                if a < 0 or a >= m or b < 0 or b >= n or board[a][b] != 'O':
+                    continue
+
+                board[a][b] = 'V'
+                st.extend([(a - 1, b), (a + 1, b), (a, b - 1), (a, b + 1)])
+
         for i in range(1, n - 1):
             for j in [0, m - 1]:
-                dfs(j, i)
+                if board[j][i] == 'O':
+                    dfs(j, i)
 
         for i in range(m):
             for j in [0, n - 1]:
-                dfs(i, j)
+                if board[i][j] == 'O':
+                    dfs(i, j)
 
         return board
