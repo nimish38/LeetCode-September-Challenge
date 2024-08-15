393fb203c4502d585a140e92fd31df4918cb60b8
Nimish <nimish.v.shinde60@gmail.com>
2024-08-15 21:22:46 +0530

    build dfs solution

--- August_24/EvaluateDivision.py
+++ August_24/EvaluateDivision.py
@@ -2,8 +2,18 @@ from collections import defaultdict
 class Solution:
     def calcEquation(self, equations, values, queries):
         # its a graph solution as equations follow transitivity principle
-
         adj, i, res = defaultdict(list), 0, []
+
+        def dfs(src, dest, vis, prod, ans):
+            if src in vis:
+                return
+            vis.add(src)
+            if src == dest:
+                ans = prod
+                return
+            for nei, val in adj[src]:
+                dfs(nei, dest, vis, prod * val, ans)
+
         for item in equations:
             u, v = item[0], item[1]
             val = values[i]
@@ -18,7 +28,9 @@ class Solution:
             if src == dest:
                 res.append(1.0)
             else:
-                res.append(dfs(src, dest, vis, 1.0))
+                ans, prod = -1.0, 1.0
+                dfs(src, dest, vis, prod, ans)
+                res.append(ans)
 
         return res
 
