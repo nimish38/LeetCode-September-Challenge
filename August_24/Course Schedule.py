1664c66767a11bb87677d5c45157dd37c7f01657
Nimish <nimish.v.shinde60@gmail.com>
2024-08-16 21:38:30 +0530

    build  topological sort check

--- August_24/Course Schedule.py
+++ August_24/Course Schedule.py
@@ -1,11 +1,28 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites):
         adj, indegree = defaultdict(list), [0]*numCourses
+
+        def topologicalSort():
+            cnt, que = 0, deque()
+            for i in indegree:
+                if indegree[i] == 0:
+                    cnt += 1
+                    que.append(i)
+            while que:
+                node = que.popleft()
+                for neighbor in adj[node]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        cnt += 1
+                        que.append(neighbor)
+            return cnt == numCourses
+
         for item in prerequisites:
             a, b = item[0], item[1]
             adj[b].append(a)
             indegree[a] += 1
-        print(adj, indegree)
+        return topologicalSort()
+
 
 print(Solution().canFinish(numCourses = 2, prerequisites = [[1,0],[0,1]]))
