8427cd89f893d1604f96343530fec8556bde0733
Nimish <nimish.v.shinde60@gmail.com>
2024-08-18 22:22:23 +0530

    Perform BFS with que

--- August_24/Snakes n Ladders.py
+++ August_24/Snakes n Ladders.py
@@ -2,12 +2,30 @@ from collections import deque
 class Solution:
     def snakesAndLadders(self, board) -> int:
         n, que = len(board), deque()
-        vis = []
+        vis, steps = [], 0
         for _ in range(n):
             vis.append([False]*n)
         vis[n - 1][0] = True
         que.append(1)
 
-        
+        while que:
+            lvl = len(que)
+            for _ in range(lvl):
+                cell = que.popleft()
+                if cell == n*n:
+                    return steps
+                for i in range(1, 7):
+                    val = cell + i
+                    if val < n*n:
+                        row, col = getCords(val)
+                        if vis[row][col]:
+                            continue
+                        if board[row][col] == -1:
+                            que.append(val)
+                        else:
+                            que.append(board[row][col])
+                        vis[row][col] = True
+
+            steps += 1
 
 print(Solution().snakesAndLadders(board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]))
