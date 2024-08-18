9f357b95b12042e7070197c151786c62d93b6d58
Nimish <nimish.v.shinde60@gmail.com>
2024-08-18 22:28:49 +0530

    get coordinates from number

--- August_24/Snakes n Ladders.py
+++ August_24/Snakes n Ladders.py
@@ -2,6 +2,15 @@ from collections import deque
 class Solution:
     def snakesAndLadders(self, board) -> int:
         n, que = len(board), deque()
+
+        def getCords(num):
+            row_top = (num - 1) // n
+            row_bottom = n - 1 - row_top
+            col_num = (num - 1) % n
+            if n % 2 == row_bottom % 2:
+                col_num = n - 1 - col_num
+            return row_bottom, col_num
+
         vis, steps = [], 0
         for _ in range(n):
             vis.append([False]*n)
@@ -27,5 +36,6 @@ class Solution:
                         vis[row][col] = True
 
             steps += 1
+        return -1
 
 print(Solution().snakesAndLadders(board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]))
