dc4c69016e2abae53156c100d45650a74bc77bac
Nimish <nimish.v.shinde60@gmail.com>
2024-08-19 22:51:56 +0530

    test solution

--- August_24/Gene Mutation.py
+++ August_24/Gene Mutation.py
@@ -3,14 +3,15 @@ class Solution:
     def minMutation(self, startGene: str, endGene: str, bank) -> int:
         if endGene not in bank:
             return -1
-        que, muta, genes, n = deque([startGene]), 0, ['A', 'T', 'C', 'G'], len(startGene)
+        que, muta, genes, n = deque([startGene]), 0, {'A', 'T', 'C', 'G'}, len(startGene)
+        # print(genes - {'A'})
         while que:
             lvl = len(que)
             for _ in range(lvl):
                 gene = que.popleft()
                 for i in range(n):
                     if gene[i] != endGene[i]:
-                        for x in genes - gene[i]:
+                        for x in genes - set(gene[i]):
                             new_gene = gene[:i] + x + gene[i + 1:]
                             if new_gene in bank:
                                 if new_gene == endGene:
@@ -19,5 +20,5 @@ class Solution:
             muta += 1
         return -1
 
-    
+print(Solution().minMutation(startGene = "AACCGGTT", endGene = "AAACGGTA", bank = ["AACCGGTA","AACCGCTA","AAACGGTA"]))
 
