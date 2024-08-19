27577993b07ccf43f4c5bc59372f90a5f3c1789a
Nimish <nimish.v.shinde60@gmail.com>
2024-08-19 22:48:38 +0530

    perform mutations by BFS

--- August_24/Gene Mutation.py
+++ August_24/Gene Mutation.py
@@ -1,10 +1,23 @@
+from collections import deque
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank) -> int:
         if endGene not in bank:
             return -1
-        diff = []
-        for i in range(len(startGene)):
-            if startGene[i] != endGene[i]:
-                diff.append(i)
+        que, muta, genes, n = deque([startGene]), 0, ['A', 'T', 'C', 'G'], len(startGene)
+        while que:
+            lvl = len(que)
+            for _ in range(lvl):
+                gene = que.popleft()
+                for i in range(n):
+                    if gene[i] != endGene[i]:
+                        for x in genes - gene[i]:
+                            new_gene = gene[:i] + x + gene[i + 1:]
+                            if new_gene in bank:
+                                if new_gene == endGene:
+                                    return muta + 1
+                                que.append(new_gene)
+            muta += 1
+        return -1
 
-        
+    
+
