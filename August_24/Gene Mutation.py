7cf72738b8b100b766f4e6edaa2a3679a43adb7c
Nimish <nimish.v.shinde60@gmail.com>
2024-08-20 23:01:44 +0530

    replace all chars and not only diff

--- August_24/Gene Mutation.py
+++ August_24/Gene Mutation.py
@@ -3,20 +3,20 @@ class Solution:
     def minMutation(self, startGene: str, endGene: str, bank) -> int:
         if endGene not in bank:
             return -1
-        que, muta, genes, n = deque([startGene]), 0, {'A', 'T', 'C', 'G'}, len(startGene)
+        que, muta, genes, n, bank, vis = deque([startGene]), 0, {'A', 'T', 'C', 'G'}, len(startGene), dict(bank), {}
         # print(genes - {'A'})
         while que:
             lvl = len(que)
             for _ in range(lvl):
                 gene = que.popleft()
                 for i in range(n):
-                    if gene[i] != endGene[i]:
-                        for x in genes - set(gene[i]):
-                            new_gene = gene[:i] + x + gene[i + 1:]
-                            if new_gene in bank:
-                                if new_gene == endGene:
-                                    return muta + 1
-                                que.append(new_gene)
+                    for x in genes:
+                        new_gene = gene[:i] + x + gene[i + 1:]
+                        if new_gene in bank and new_gene not in vis:
+                            if new_gene == endGene:
+                                return muta + 1
+                            que.append(new_gene)
+                            vis.add(gene)
             muta += 1
         return -1
 
