a0be929cea19078e44f6a7453bc65f7db52d6b0b
Nimish <nimish.v.shinde60@gmail.com>
2024-08-22 20:50:23 +0530

    build all unique chars avaiable

--- August_24/Word Ladder.py
+++ August_24/Word Ladder.py
@@ -1,6 +1,17 @@
 from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList) -> int:
-        wordList, que = dict.fromkeys(wordList, 0), deque([beginWord])
+        wordList, que, cnt = dict.fromkeys(wordList, 0), deque([beginWord]), 0
         if endWord not in wordList:
             return 0
+
+        vis, letters = {}, set()
+        for w in wordList:
+            letters.add(set(w))
+        
+        while que:
+            lvl = len(que)
+            for _ in range(lvl):
+                word = que.popleft()
+                if word == endWord:
+                    return cnt
