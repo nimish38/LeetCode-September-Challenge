461d4d707de0ae3dde5408f7fe7dc2e9a005d1d9
Nimish <nimish.v.shinde60@gmail.com>
2024-08-22 20:54:58 +0530

    Iterate on all chars at all positions

--- August_24/Word Ladder.py
+++ August_24/Word Ladder.py
@@ -5,13 +5,21 @@ class Solution:
         if endWord not in wordList:
             return 0
 
-        vis, letters = {}, set()
+        vis, letters, n = {}, set(), len(beginWord)
         for w in wordList:
             letters.add(set(w))
-        
+
         while que:
             lvl = len(que)
             for _ in range(lvl):
                 word = que.popleft()
                 if word == endWord:
                     return cnt
+                for i in range(n):
+                    for char in letters:
+                        new_word = word[:i] + char + word[i + 1:]
+                        if new_word not in vis and new_word in wordList:
+                            que.append(new_word)
+                            vis[word] = 1
+            cnt += 1
+        return 0
