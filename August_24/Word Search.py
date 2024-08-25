493d86ca5755b5dcc32e4c7c411d52ae4c18a6f5
Nimish <nimish.v.shinde60@gmail.com>
2024-08-25 21:49:09 +0530

    dfs traversal

--- August_24/Word Search.py
+++ August_24/Word Search.py
@@ -1,4 +1,15 @@
 class Solution:
+    def dfs(self, i, j, board, curr):
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0] or board[i][j] not in curr):
+            return
+        if '*' in curr:
+            self.res.append(curr['*'])
+        board[i][j] = '$'
+        neigh = [(-1, 0),(1, 0),(0, -1),(0, 1)]
+        for dx, dy in neigh:
+            self.dfs(i + dx, j + dy, board, curr[board[i][j]])
+
+
     def findWords(self, board:, words):
         self.trie = {}
         for word in words:
@@ -13,7 +24,7 @@ class Solution:
         self.res = []
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] in self.trie:
-                    dfs(i, j, board)
-
+                self.dfs(i, j, board, self.trie)
         return self.res
+
+    
