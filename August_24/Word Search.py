bf60dfbcad4162ff943dff9b35864e69aa1d6f7b
Nimish <nimish.v.shinde60@gmail.com>
2024-08-26 22:02:39 +0530

    bug fix

--- August_24/Word Search.py
+++ August_24/Word Search.py
@@ -1,16 +1,18 @@
 class Solution:
     def dfs(self, i, j, board, curr):
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0] or board[i][j] not in curr):
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] not in curr:
             return
-        if '*' in curr:
-            self.res.append(curr['*'])
+        if board[i][j] in curr and '*' in curr[board[i][j]]:
+            self.res.append(curr[board[i][j]]['*'])
+        key = board[i][j]
         board[i][j] = '$'
         neigh = [(-1, 0),(1, 0),(0, -1),(0, 1)]
         for dx, dy in neigh:
-            self.dfs(i + dx, j + dy, board, curr[board[i][j]])
+            if i + dx >= 0 and i + dx < len(board) and j + dy >= 0 and j + dy < len(board[0]) and key in curr:
+                self.dfs(i + dx, j + dy, board, curr[key])
 
 
-    def findWords(self, board:, words):
+    def findWords(self, board, words):
         self.trie = {}
         for word in words:
             curr = self.trie
@@ -24,7 +26,8 @@ class Solution:
         self.res = []
         for i in range(len(board)):
             for j in range(len(board[0])):
-                self.dfs(i, j, board, self.trie)
+                if board[i][j] in self.trie:
+                    self.dfs(i, j, board, self.trie)
         return self.res
 
-    
+print(Solution().findWords(board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]))
