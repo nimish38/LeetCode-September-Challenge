faff7819ae3bfacc402a362ce2ffc505d75518a4
Nimish <nimish.v.shinde60@gmail.com>
2024-08-26 22:42:51 +0530

    deep copy board for each recursion

--- August_24/Word Search.py
+++ August_24/Word Search.py
@@ -1,15 +1,16 @@
+import copy
 class Solution:
-    def dfs(self, i, j, board, curr):
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] not in curr:
+    def dfs(self, i, j, bo, curr):
+        if i < 0 or i >= len(bo) or j < 0 or j >= len(bo[0]) or bo[i][j] not in curr:
             return
-        if board[i][j] in curr and '*' in curr[board[i][j]]:
-            self.res.append(curr[board[i][j]]['*'])
-        key = board[i][j]
-        board[i][j] = '$'
+        if bo[i][j] in curr and '*' in curr[bo[i][j]]:
+            self.res.append(curr[bo[i][j]]['*'])
+        key = bo[i][j]
+        bo[i][j] = '$'
         neigh = [(-1, 0),(1, 0),(0, -1),(0, 1)]
         for dx, dy in neigh:
-            if i + dx >= 0 and i + dx < len(board) and j + dy >= 0 and j + dy < len(board[0]) and key in curr:
-                self.dfs(i + dx, j + dy, board, curr[key])
+            if i + dx >= 0 and i + dx < len(bo) and j + dy >= 0 and j + dy < len(bo[0]) and key in curr:
+                self.dfs(i + dx, j + dy, bo, curr[key])
 
 
     def findWords(self, board, words):
@@ -27,7 +28,8 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] in self.trie:
-                    self.dfs(i, j, board, self.trie)
+                    new_board = copy.deepcopy(board)
+                    self.dfs(i, j, new_board, self.trie)
         return self.res
 
 print(Solution().findWords(board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]))
