b3972ac99609331296af894327ed7ff5924f0d5e
Nimish <nimish.v.shinde60@gmail.com>
2024-08-26 22:52:16 +0530

    deep copy board for each recursion

--- August_24/Word Search.py
+++ August_24/Word Search.py
@@ -4,13 +4,14 @@ class Solution:
         if i < 0 or i >= len(bo) or j < 0 or j >= len(bo[0]) or bo[i][j] not in curr:
             return
         if bo[i][j] in curr and '*' in curr[bo[i][j]]:
-            self.res.append(curr[bo[i][j]]['*'])
+            self.res.add(curr[bo[i][j]]['*'])
         key = bo[i][j]
         bo[i][j] = '$'
         neigh = [(-1, 0),(1, 0),(0, -1),(0, 1)]
         for dx, dy in neigh:
             if i + dx >= 0 and i + dx < len(bo) and j + dy >= 0 and j + dy < len(bo[0]) and key in curr:
-                self.dfs(i + dx, j + dy, bo, curr[key])
+                new_bo = copy.deepcopy(bo)
+                self.dfs(i + dx, j + dy, new_bo, curr[key])
 
 
     def findWords(self, board, words):
@@ -24,12 +25,12 @@ class Solution:
             curr['*'] = word
 
 
-        self.res = []
+        self.res = set()
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if board[i][j] in self.trie:
                     new_board = copy.deepcopy(board)
                     self.dfs(i, j, new_board, self.trie)
-        return self.res
+        return list(self.res)
 
 print(Solution().findWords(board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]))
