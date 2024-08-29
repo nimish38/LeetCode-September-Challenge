c4ed33b0ea6a39892adf4f880717663734c946c6
Nimish <nimish.v.shinde60@gmail.com>
2024-08-29 20:15:04 +0530

    using itertool permutations

--- August_24/Permutations.py
+++ August_24/Permutations.py
@@ -1,18 +1,7 @@
+import itertools
 class Solution:
     def permute(self, nums):
-        res, n = [], len(nums)
-
-        def solve(combo, rem):
-            if len(combo) == n:
-                res.append([*combo])
-                return
-            for i in range(len(rem)):
-                combo.append(rem[i])
-                temp = rem[:i] + rem[i + 1:]
-                solve(combo, temp)
-                combo.pop()
-
-        solve([], nums)
-        return res
+        x = map(list, itertools.permutations(nums))
+        return list(x)
 
 print(Solution().permute(nums = [1,2,3]))
