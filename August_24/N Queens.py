f2126728fc9f4783ddeccfb181269e7d98f4c832
Nimish <nimish.v.shinde60@gmail.com>
2024-09-01 12:33:48 +0530

    check if cell is attacked by any queen

--- August_24/N Queens.py
+++ August_24/N Queens.py
@@ -4,9 +4,38 @@ class Solution:
         for _ in range(n):
             board.append([1]*n)
 
+        def isAvailable(row, col):
+            left = col
+            while left >= 0:
+                if board[row][left] == 'Q':
+                    return False
+                left -= 1
+
+            up = row
+            while up >= 0:
+                if board[up][col] == 'Q':
+                    return False
+                up -= 1
+
+            lrow, lcol = row, col
+            while lrow >= 0 and lcol >= 0:
+                if board[lrow][lcol] == 'Q':
+                    return False
+                lrow -= 1
+                lcol -= 1
+
+            urow, rcol = row, col
+            while urow >= 0 and rcol < n:
+                if board[urow][rcol] == 'Q':
+                    return False
+                urow -= 1
+                rcol += 1
+
+            return True
+
         def solve(row):
             for i in range(n):
-                if isAvailable(board[row][i]):
+                if isAvailable(row, i):
                     board[row][i] = 'Q'
                     if row == n - 1:
                         addanswer(board)
