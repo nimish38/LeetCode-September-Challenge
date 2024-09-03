626f2a1110183a9db504c1543f57e01d7f1d126c
Nimish <nimish.v.shinde60@gmail.com>
2024-09-03 21:18:27 +0530

    backtracking template

---
+++ August_24/Generate Parenthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generateParenthesis(self, n: int):
+        res = []
+
+        def solve(str, open, close):
+            if open == 0 and close == 0:
+                res.append(str)
+                return
+            if open > 0:
+                solve(str + '(', open - 1, close)
+            if close > open:
+                solve(str + ')', open, close - 1)
+
+        solve('(', n - 1, n)
+        return res
