207c87a12c6f92c7fb040cae05db04a88b86c8ce
Nimish <nimish.v.shinde60@gmail.com>
2024-09-04 22:21:30 +0530

    test debug and fix

--- Sept_24/Word Search.py
+++ Sept_24/Word Search.py
@@ -1,22 +1,26 @@
 class Solution:
-    def exist(self, board:, word: str) -> bool:
+    def exist(self, board, word: str):
         m, n = len(board), len(board[0])
 
         def search(row, col, ind):
             if ind == len(word):
                 return True
-            if m <= row < 0 or n <= col < 0 or board[i][j] != word[ind]:
+            if row < 0 or row >= m or col >= n or col < 0 or board[row][col] != word[ind]:
                 return False
+            board[row][col] = '$'
             dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
             for dx, dy in dir:
                 if search(row + dx, col + dy, ind + 1):
                     return True
+            board[row][col] = word[ind]
             return False
 
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    if search(i, j, 1):
+                    if search(i, j, 0):
                         return True
         return False
+
+print(Solution().exist(board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"))
