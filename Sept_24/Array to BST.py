47618e495021774ca72b490b272a1da0c420958e
Nimish <nimish.v.shinde60@gmail.com>
2024-09-05 21:30:03 +0530

    only passing range

--- Sept_24/Array to BST.py
+++ Sept_24/Array to BST.py
@@ -7,14 +7,15 @@ class TreeNode:
 
 class Solution:
     def sortedArrayToBST(self, nums):
-        if not nums:
-            return None
-        mid = len(nums) // 2
-        root = TreeNode(nums[mid])
-        root.left = self.sortedArrayToBST(nums[:mid])
-        root.right = self.sortedArrayToBST(nums[mid + 1:])
-        return root
-
+        def build(low, high):
+            if low > high:
+                return None
+            mid = (high + low) // 2
+            root = TreeNode(nums[mid])
+            root.left = build(low, mid - 1)
+            root.right = build(mid + 1, high)
+            return root
+        return build(0, len(nums)-1)
 
 x = Solution().sortedArrayToBST(nums = [-10,-3,0,5,9])
 print(x.val)
