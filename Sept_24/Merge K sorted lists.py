95c61fb304f78ded7bb8ab6b2c551f2b73076e8c
Nimish <nimish.v.shinde60@gmail.com>
2024-09-10 22:19:39 +0530

    test and debug

--- Sept_24/Merge K sorted lists.py
+++ Sept_24/Merge K sorted lists.py
@@ -8,22 +8,21 @@ class Solution:
     def mergeKLists(self, lists):
 
         def merge(l1, l2):
-            if not l1 and not l2:
-                return None
             if not l1:
                 return l2
             if not l2:
                 return l1
 
             if l1.val <= l2.val:
-                merge(l1.next, l2)
+                l1.next = merge(l1.next, l2)
                 return l1
             else:
-                merge(l1, l2.next)
+                l2.next = merge(l1, l2.next)
                 return l2
 
-
         start, end = 0, len(lists) - 1
+        if start > end:
+            return None
         if start == end:
             return lists[start]
         mid = (start + end) // 2
@@ -31,3 +30,13 @@ class Solution:
         list2 = self.mergeKLists(lists[mid:])
 
         return merge(list1, list2)
+
+
+a, b, c = ListNode(1), ListNode(4), ListNode(5)
+a.next, b.next = b, c
+p, q, r = ListNode(1), ListNode(3), ListNode(4)
+p.next, q.next = q, r
+x, y = ListNode(2), ListNode(6)
+x.next = y
+z = Solution().mergeKLists([a, p, x])
+print(z)
