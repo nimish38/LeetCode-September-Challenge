9ae1c8d4a2bdb6096ae16f0e88947a77462a9b39
Nimish <nimish.v.shinde60@gmail.com>
2024-09-13 22:04:51 +0530

    binary search

--- Sept_24/Search insert position.py
+++ Sept_24/Search insert position.py
@@ -1,9 +1,15 @@
-import bisect
 class Solution:
     def searchInsert(self, nums, target: int):
-        if target in nums:
-            return nums.index(target)
-        return bisect.bisect_left(nums, target)
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return start
 
-print(Solution().searchInsert(nums = [1,3,5,6], target = 7))
+print(Solution().searchInsert(nums = [1,3,5,6], target = 2))
 
