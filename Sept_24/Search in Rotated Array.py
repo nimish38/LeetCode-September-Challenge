37bdeb07ed28dc9fec780c0bc806f657a8f8d670
Nimish <nimish.v.shinde60@gmail.com>
2024-09-17 21:52:39 +0530

    debug and fix

--- Sept_24/Search in Rotated Array.py
+++ Sept_24/Search in Rotated Array.py
@@ -4,7 +4,7 @@ class Solution:
 
         def findPivot():
             start, end = 0, n - 1
-            while start < end:
+            while start <= end:
                 mid = (start + end) // 2
                 if nums[mid] > nums[mid + 1]:
                     return mid
@@ -28,10 +28,10 @@ class Solution:
         pivot = findPivot()
         if pivot != -1:
             left = binarySearch(0, pivot)
-            if left != -1:
+            if left == -1:
                 right = binarySearch(pivot + 1, n - 1)
                 return right
             return left
         return binarySearch(0, n - 1)
 
-print(Solution().search(nums = [4,5,6,7,0,1,2], target = 0))
+print(Solution().search(nums = [3,4,5,6,7], target = 0))
