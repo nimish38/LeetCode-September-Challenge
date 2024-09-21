abf746947b3433b3b5beddc25bc4f23e4da37b5b
Nimish <nimish.v.shinde60@gmail.com>
2024-09-21 23:55:39 +0530

    binary search and find partition elements

--- Sept_24/Median of sorted arrays.py
+++ Sept_24/Median of sorted arrays.py
@@ -1,30 +1,18 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        m, n, odd = len(nums1), len(nums2), False
-        nums1.append(float('inf'))
-        nums2.append(float('inf'))
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
 
-        if (m + n) % 2:
-            odd = True
+        start, end = 0, m
+        while start <= end:
+            Px = (start + end) // 2
+            Py = ((m + n + 1) // 2) - Px
 
-        i, j, k = 0, 0, 0
-        idx1 = (m + n) // 2
-        idx2 = idx1 - 1
-        mid1, mid2 = -1, -1
-        while k < idx1:
-            if nums1[i] < nums2[j]:
-                if k == idx2:
-                    mid2 = nums1[i]
-                i += 1
-            else:
-                if k == idx2:
-                    mid2 = nums2[j]
-                j += 1
-            k += 1
+            x1 = nums1[Px - 1]
+            x2 = nums2[Py - 1]
 
-        mid1 = min(nums1[i], nums2[j])
-        if not odd:
-            return (mid1 + mid2) / 2
-        return mid1
+            x3 = nums1[Px]
+            x4 = nums2[Py]
 
 print(Solution().findMedianSortedArrays(nums1 = [1,3], nums2 = [2,4,5]))
