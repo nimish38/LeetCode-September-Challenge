0504ccface647a0b613a9a8210fa79bafb94ef15
Nimish <nimish.v.shinde60@gmail.com>
2024-09-22 00:00:57 +0530

    Correct condition

--- Sept_24/Median of sorted arrays.py
+++ Sept_24/Median of sorted arrays.py
@@ -9,10 +9,29 @@ class Solution:
             Px = (start + end) // 2
             Py = ((m + n + 1) // 2) - Px
 
-            x1 = nums1[Px - 1]
-            x2 = nums2[Py - 1]
+            if Px == 0:
+                x1 = float('-inf')
+            else:
+                x1 = nums1[Px - 1]
+            if Py == 0:
+                x2 = float('-inf')
+            else:
+                x2 = nums2[Py - 1]
 
-            x3 = nums1[Px]
-            x4 = nums2[Py]
+            if Px == m:
+                x3 = float('inf')
+            else:
+                x3 = nums1[Px]
+            if Py == n:
+                x4 = float('inf')
+            else:
+                x4 = nums2[Py]
+
+
+            if x1 <= x4 and x2 <= x3:
+                if (m + n) % 2:
+                    return max(x1, x2)
+                return (max(x1, x2) + min(x3, x4)) / 2
+
 
 print(Solution().findMedianSortedArrays(nums1 = [1,3], nums2 = [2,4,5]))
