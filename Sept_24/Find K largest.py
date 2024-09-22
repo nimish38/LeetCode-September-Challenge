996a9f31ca149048b129acd196d64f5f4a73fe33
Nimish <nimish.v.shinde60@gmail.com>
2024-09-22 13:22:55 +0530

    using builtin heapify heapmax

--- Sept_24/Find K largest.py
+++ Sept_24/Find K largest.py
@@ -2,6 +2,8 @@ import heapq
 class Solution:
     def findKthLargest(self, nums, k: int) -> int:
         heapq._heapify_max(nums)
-        return nums[k - 1]
+        for _ in range(k - 1):
+            heapq._heappop_max(nums)
+        return heapq.heappop(nums)
 
-print(Solution().findKthLargest(nums = [3,2,1,5,6,4], k = 2))
+print(Solution().findKthLargest(nums = [3,2,3,1,2,4,5,5,6], k = 4))
