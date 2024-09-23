738b70005f067a543eaddada4c68633d80f7d487
Nimish <nimish.v.shinde60@gmail.com>
2024-09-23 22:19:17 +0530

    Pop heap and return profits

--- Sept_24/IPO.py
+++ Sept_24/IPO.py
@@ -6,11 +6,19 @@ class Solution:
                 w += profits[i]
 
         heap = []
-        heapq._heapify_max(heap)
-
         for i in range(len(capital)):
             if capital[i] != 0:
-                heapq._heapify_max(heap, (profits[i], capital[i]))
+                heapq.heappush(heap, (profits[i], capital[i]))
+        heapq._heapify_max(heap)
 
-        
+        while w > 0 and k > 0:
+            pro, cap = heapq.heappop(heap)
+            if cap <= w:
+                w = w + pro - cap
+                k -= 1
+
+        return w
+
+
+print(Solution().findMaximizedCapital(k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]))
 
