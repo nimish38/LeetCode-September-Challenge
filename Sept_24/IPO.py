520796258fd916e80225ef0cc7b4cd7d7acef062
Nimish <nimish.v.shinde60@gmail.com>
2024-09-24 23:44:01 +0530

    make pairs of values

--- Sept_24/IPO.py
+++ Sept_24/IPO.py
@@ -1,24 +1,12 @@
 import heapq
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits, capital) -> int:
+        pairs = []
         for i in range(len(capital)):
             if capital[i] == 0:
-                w += profits[i]
-                k -= 2
-
-        heap = []
-        for i in range(len(capital)):
-            if capital[i] != 0:
-                heapq.heappush(heap, (profits[i], capital[i]))
-        heapq._heapify_max(heap)
-
-        while w > 0 and k > 0:
-            pro, cap = heapq.heappop(heap)
-            if cap <= w:
-                w = w + pro - cap
-                k -= 1
-
-        return w
+                pairs.append((capital[i], profits[i]))
+        pairs.sort()
+    
 
 
 print(Solution().findMaximizedCapital(k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]))
