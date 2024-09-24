0cd23dfff444b7cdd91df8a1a06d05b37d0642a7
Nimish <nimish.v.shinde60@gmail.com>
2024-09-24 23:53:54 +0530

    test and debug

--- Sept_24/IPO.py
+++ Sept_24/IPO.py
@@ -3,8 +3,7 @@ class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits, capital) -> int:
         pairs, heap, i = [], [], 0
         for i in range(len(capital)):
-            if capital[i] == 0:
-                pairs.append((capital[i], profits[i]))
+            pairs.append((capital[i], profits[i]))
         pairs.sort()
         i = 0
         while k > 0:
@@ -12,10 +11,11 @@ class Solution:
                 heapq.heappush(heap, pairs[i][1])
                 i += 1
 
+            heapq._heapify_max(heap)
             w += heapq._heappop_max(heap)
             k -= 1
         return w
 
 
-print(Solution().findMaximizedCapital(k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]))
+print(Solution().findMaximizedCapital(k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]))
 
