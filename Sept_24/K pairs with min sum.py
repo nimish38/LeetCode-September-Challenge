45e1600e743781edb91b9bf1359eb4b2404ec0bf
Nimish <nimish.v.shinde60@gmail.com>
2024-09-26 20:16:50 +0530

    using min heap to track least sum elements

--- Sept_24/K pairs with min sum.py
+++ Sept_24/K pairs with min sum.py
@@ -1,21 +1,27 @@
 import heapq
 class Solution:
     def kSmallestPairs(self, nums1, nums2, k: int):
-        res, i, j = [], 0, 0
+        heap, m, n = [], len(nums1) - 1, len(nums2) - 1
+        heapq.heappush(heap, (nums1[0] + nums2[0], [0, 0]))
+        vis, k, res = {(0, 0)}, k - 1, []
 
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                sum, pair = nums1[i] + nums2[j], [nums1[i], nums2[j]]
-                if len(res) < k:
-                    heapq.heappush(res, (-1 * sum, pair))
-                elif sum < (-1 * res[0][0]):
-                    heapq.heappush(res, (-1 * sum, pair))
-                    heapq.heappop(res)
-                else:
-                    break
-        result = []
-        for _ in range(k):
-            result.append(heapq.heappop(res)[1])
-        return result[::-1]
+        while k > 0:
+            sum, val = heapq.heappop(heap)
+            res.append([nums1[val[0]], nums2[val[1]]])
+            k -= 1
+
+            if val[0] < m and (val[0] + 1, val[1]) not in vis:
+                sum_new = nums1[val[0] + 1] + nums2[val[1]]
+                heapq.heappush(res, (sum_new, [val[0] + 1, val[1]]))
+                vis.add((val[0] + 1, val[1]))
+
+            if val[1] < n and (val[0], val[1] + 1) not in vis:
+                sum_new = nums1[val[0]] + nums2[val[1] + 1]
+                heapq.heappush(res, (sum_new, [val[0], val[1] + 1]))
+                vis.add((val[0], val[1] + 1))
+            
+
+
+
 
 print(Solution().kSmallestPairs(nums1 = [1,7,11], nums2 = [2,4,6], k = 3))
