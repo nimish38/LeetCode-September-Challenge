3cf4a60f8737d42b9e811e2e259062a0e35c046e
Nimish <nimish.v.shinde60@gmail.com>
2024-09-26 19:32:26 +0530

    break instead of checking all pairs

--- Sept_24/K pairs with min sum.py
+++ Sept_24/K pairs with min sum.py
@@ -6,9 +6,13 @@ class Solution:
         for i in range(len(nums1)):
             for j in range(len(nums2)):
                 sum, pair = nums1[i] + nums2[j], [nums1[i], nums2[j]]
-                heapq.heappush(res, (-1 * sum, pair))
-                if len(res) > k:
+                if len(res) < k:
+                    heapq.heappush(res, (-1 * sum, pair))
+                elif sum < (-1 * res[0][0]):
+                    heapq.heappush(res, (-1 * sum, pair))
                     heapq.heappop(res)
+                else:
+                    break
         result = []
         for _ in range(k):
             result.append(heapq.heappop(res)[1])
