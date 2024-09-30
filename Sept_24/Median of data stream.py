5088690784c1aeb991bcc2ea35d66e5c05f84889
Nimish <nimish.v.shinde60@gmail.com>
2024-09-30 22:35:49 +0530

    using heap to add element

---
+++ Sept_24/Median of data stream.py
@@ -0,0 +1,19 @@
+import heapq
+class MedianFinder:
+    def __init__(self):
+        self.nums = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.nums, num)
+
+    def findMedian(self) -> float:
+        n = len(self.nums)
+        if n % 2:
+            return self.nums[n // 2]
+        else:
+            return (self.nums[n//2] + self.nums[(n//2) - 1])/2
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
