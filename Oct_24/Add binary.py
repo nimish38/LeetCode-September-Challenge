243cb9cd4d8d9b8c9210c5b6db7a0e5c91b11aba
Nimish <nimish.v.shinde60@gmail.com>
2024-10-02 14:59:25 +0530

    string manipulation and fix

--- Oct_24/Add binary.py
+++ Oct_24/Add binary.py
@@ -1,6 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        i, j, res, carry = len(a) - 1, len(b) - 1, False
+        i, j, res, carry = len(a) - 1, len(b) - 1, '', False
         while i >= 0 and j >= 0:
             if a[i] == '1' and b[j] == '1':
                 if carry:
@@ -16,12 +16,47 @@ class Solution:
             else:
                 if carry:
                     res = '1' + res
+                    carry = False
                 else:
                     res = '0' + res
+
+            i -= 1
+            j -= 1
+
+        while i >= 0:
+            if a[i] == '1':
+                if carry:
+                    res = '0' + res
+                    carry = True
+                else:
+                    res = '1' + res
+            else:
+                if carry:
+                    res = '1' + res
+                    carry = False
+                else:
+                    res = '0' + res
+            i -= 1
+
+        while j >= 0:
+            if b[j] == '1':
+                if carry:
+                    res = '0' + res
+                    carry = True
+                else:
+                    res = '1' + res
+            else:
+                if carry:
+                    res = '1' + res
+                    carry = False
+                else:
+                    res = '0' + res
+            j -= 1
+
 
         if carry:
             res = '1' + res
         return res
 
 
-print(Solution().addBinary(a = "11", b = "1"))
+print(Solution().addBinary(a = "1010", b = "1011"))
