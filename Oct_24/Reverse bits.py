a0c856fac03e0249e273e5b52ef57622159d3fde
Nimish <nimish.v.shinde60@gmail.com>
2024-10-03 21:42:51 +0530

    add leading zeroes

--- Oct_24/Reverse bits.py
+++ Oct_24/Reverse bits.py
@@ -1,4 +1,8 @@
 class Solution:
-    def reverseBits(self, n: int) -> int:
-        n = str(n)[::-1]
-        return int(n, 10)
+    def reverseBits(self, n) -> int:
+        n = format(n, 'b')
+        n = ('0' * (32 - len(n))) + n
+        n = n[::-1]
+        return int(n, 2)
+
+print(Solution().reverseBits(43261596))
