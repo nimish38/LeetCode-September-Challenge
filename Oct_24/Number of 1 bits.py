46efcbde331d622c8cca0b067ee47243eeb9ee46
Nimish <nimish.v.shinde60@gmail.com>
2024-10-04 22:03:21 +0530

    using binary method

--- Oct_24/Number of 1 bits.py
+++ Oct_24/Number of 1 bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        n = format(n, 'b')
-        return str(n).count('1')
+        n = bin(n)[1:]
+        return n.count('1')
 
-print(Solution().hammingWeight(n = 11))
+print(Solution().hammingWeight(n = 2147483645))
