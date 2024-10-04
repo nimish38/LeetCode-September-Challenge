042a6f84b1299552c7d5a6f167813a8dcda29a37
Nimish <nimish.v.shinde60@gmail.com>
2024-10-04 22:06:21 +0530

    use right shift operator

--- Oct_24/Number of 1 bits.py
+++ Oct_24/Number of 1 bits.py
@@ -1,6 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        n = bin(n)[1:]
-        return n.count('1')
+        res = 0
+        while n:
+            res += n % 2 # 0 if rightmost bit is 0, 1 if rightmost bit is 1
+            n = n >> 1 # shift right to the next bit
+        return res
 
 print(Solution().hammingWeight(n = 2147483645))
