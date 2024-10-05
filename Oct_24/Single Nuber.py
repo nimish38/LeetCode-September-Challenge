29bdadcd98f77b25d766e198c83f453f3ace17a7
Nimish <nimish.v.shinde60@gmail.com>
2024-10-05 13:04:09 +0530

    using operator and reduce

--- Oct_24/Single Nuber.py
+++ Oct_24/Single Nuber.py
@@ -1,8 +1,6 @@
+import functools, operator
 class Solution:
     def singleNumber(self, nums) -> int:
-        res = 0
-        for num in nums:
-            res ^= num
-        return res
+        return functools.reduce(operator.xor, nums)
 
 print(Solution().singleNumber(nums = [4,1,2,1,2]))
