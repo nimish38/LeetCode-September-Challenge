6b831624cd62a708960ab8aeb146a3c0950ab558
Nimish <nimish.v.shinde60@gmail.com>
2024-10-06 17:27:43 +0530

    bit manipulation funde

--- Oct_24/Single Number II.py
+++ Oct_24/Single Number II.py
@@ -1,11 +1,8 @@
 class Solution:
     def singleNumber(self, nums) -> int:
-        nums.sort()
-        i = 0
-        while i < len(nums):
-            if i < len(nums) - 1 and nums[i] == nums[i + 2]:
-                i += 3
-            else:
-                return nums[i]
+
+        # Left shift by two bits: x << 2 - is similar to multiply by 4
+        # check if kth bit is 0 or 1: (1 << k) && num - first right shift 1 by k bits then and it with the number
+        # make kth bit as 1 : (1 << k) || num - first right shift 1 by k bits then or it with number
 
 print(Solution().singleNumber(nums = [0,1,0,1,0,1,99]))
