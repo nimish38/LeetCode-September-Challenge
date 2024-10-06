6c47f5afdc3c21a420e62ff30b4576d752f247c7
Nimish <nimish.v.shinde60@gmail.com>
2024-10-06 16:24:10 +0530

    debug and test solution

--- Oct_24/Single Number II.py
+++ Oct_24/Single Number II.py
@@ -3,9 +3,9 @@ class Solution:
         nums.sort()
         i = 0
         while i < len(nums):
-            if nums[i] == nums[i + 2]:
+            if i < len(nums) - 1 and nums[i] == nums[i + 2]:
                 i += 3
             else:
                 return nums[i]
 
-print(Solution().singleNumber())
+print(Solution().singleNumber(nums = [0,1,0,1,0,1,99]))
