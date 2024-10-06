ee932faa1ec48a7b9853b47af66a0dd4a5ab3b8d
Nimish <nimish.v.shinde60@gmail.com>
2024-10-06 17:31:52 +0530

    check bit by bit and set 1 in res

--- Oct_24/Single Number II.py
+++ Oct_24/Single Number II.py
@@ -5,4 +5,17 @@ class Solution:
         # check if kth bit is 0 or 1: (1 << k) && num - first right shift 1 by k bits then and it with the number
         # make kth bit as 1 : (1 << k) || num - first right shift 1 by k bits then or it with number
 
+        res = 0
+        for k in range(0, 32):
+            curr, cntOne, cntZer = 1 << k, 0, 0
+            for num in nums:
+                if curr & num == 0:
+                    cntZer += 1
+                else:
+                    cntOne += 1
+            if cntOne % 3:
+                res = res | curr
+        return res
+
+
 print(Solution().singleNumber(nums = [0,1,0,1,0,1,99]))
