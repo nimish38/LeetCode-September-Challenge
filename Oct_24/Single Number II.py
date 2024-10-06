91982e34e769730a44c222e1807b7e097c5da7d4
Nimish <nimish.v.shinde60@gmail.com>
2024-10-06 17:41:54 +0530

    negative numbers are store as 2s complement

--- Oct_24/Single Number II.py
+++ Oct_24/Single Number II.py
@@ -1,6 +1,9 @@
 class Solution:
     def singleNumber(self, nums) -> int:
 
+        def twos_complement(j):
+            return j - (1 << (j.bit_length()))
+
         # Left shift by two bits: x << 2 - is similar to multiply by 4
         # check if kth bit is 0 or 1: (1 << k) && num - first right shift 1 by k bits then and it with the number
         # make kth bit as 1 : (1 << k) || num - first right shift 1 by k bits then or it with number
@@ -13,7 +16,9 @@ class Solution:
                     cntOne += 1
             if cntOne % 3:
                 res = res | curr
+        if res not in nums:
+            return twos_complement(res)
         return res
 
 
-print(Solution().singleNumber(nums = [0,1,0,1,0,1,99]))
+print(Solution().singleNumber(nums = [-2,-2,1,1,4,1,4,4,-4,-2]))
