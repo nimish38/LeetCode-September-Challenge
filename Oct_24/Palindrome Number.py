2235c9a64cec68a5e2255c4969305a01de0acb6d
Nimish <nimish.v.shinde60@gmail.com>
2024-10-09 22:10:48 +0530

    with integer calculations

--- Oct_24/Palindrome Number.py
+++ Oct_24/Palindrome Number.py
@@ -1,5 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        return x == x[::-1]
-print(Solution().isPalindrome(x = 121))
+        if x < 0:
+            return False
+        divisor = 1
+        while x >= divisor:
+            divisor *= 10
+        divisor //= 10
+        while x > 10:
+            left, right = x // divisor, x % 10
+            if left != right:
+                return False
+            x %= divisor
+            x //= 10
+            divisor //= 100
+        return True
+
+print(Solution().isPalindrome(x = 31213))
