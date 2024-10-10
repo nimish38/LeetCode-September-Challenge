ba0d788b3f2d7913fca64f60ff0d4bef8ed28cef
Nimish <nimish.v.shinde60@gmail.com>
2024-10-10 21:58:07 +0530

    brute force

---
+++ Oct_24/Plus One.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits):
+        i, carry = len(digits) - 1, False
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        else:
+            digits[i] = 0
+            carry = True
+            i -= 1
+            while i >= 0 and carry:
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    carry = False
+                i -= 1
+            return digits
+        
