630d4755d4c9fda294f43b8e524d85e8d7c6a2a7
Nimish <nimish.v.shinde60@gmail.com>
2024-10-10 22:10:50 +0530

    not using carry flag

--- Oct_24/Plus One.py
+++ Oct_24/Plus One.py
@@ -1,22 +1,11 @@
 class Solution:
     def plusOne(self, digits):
-        i, carry = len(digits) - 1, False
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        else:
+        for i in range(len(digits) -1 , -1, -1):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
             digits[i] = 0
-            carry = True
-            i -= 1
-            while i >= 0 and carry:
-                if digits[i] == 9:
-                    digits[i] = 0
-                else:
-                    digits[i] += 1
-                    carry = False
-                i -= 1
-            if carry:
-                digits = [1] + digits
-            return digits
+            if i == 0:
+                return [1] + digits
 
 print(Solution().plusOne(digits=[1,2,3,9,9]))
