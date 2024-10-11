7075e3fedf8791481f04cc400784a27caee78489
Nimish <nimish.v.shinde60@gmail.com>
2024-10-11 21:42:05 +0530

    brute force

---
+++ Oct_24/Factorial zeroes.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        if n == 0:
+            return 1
+        n, res = math.factorial(n), 0
+        while n%10 == 0:
+            res += 1
+            n //= 10
+        return res
