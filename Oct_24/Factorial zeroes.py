87ac13ca2b1694f8be762f66621f966ebe508a7c
Nimish <nimish.v.shinde60@gmail.com>
2024-10-11 22:19:00 +0530

    Count number of 5's

--- Oct_24/Factorial zeroes.py
+++ Oct_24/Factorial zeroes.py
@@ -1,12 +1,9 @@
-import math
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        if n == 0:
-            return 1
-        n, res = math.factorial(n), 0
-        while n%10 == 0:
-            res += 1
-            n //= 10
+        div, res = 5, 0
+        while div <= n:
+            res += n // div
+            div *= 5
         return res
 
-print(Solution().trailingZeroes(n = 5))
+print(Solution().trailingZeroes(n = 150))
