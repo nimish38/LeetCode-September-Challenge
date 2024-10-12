43e4fb02313e512eb8d6425eadea7c7caa30fe0b
Nimish <nimish.v.shinde60@gmail.com>
2024-10-12 14:29:44 +0530

    binary search on square root

---
+++ Oct_24/Sqrt.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1: return 1
+        l, r = 1, x//2
+        while l < r:
+            mid = (l + r) // 2
+            if mid * mid == x:
+                return mid
+            if mid * mid > x:
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
