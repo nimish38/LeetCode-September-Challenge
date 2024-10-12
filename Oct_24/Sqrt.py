4e0e5945e89debc896db33a81f8f07fc85ea96d9
Nimish <nimish.v.shinde60@gmail.com>
2024-10-12 14:54:15 +0530

    test and debug and fix

--- Oct_24/Sqrt.py
+++ Oct_24/Sqrt.py
@@ -2,14 +2,14 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x == 1: return 1
         l, r = 1, x//2
-        while l < r:
+        while l <= r:
             mid = (l + r) // 2
             if mid * mid == x:
                 return mid
             if mid * mid > x:
-                r = mid
+                r = mid - 1
             else:
                 l = mid + 1
         return l - 1
 
-print(Solution().mySqrt(x = 36))
+print(Solution().mySqrt(x = 4))
