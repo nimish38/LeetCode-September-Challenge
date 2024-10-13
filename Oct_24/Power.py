1b3424b5844043ea2f62dde51e0e1d297438823a
Nimish <nimish.v.shinde60@gmail.com>
2024-10-13 21:18:45 +0530

    brute force

--- Oct_24/Power.py
+++ Oct_24/Power.py
@@ -1,5 +1,11 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x ** n
+        res = 1
+        for _ in range(abs(n)):
+            res *= x
+
+        if n < 0:
+            return 1 / res
+        return res
 
 print(Solution().myPow(x = 2.10000, n = 3))
