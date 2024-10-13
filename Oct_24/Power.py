be1c170b9d612d47607fce3a762c3a12f685ceb8
Nimish <nimish.v.shinde60@gmail.com>
2024-10-13 21:36:25 +0530

    binary exponentiation

--- Oct_24/Power.py
+++ Oct_24/Power.py
@@ -1,11 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        res = 1
-        for _ in range(abs(n)):
-            res *= x
-
+        if n == 0:
+            return 1
         if n < 0:
-            return 1 / res
-        return res
+            return self.myPow(1/x, -n)
+        if n % 2:
+            return self.myPow(x*x, n//2)
+        else:
+            return x * self.myPow(x * x, (n-1)//2)
 
 print(Solution().myPow(x = 2.10000, n = 3))
