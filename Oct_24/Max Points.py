fd82355f6ada6d0f77193eb239d4510ca1ab16b1
Nimish <nimish.v.shinde60@gmail.com>
2024-10-14 23:01:35 +0530

    test and debug

--- Oct_24/Max Points.py
+++ Oct_24/Max Points.py
@@ -5,13 +5,20 @@ class Solution:
             for j in range(i + 1, n):
                 cnt = 2
                 x1, y1 = points[i][0], points[i][1]
-                slope = (points[j][1] - y1) / (points[j][0] - x1)
+                if points[j][0] != x1:
+                    slope = (points[j][1] - y1) / (points[j][0] - x1)
+                else:
+                    slope = float('inf')
+                for k in range(n):
+                    if k != i and k != j:
+                        if points[k][0] != x1:
+                            new_slope = (points[k][1] - y1) / (points[k][0] - x1)
+                        else:
+                            new_slope = float('inf')
+                        if slope == new_slope:
+                            cnt += 1
 
-            for k in range(n):
-                if k != i and k != j:
-                    new_slope = (points[k][1] - y1) / (points[k][0] - x1)
-                    if slope == new_slope:
-                        cnt += 1
-
-            res = max(res, cnt)
+                res = max(res, cnt)
         return res
+
+print(Solution().maxPoints(points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]))
