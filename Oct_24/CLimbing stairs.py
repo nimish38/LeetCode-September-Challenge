d50285dee493031816345cc4b1e9b0edb410756c
Nimish <nimish.v.shinde60@gmail.com>
2024-10-16 22:39:10 +0530

    added memoization

--- Oct_24/CLimbing stairs.py
+++ Oct_24/CLimbing stairs.py
@@ -1,11 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        steps = [-1]*(n+1)
+        steps[0] = 1
+
         def solve(x):
             if x < 0:
                 return 0
             if x == 0:
                 return 1
-            return solve(x - 1) + solve(x-2)
+            if steps[x] == -1:
+                steps[x] = solve(x-1) + solve(x-2)
+            return steps[x]
+
         return solve(n)
 
-print(Solution().climbStairs())
+print(Solution().climbStairs(3))
