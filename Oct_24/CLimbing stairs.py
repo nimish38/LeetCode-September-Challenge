307cb70f883d3ceb6c1e9f3bd95e13ad3570b732
Nimish <nimish.v.shinde60@gmail.com>
2024-10-17 22:16:58 +0530

    bottom up approach using only variables

--- Oct_24/CLimbing stairs.py
+++ Oct_24/CLimbing stairs.py
@@ -2,13 +2,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 0 or n == 1 or n == 2:
             return n
-        steps = [-1]*(n+1)
-        steps[1] = 1
-        steps[0] = 0
-        steps[2] = 2
+        one_step, two_step, dest = 1, 2, 3
 
         for i in range(3, n + 1):
-            steps[i] = steps[i - 1] + steps[i - 2]
-        return steps[n]
+            dest = one_step + two_step
+            one_step, two_step = two_step, dest
 
-print(Solution().climbStairs(4))
+        return dest
+
+print(Solution().climbStairs(5))
