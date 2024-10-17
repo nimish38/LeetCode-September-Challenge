7fe3abd2200b723a2aeca1df13b0cde4c13a2b3d
Nimish <nimish.v.shinde60@gmail.com>
2024-10-17 22:02:45 +0530

    bottom up approach using array

--- Oct_24/CLimbing stairs.py
+++ Oct_24/CLimbing stairs.py
@@ -1,17 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         steps = [-1]*(n+1)
-        steps[0] = 1
+        steps[1] = 1
+        steps[0] = 0
+        steps[2] = 2
 
-        def solve(x):
-            if x < 0:
-                return 0
-            if x == 0:
-                return 1
-            if steps[x] == -1:
-                steps[x] = solve(x-1) + solve(x-2)
-            return steps[x]
+        for i in range(3, n + 1):
+            steps[i] = steps[i - 1] + steps[i - 2]
+        return steps[n]
 
-        return solve(n)
-
-print(Solution().climbStairs(3))
+print(Solution().climbStairs(4))
