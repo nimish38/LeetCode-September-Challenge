6894b656f8c68a45250bb2b3bea93dd87183d468
Nimish <nimish.v.shinde60@gmail.com>
2024-10-18 21:42:24 +0530

    recursive and fix bug

--- Oct_24/House Robber.py
+++ Oct_24/House Robber.py
@@ -3,10 +3,12 @@ class Solution:
         n = len(nums)
 
         def solve(ind, curr):
-            if ind == n:
+            if ind >= n:
                 return curr
             incl = solve(ind + 2, curr + nums[ind])
             excl = solve(ind + 1, curr)
             return max(incl, excl)
-        
+
         return solve(0, 0)
+
+print(Solution().rob(nums = [2,7,9,3,1]))
