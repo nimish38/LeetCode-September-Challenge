a253d76ad8671dfe10f555bdb8f6fbb927fcab46
Nimish <nimish.v.shinde60@gmail.com>
2024-10-18 22:31:51 +0530

    bottom up with memoization

--- Oct_24/House Robber.py
+++ Oct_24/House Robber.py
@@ -1,15 +1,29 @@
 class Solution:
     def rob(self, nums) -> int:
+        # n = len(nums)
+        # if n == 1:
+        #     return nums[0]
+        #
+        # amt = [-1]*(n + 1)
+        # amt[0], amt[1] = 0, nums[0]
+        #
+        # for i in range(2, n + 1):
+        #     amt[i] = max(amt[i - 1], amt[i - 2] + nums[i - 1])
+        # return amt[n]
+
         n = len(nums)
-        if n == 1:
-            return nums[0]
+        amt = [-1] * (n)
 
-        amt = [-1]*(n + 1)
-        amt[0], amt[1] = 0, nums[0]
+        def solve(ind):
+            if ind >= n:
+                return 0
+            if amt[ind] == -1:
+                incl = solve(ind + 2) + nums[ind]
+                excl = solve(ind + 1)
+                amt[ind] = max(incl, excl)
+            return amt[ind]
 
-        for i in range(2, n + 1):
-            amt[i] = max(amt[i - 1], amt[i - 2] + nums[i - 1])
-        return amt[n]
+        return solve(0)
 
 
 print(Solution().rob(nums = [2,7,9,3,1]))
