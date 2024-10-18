39409ec1c95b88f12d0b12d9bbd82c5d07292186
Nimish <nimish.v.shinde60@gmail.com>
2024-10-18 21:34:28 +0530

    try recursive approach

---
+++ Oct_24/House Robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums) -> int:
+        n = len(nums)
+
+        def solve(ind, curr):
+            if ind == n:
+                return curr
+            incl = solve(ind + 2, curr + nums[ind])
+            excl = solve(ind + 1, curr)
+            return max(incl, excl)
+        
+        return solve(0, 0)
