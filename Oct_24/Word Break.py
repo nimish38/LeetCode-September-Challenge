803cb04db90c741b16a5f18ad7b9579444e5cdd1
Nimish <nimish.v.shinde60@gmail.com>
2024-10-19 11:25:07 +0530

    recursive attempt

--- Oct_24/Word Break.py
+++ Oct_24/Word Break.py
@@ -3,13 +3,11 @@ class Solution:
         n = len(s)
 
         def solve(ind):
-            if ind >= n:
-                return False
-            for i in range(1, n - ind - 1):
-                if s[ind: ind + i] in wordDict:
-                    if s[ind + i + 1:] in wordDict:
-                        return True
-                    return solve(ind + i + 1)
+            if ind >= n or s in wordDict:
+                return True
+            for i in range(1, n):
+                if s[ind: ind + i] in wordDict and solve(ind + i):
+                    return True
             return False
 
-        return solve(o)
+        return solve(0)
