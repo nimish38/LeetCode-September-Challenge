f50fdfb037685835c5aaf9930289a0d7b5396d0f
Nimish <nimish.v.shinde60@gmail.com>
2024-10-19 11:30:22 +0530

    use dictionary for quicker access

--- Oct_24/Word Break.py
+++ Oct_24/Word Break.py
@@ -1,13 +1,17 @@
 class Solution:
     def wordBreak(self, s: str, wordDict) -> bool:
-        n = len(s)
+        n, words = len(s), {}
+        for ele in wordDict:
+            words[ele] = 1
 
         def solve(ind):
-            if ind >= n or s in wordDict:
+            if ind >= n or s in words:
                 return True
             for i in range(1, n):
-                if s[ind: ind + i] in wordDict and solve(ind + i):
+                if s[ind: ind + i] in words and solve(ind + i):
                     return True
             return False
 
         return solve(0)
+
+print(Solution().wordBreak(s = "catsandog", wordDict = ["cats","og","sand","and","cat"]))
