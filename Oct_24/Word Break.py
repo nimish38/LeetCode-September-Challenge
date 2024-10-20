20a8b98675b62b7746955874c4d7333222ed7d00
Nimish <nimish.v.shinde60@gmail.com>
2024-10-20 17:57:29 +0530

    use dictionary for quicker access

--- Oct_24/Word Break.py
+++ Oct_24/Word Break.py
@@ -3,14 +3,18 @@ class Solution:
         n, words = len(s), {}
         for ele in wordDict:
             words[ele] = 1
+        memo = [-1] * (n + 1)
 
         def solve(ind):
             if ind >= n or s in words:
                 return True
-            for i in range(1, n):
-                if s[ind: ind + i] in words and solve(ind + i):
-                    return True
-            return False
+            if memo[ind] == -1:
+                for i in range(1, n):
+                    if s[ind: ind + i] in words and solve(ind + i):
+                        memo[ind] = True
+                        return True
+                memo[ind] = False
+            return memo[ind]
 
         return solve(0)
 
