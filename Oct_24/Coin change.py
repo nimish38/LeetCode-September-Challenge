d1f531d63a4cf7ae0ad9bc5543552511060e23e5
Nimish <nimish.v.shinde60@gmail.com>
2024-10-20 19:22:00 +0530

    memoization

--- Oct_24/Coin change.py
+++ Oct_24/Coin change.py
@@ -2,15 +2,20 @@ class Solution:
     def coinChange(self, coins, amount: int) -> int:
         if amount == 0:
             return 0
+        memo = [-1]*(amount + 1)
+
         def solve(change):
             if change in coins:
                 return 1
-            res = float('inf')
-            for coin in coins:
-                if change - coin > 0:
-                    temp = 1 + solve(change - coin)
-                    res = min(temp, res)
-            return res
+            if memo[change] == -1:
+                res = float('inf')
+                for coin in coins:
+                    if change - coin > 0:
+                        temp = 1 + solve(change - coin)
+                        res = min(temp, res)
+                memo[change] = res
+            return memo[change]
+
         ans = solve(amount)
         if ans == float('inf'):
             return -1
