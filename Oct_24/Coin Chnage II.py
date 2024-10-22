d3881e5446134671b308134d21960e34d9743f62
Nimish <nimish.v.shinde60@gmail.com>
2024-10-22 21:47:57 +0530

    added memoization

--- Oct_24/Coin Chnage II.py
+++ Oct_24/Coin Chnage II.py
@@ -2,14 +2,16 @@ class Solution:
     def change(self, amount: int, coins) -> int:
         if amount == 0:
             return 0
-        self.temp = 0
+        self.temp, memo = 0, [False]*(amount+1)
 
         def solve(amt):
-            for coin in coins:
-                if amt - coin == 0:
-                    self.temp += 1
-                elif amt - coin > 0:
-                    solve(amt - coin)
+            if not memo[amt]:
+                for coin in coins:
+                    if amt - coin == 0:
+                        self.temp += 1
+                    elif amt - coin > 0:
+                        solve(amt - coin)
+                memo[amt] = True
         solve(amount)
         return self.temp
         # return self.res
