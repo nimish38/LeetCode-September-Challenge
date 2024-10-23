93a6bab93aebe7eebe7de3045b0a17514e7d97a7
Nimish <nimish.v.shinde60@gmail.com>
2024-10-23 22:14:10 +0530

    recursively track index and amount

--- Oct_24/Coin Chnage II.py
+++ Oct_24/Coin Chnage II.py
@@ -2,18 +2,19 @@ class Solution:
     def change(self, amount: int, coins) -> int:
         if amount == 0:
             return 0
-        self.temp, memo = 0, [False]*(amount+1)
+        n = len(coins)
 
-        def solve(amt):
-            if not memo[amt]:
-                for coin in coins:
-                    if amt - coin == 0:
-                        self.temp += 1
-                    elif amt - coin > 0:
-                        solve(amt - coin)
-                memo[amt] = True
-        solve(amount)
-        return self.temp
-        # return self.res
+        def solve(ind, amt):
+            if ind >= n:
+                return 0
+            if amt == 0:
+                return 1
+            if amt < coins[ind]:
+                return solve(ind + 1, amt)
+            take = solve(ind, amt - coins[ind])
+            skip = solve(ind + 1, amt)
+            return take + skip
+
+        return solve(0, amount)
 
 print(Solution().change(amount = 5, coins = [1,2,5]))
