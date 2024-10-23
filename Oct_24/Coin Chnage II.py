fea0448bab4a7dc4432c08f036dabec45ee61bee
Nimish <nimish.v.shinde60@gmail.com>
2024-10-23 22:18:06 +0530

    memoization

--- Oct_24/Coin Chnage II.py
+++ Oct_24/Coin Chnage II.py
@@ -1,19 +1,21 @@
 class Solution:
     def change(self, amount: int, coins) -> int:
-        if amount == 0:
-            return 0
-        n = len(coins)
+        n, memo = len(coins), []
+        for _ in range(n + 1):
+            memo.append([-1] * (amount + 1))
 
         def solve(ind, amt):
             if ind >= n:
                 return 0
             if amt == 0:
                 return 1
-            if amt < coins[ind]:
-                return solve(ind + 1, amt)
-            take = solve(ind, amt - coins[ind])
-            skip = solve(ind + 1, amt)
-            return take + skip
+            if memo[ind][amt] == -1:
+                if amt < coins[ind]:
+                    return solve(ind + 1, amt)
+                take = solve(ind, amt - coins[ind])
+                skip = solve(ind + 1, amt)
+                memo[ind][amt] = take + skip
+            return memo[ind][amt]
 
         return solve(0, amount)
 
