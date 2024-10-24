186077c9fbee696f436e5baf73bd4ae163cb5149
Nimish <nimish.v.shinde60@gmail.com>
2024-10-24 22:25:57 +0530

    test solution

--- Oct_24/Longest Incr Subseq.py
+++ Oct_24/Longest Incr Subseq.py
@@ -5,6 +5,7 @@ class Solution:
         def solve(ind, last):
             if ind == n:
                 return 0
+            take = 0
             if nums[ind] > last:
                 take = 1 + solve(ind + 1, nums[ind])
             skip = solve(ind + 1, last)
@@ -12,4 +13,4 @@ class Solution:
 
         return solve(0, last)
 
-print(Solution().lengthOfLIS())
+print(Solution().lengthOfLIS(nums = [0,1,0,3,2,3]))
