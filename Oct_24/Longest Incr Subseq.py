b72ae06d4055b76beea94278c143655574b0c084
Nimish <nimish.v.shinde60@gmail.com>
2024-10-24 22:29:58 +0530

    added memoization

--- Oct_24/Longest Incr Subseq.py
+++ Oct_24/Longest Incr Subseq.py
@@ -1,16 +1,20 @@
 class Solution:
     def lengthOfLIS(self, nums) -> int:
         n, last = len(nums), float('-inf')
+        memo = [-1] * (n + 1)
 
         def solve(ind, last):
             if ind == n:
                 return 0
-            take = 0
-            if nums[ind] > last:
-                take = 1 + solve(ind + 1, nums[ind])
-            skip = solve(ind + 1, last)
-            return max(take, skip)
+            if memo[ind] == -1:
+                take = 0
+                if nums[ind] > last:
+                    take = 1 + solve(ind + 1, nums[ind])
+                skip = solve(ind + 1, last)
+                memo[ind] = max(take, skip)
+            return memo[ind]
 
-        return solve(0, last)
+        solve(0, last)
+        return memo[n]
 
 print(Solution().lengthOfLIS(nums = [0,1,0,3,2,3]))
