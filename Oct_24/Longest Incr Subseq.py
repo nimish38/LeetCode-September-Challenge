6742906059775da3a6cdb784bd0214cd97cb10af
Nimish <nimish.v.shinde60@gmail.com>
2024-10-26 13:48:34 +0530

    bottom up storing state LIS in array

--- Oct_24/Longest Incr Subseq.py
+++ Oct_24/Longest Incr Subseq.py
@@ -4,10 +4,10 @@ class Solution:
         dp = [1] * n
 
         for i in range(1, n):
-            for j in range(n, i):
+            for j in range(i):
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], 1 + dp[j])
-                    res = max(res, dp[i])
+            res = max(res, dp[i])
         return res
 
 print(Solution().lengthOfLIS(nums = [0,1,0,3,2,3]))
