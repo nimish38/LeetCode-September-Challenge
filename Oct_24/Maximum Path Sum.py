b800d9f63dbe4200d5105a088e701f5bd8f4dc81
Nimish <nimish.v.shinde60@gmail.com>
2024-10-29 22:10:11 +0530

    recursively explore

---
+++ Oct_24/Maximum Path Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPathSum(self, grid) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def solve(row, col):
+            if row < 0 or col < 0:
+                return float('inf')
+            if row == 0 and col == 0:
+                return grid[0][0]
+            left = solve(row, col - 1)
+            up = solve(row - 1, col)
+            return grid[row][col] + min(left, up)
+
+        return solve(m - 1, n - 1)
