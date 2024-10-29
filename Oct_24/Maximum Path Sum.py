b473e24d46beb9f5d0442d4bb379a0b3c310cc11
Nimish <nimish.v.shinde60@gmail.com>
2024-10-29 22:52:53 +0530

    top down

--- Oct_24/Maximum Path Sum.py
+++ Oct_24/Maximum Path Sum.py
@@ -1,20 +1,14 @@
 class Solution:
     def minPathSum(self, grid) -> int:
-        m, n, memo = len(grid), len(grid[0]), []
-        for _ in range(m):
-            memo.append([-1]*n)
-
-        def solve(row, col):
-            if row < 0 or col < 0:
-                return float('inf')
-            if row == 0 and col == 0:
-                return grid[0][0]
-            if memo[row][col] == -1:
-                left = solve(row, col - 1)
-                up = solve(row - 1, col)
-                memo[row][col] = grid[row][col] + min(left, up)
-            return memo[row][col]
-
-        return solve(m - 1, n - 1)
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        for j in range(1, n):
+            grid[0][j] += grid[0][j-1]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        return grid[m-1][n-1]
 
 print(Solution().minPathSum(grid = [[1,2,3],[4,5,6]]))
