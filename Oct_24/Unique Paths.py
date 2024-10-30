be8effe23861be0f5b72a953863a4def304dcf38
Nimish <nimish.v.shinde60@gmail.com>
2024-10-30 22:23:47 +0530

    debug and check

--- Oct_24/Unique Paths.py
+++ Oct_24/Unique Paths.py
@@ -2,22 +2,22 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         memo = []
+        for _ in range(m):
+            memo.append([0]*n)
 
         for i in range(m):
             if not obstacleGrid[i][0]:
                 memo[i][0] = 1
-            else:
-                memo[i][0] = 0
 
         for i in range(n):
             if not obstacleGrid[0][i]:
                 memo[0][i] = 1
-            else:
-                memo[0][i] = 0
 
         for i in range(1, m):
             for j in range(1, n):
-                memo[i][j] = memo[i - 1][j] + memo[i][j - 1]
+                if not obstacleGrid[i][j]:
+                    memo[i][j] = memo[i - 1][j] + memo[i][j - 1]
 
         return memo[m - 1][n - 1]
 
+print(Solution().uniquePathsWithObstacles(obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]))
