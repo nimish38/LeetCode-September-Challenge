33c500962af9931d2b4519ae2636523d37da61dd
Nimish <nimish.v.shinde60@gmail.com>
2024-10-31 22:21:03 +0530

    brute force

---
+++ Oct_24/Longest Palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n, maxlen, res = len(s), 1, ''
+        for i in range(n):
+            for j in range(i, n):
+                if j + 1 - i > maxlen:
+                    x = s[i:j+1]
+                    if x == x[::-1]:
+                        maxlen, res = len(x), x
+        return res
