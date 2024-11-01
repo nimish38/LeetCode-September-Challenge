bf54359fcea646ba5fa3e63e3f02eb6d30746a3b
Nimish <nimish.v.shinde60@gmail.com>
2024-11-01 15:18:01 +0530

    bottom up edge case

--- Oct_24/Longest Palindrome.py
+++ Oct_24/Longest Palindrome.py
@@ -6,7 +6,7 @@ class Solution:
             memo[_][_] = True
             res = s[_]
 
-        for l in range(2, n):
+        for l in range(2, n + 1):
             for i in range(n - l + 1):
                 j = i + l - 1
                 if s[i] == s[j] and (l == 2 or memo[i + 1][j - 1]):
