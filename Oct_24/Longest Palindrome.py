5cd87aed856c1226eff70b5c75ef3b48bcc534d9
Nimish <nimish.v.shinde60@gmail.com>
2024-11-01 14:00:37 +0530

    palindrome check with recursion

--- Oct_24/Longest Palindrome.py
+++ Oct_24/Longest Palindrome.py
@@ -1,12 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n, maxlen, res = len(s), 0, ''
+
+        def solve(i, j):
+            if i >= j:
+                return True
+            if s[i] == s[j]:
+                return solve(i + 1, j - 1)
+            else:
+                return False
+
         for i in range(n):
             for j in range(i, n):
                 if j + 1 - i > maxlen:
-                    x = s[i:j+1]
-                    if x == x[::-1]:
-                        maxlen, res = len(x), x
+                    if solve(i, j):
+                        maxlen, res = j + 1 - i, s[i: j + 1]
         return res
 
 print(Solution().longestPalindrome(s = "babad"))
