e71d89ae631e756c3e621e26eb8ca2c4d4fe758e
Nimish <nimish.v.shinde60@gmail.com>
2024-11-01 15:16:26 +0530

    bottom up edge case

--- Oct_24/Longest Palindrome.py
+++ Oct_24/Longest Palindrome.py
@@ -1,19 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if s == s[::-1]:
-            return s
-        n, maxlen, res, memo = len(s), 0, '', []
-        for _ in range(n + 1):
-            memo.append([-1]*(n+1))
+        n, res, memo = len(s), '', []
+        for _ in range(n):
+            memo.append([-1]*(n))
             memo[_][_] = True
+            res = s[_]
 
         for l in range(2, n):
             for i in range(n - l + 1):
-                for j in range(i, n):
-                    if s[i] == s[j] and (l == 2 or memo[i + 1][j - 1]):
-                        res, memo[i][j] = s[i: j + 1], True
-                    else:
-                        memo[i][j] = False
+                j = i + l -1
+                if s[i] == s[j] and (l == 2 or memo[i + 1][j - 1]):
+                    res, memo[i][j] = s[i: j + 1], True
+                else:
+                    memo[i][j] = False
         return res
 
-print(Solution().longestPalindrome(s = "babad"))
+print(Solution().longestPalindrome(s = "a"))
