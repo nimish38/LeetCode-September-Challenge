bffd1112d0e16f40865856c7292b5a58c796c6cf
Nimish <nimish.v.shinde60@gmail.com>
2024-11-01 14:21:48 +0530

    palindrome check with recursion and memoization

--- Oct_24/Longest Palindrome.py
+++ Oct_24/Longest Palindrome.py
@@ -1,14 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n, maxlen, res = len(s), 0, ''
+        n, maxlen, res, memo = len(s), 0, '', []
+        for _ in range(n + 1):
+            memo.append([-1]*(n+1))
 
         def solve(i, j):
             if i >= j:
                 return True
-            if s[i] == s[j]:
-                return solve(i + 1, j - 1)
-            else:
-                return False
+            if memo[i][j] == -1:
+                if s[i] == s[j]:
+                    memo[i][j] = solve(i + 1, j - 1)
+                else:
+                    memo[i][j] = False
+            return memo[i][j]
 
         for i in range(n):
             for j in range(i, n):
