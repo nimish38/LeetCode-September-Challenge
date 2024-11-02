a9801be0d97dbf7f805a6a3e871b0b938c60be3b
Nimish <nimish.v.shinde60@gmail.com>
2024-11-02 14:10:34 +0530

    changed memoization to 2d

--- Nov_24/Interleaving String.py
+++ Nov_24/Interleaving String.py
@@ -4,22 +4,19 @@ class Solution:
         if m + n != o:
             return False
         for _ in range(m+1):
-            l = []
-            for x in range(n+1):
-                l.append([-1] * (o+1))
-            memo.append(l)
+            memo.append([-1] * (n+1))
 
         def solve(i, j, k):
             if i == m and j == n and k == o:
                 return True
-            if memo[i][j][k] == -1:
+            if memo[i][j] == -1:
                 first, second = False, False
                 if j < n and s3[k] == s2[j]:
                     second = solve(i, j + 1, k + 1)
-                if i < m and  s3[k] == s1[i]:
+                if i < m and s3[k] == s1[i]:
                     first = solve(i + 1, j, k + 1)
-                memo[i][j][k] =  first or second
-            return memo[i][j][k]
+                memo[i][j] = first or second
+            return memo[i][j]
 
         return solve(0, 0, 0)
 
