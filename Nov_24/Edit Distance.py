3ca3e875ff498002df3d85b4d19201500f4a1a67
Nimish <nimish.v.shinde60@gmail.com>
2024-11-11 22:27:39 +0530

    added memoization

--- Nov_24/Edit Distance.py
+++ Nov_24/Edit Distance.py
@@ -1,16 +1,20 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
+        m, n, memo = len(word1), len(word2), []
+        for _ in range(m):
+            memo.append([-1]*n)
 
         def solve(i, j):
             if i >= m or j >= n:
                 return (m - i) + (n - j)
-            if word1[i] == word2[j]:
-                return solve(i + 1, j + 1)
-            add = 1 + solve(i, j + 1)
-            delete = 1 + solve(i + 1, j)
-            replace = 1 + solve(1 + i, j + 1)
-            return min(add, delete, replace)
+            if memo[i][j] == -1:
+                if word1[i] == word2[j]:
+                    return solve(i + 1, j + 1)
+                add = 1 + solve(i, j + 1)
+                delete = 1 + solve(i + 1, j)
+                replace = 1 + solve(1 + i, j + 1)
+                memo[i][j] = min(add, delete, replace)
+            return memo[i][j]
 
         return solve(0, 0)
 
