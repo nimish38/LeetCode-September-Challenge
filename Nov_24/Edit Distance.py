10c699b092f0f5ce42d9f9c249a68e56cb9285e7
Nimish <nimish.v.shinde60@gmail.com>
2024-11-11 22:24:30 +0530

    debug and check

--- Nov_24/Edit Distance.py
+++ Nov_24/Edit Distance.py
@@ -3,14 +3,15 @@ class Solution:
         m, n = len(word1), len(word2)
 
         def solve(i, j):
-            if i >= m and j >= n:
-                return 0
-            if i >= m:
-                add = 1 + solve(i, j + 1)
+            if i >= m or j >= n:
+                return (m - i) + (n - j)
             if word1[i] == word2[j]:
                 return solve(i + 1, j + 1)
+            add = 1 + solve(i, j + 1)
             delete = 1 + solve(i + 1, j)
             replace = 1 + solve(1 + i, j + 1)
             return min(add, delete, replace)
 
         return solve(0, 0)
+
+print(Solution().minDistance(word1 = "intention", word2 = "execution"))
