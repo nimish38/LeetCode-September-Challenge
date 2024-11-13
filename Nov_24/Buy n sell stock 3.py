fa1c6ad548040ee5af3c3db19540f5e63027b9df
Nimish <nimish.v.shinde60@gmail.com>
2024-11-13 21:26:37 +0530

    recursive with 2 transactions cap

---
+++ Nov_24/Buy n sell stock 3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        n = len(prices)
+
+        def solve(ind, buy, cap):
+            if ind == n or cap == 0:
+                return 0
+            if buy:
+                take = solve(ind + 1, False, cap) - prices[ind]
+                hold = solve(ind + 1, True, cap)
+            else:
+                take = solve(ind + 1, True, cap - 1) + prices[ind]
+                hold = solve(ind + 1, False, cap)
+            return max(take, hold)
+
+        return solve(0, True, 2)
