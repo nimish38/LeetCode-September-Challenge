c8ddafdf4e686e06d862419daa907d38b41d12bf
Nimish <nimish.v.shinde60@gmail.com>
2024-11-13 21:35:34 +0530

    memoization

--- Nov_24/Buy n sell stock 3.py
+++ Nov_24/Buy n sell stock 3.py
@@ -1,18 +1,25 @@
 class Solution:
     def maxProfit(self, prices) -> int:
-        n = len(prices)
+        n, memo = len(prices), []
+        for _ in range(n):
+            x = []
+            for z in range(3):
+                x.append([-1] * 2)
+            memo.append(x)
 
         def solve(ind, buy, cap):
             if ind == n or cap == 0:
                 return 0
-            if buy:
-                take = solve(ind + 1, False, cap) - prices[ind]
-                hold = solve(ind + 1, True, cap)
-            else:
-                take = solve(ind + 1, True, cap - 1) + prices[ind]
-                hold = solve(ind + 1, False, cap)
-            return max(take, hold)
+            if memo[ind][cap][buy] == -1:
+                if buy:
+                    take = solve(ind + 1, 0, cap) - prices[ind]
+                    hold = solve(ind + 1, 1, cap)
+                else:
+                    take = solve(ind + 1, 1, cap - 1) + prices[ind]
+                    hold = solve(ind + 1, 0, cap)
+                memo[ind][cap][buy] = max(take, hold)
+            return memo[ind][cap][buy]
 
-        return solve(0, True, 2)
+        return solve(0, 1, 2)
 
 print(Solution().maxProfit(prices = [1,2,3,4,5]))
