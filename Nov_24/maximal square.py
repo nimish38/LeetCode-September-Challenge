f24fc98c5c27741272cebe3f72fd836a39a04c75
Nimish <nimish.v.shinde60@gmail.com>
2024-11-15 22:45:14 +0530

    added memoization

--- Nov_24/maximal square.py
+++ Nov_24/maximal square.py
@@ -1,20 +1,24 @@
 class Solution:
     def maximalSquare(self, matrix) -> int:
-        m, n, self.best = len(matrix), len(matrix[0]), 0
+        m, n, self.best, memo = len(matrix), len(matrix[0]), 0, []
+        for _ in range(m):
+            memo.append([-1]*n)
 
         def solve(i, j):
             if i >= m or j >= n:
                 return 0
-            right = solve(i, j + 1)
-            diag = solve(i + 1, j + 1)
-            down = solve(i + 1, j)
+            if memo[i][j] == -1:
+                right = solve(i, j + 1)
+                diag = solve(i + 1, j + 1)
+                down = solve(i + 1, j)
 
-            if matrix[i][j] == '1':
-                ans = 1 + min(right, down, diag)
-                self.best = max(self.best, ans)
-                return ans
-            else:
-                return 0
+                if matrix[i][j] == '1':
+                    ans = 1 + min(right, down, diag)
+                    self.best = max(self.best, ans)
+                    memo[i][j] = ans
+                else:
+                    memo[i][j] = 0
+            return memo[i][j]
 
         solve(0, 0)
         return self.best ** 2
