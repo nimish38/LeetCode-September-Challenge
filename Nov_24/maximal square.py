57f394cadd44b3bd697cf11baeec356ebc4489d8
Nimish <nimish.v.shinde60@gmail.com>
2024-11-15 22:54:34 +0530

    top down approach

--- Nov_24/maximal square.py
+++ Nov_24/maximal square.py
@@ -4,23 +4,23 @@ class Solution:
         for _ in range(m):
             memo.append([-1]*n)
 
-        def solve(i, j):
-            if i >= m or j >= n:
-                return 0
-            if memo[i][j] == -1:
-                right = solve(i, j + 1)
-                diag = solve(i + 1, j + 1)
-                down = solve(i + 1, j)
+        for col in range(n):
+            memo[m - 1][col] = int(matrix[m - 1][col])
+        for row in range(m):
+            memo[row][n - 1] = int(matrix[row][n - 1])
 
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
                 if matrix[i][j] == '1':
+                    right = memo[i][j + 1]
+                    diag = memo[i + 1][j + 1]
+                    down = memo[i + 1][j]
                     ans = 1 + min(right, down, diag)
                     self.best = max(self.best, ans)
                     memo[i][j] = ans
                 else:
                     memo[i][j] = 0
-            return memo[i][j]
 
-        solve(0, 0)
         return self.best ** 2
 
 print(Solution().maximalSquare(matrix=[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
