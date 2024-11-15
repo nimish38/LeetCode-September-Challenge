5c3595bdb201d4f4b75bb1c4bf5dd3d2e58068ff
Nimish <nimish.v.shinde60@gmail.com>
2024-11-15 22:39:33 +0530

    test and debug

--- Nov_24/maximal square.py
+++ Nov_24/maximal square.py
@@ -3,17 +3,20 @@ class Solution:
         m, n, self.best = len(matrix), len(matrix[0]), 0
 
         def solve(i, j):
-            if i == m or j == n:
+            if i >= m or j >= n:
                 return 0
             right = solve(i, j + 1)
             diag = solve(i + 1, j + 1)
             down = solve(i + 1, j)
 
-            if matrix[i][j]:
+            if matrix[i][j] == '1':
                 ans = 1 + min(right, down, diag)
                 self.best = max(self.best, ans)
                 return ans
+            else:
+                return 0
 
         solve(0, 0)
-        return self.best
-        
+        return self.best ** 2
+
+print(Solution().maximalSquare(matrix=[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
