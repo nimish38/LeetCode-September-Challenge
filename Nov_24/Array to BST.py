f4abe050559947e5f0b1aed53bd30a4c165311d9
Nimish <nimish.v.shinde60@gmail.com>
2024-11-17 13:43:04 +0530

    build tree and test

--- Nov_24/Array to BST.py
+++ Nov_24/Array to BST.py
@@ -15,4 +15,7 @@ class Solution:
         root.left = self.sortedArrayToBST(nums[:mid])
         root.right = self.sortedArrayToBST(nums[mid + 1:])
         return root
-    
+
+
+x = Solution().sortedArrayToBST([-10,-3,0,5,9])
+print(x.val)
