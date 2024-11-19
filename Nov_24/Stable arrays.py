194a58bd469d44e24de06130e40d43d2ce92fa62
Nimish <nimish.v.shinde60@gmail.com>
2024-11-19 22:14:13 +0530

    recursive attempt

--- Nov_24/Stable arrays.py
+++ Nov_24/Stable arrays.py
@@ -1,3 +1,12 @@
 class Solution:
     def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
-        return 0
+        def solve(zero, one, consCnt, consEle):
+            if zero == 0 and one == 0:
+                return 1
+            if zero > 0 and consCnt <= limit or consEle == 1:
+                solve(zero - 1, one, consCnt + 1, 0)
+
+            if one > 0 and consCnt <= limit or consEle == 0:
+                solve(zero, one - 1, consCnt + 1, 1)
+
+        return solve(zero, one - 1, 1, 1) + solve(zero - 1, one, 1, 0)
