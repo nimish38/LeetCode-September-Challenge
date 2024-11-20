0bfe5c51a6d44f7957a4c50a0bd8bcee70c07d03
Nimish <nimish.v.shinde60@gmail.com>
2024-11-20 12:55:31 +0530

    debug and test and fix

--- Nov_24/Stable arrays.py
+++ Nov_24/Stable arrays.py
@@ -8,13 +8,15 @@ class Solution:
                 if consEle == 0 and consCnt <= limit:
                     res += solve(zero - 1, one, consCnt + 1, 0)
                 else:
-                    res += solve(zero, one - 1, 1, 1)
+                    res += solve(zero - 1, one, 1, 0)
 
             if one > 0:
                 if consEle == 1 and consCnt <= limit:
                     res += solve(zero, one - 1, consCnt + 1, 1)
                 else:
-                    res += solve(zero - 1, one, 1, 0)
+                    res += solve(zero, one - 1, 1, 1)
 
             return res
         return solve(zero, one - 1, 1, 1) + solve(zero - 1, one, 1, 0)
+
+print(Solution().numberOfStableArrays(zero = 1, one = 1, limit = 2))
