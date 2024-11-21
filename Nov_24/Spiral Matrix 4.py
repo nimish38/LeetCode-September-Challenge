485b437789c264e56980fb9ec5f98e5e3b37743f
Nimish <nimish.v.shinde60@gmail.com>
2024-11-21 20:50:41 +0530

    spiral traversal

--- Nov_24/Spiral Matrix 4.py
+++ Nov_24/Spiral Matrix 4.py
@@ -12,6 +12,24 @@ class Solution:
             for j in range(n):
                 x.append((i, j))
             mat.append(x)
-        return mat
 
-print(Solution().spiralMatrix(3, 5, None))
+        up, down, left, right = 0, m - 1, 0, n - 1
+        while up <= down and left <= right:
+
+            for i in range(left, right + 1):
+                print(mat[up][i])
+            up += 1
+
+            for i in range(up, down + 1):
+                print(mat[i][right])
+            right -= 1
+
+            for i in range(right, left - 1, -1):
+                print(mat[down][i])
+            down -= 1
+
+            for i in range(down, up - 1, -1):
+                print(mat[i][left])
+            left += 1
+
+print(Solution().spiralMatrix(4, 3, None))
