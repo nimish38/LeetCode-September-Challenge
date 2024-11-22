43fa9da73c8ccbe50df3149d27c8c46e651535ec
Nimish <nimish.v.shinde60@gmail.com>
2024-11-22 22:20:19 +0530

    Multi level trees

--- Nov_24/Binary Tree Factors.py
+++ Nov_24/Binary Tree Factors.py
@@ -1,16 +1,17 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr) -> int:
         fact = [1]*(len(arr))
+        arr.sort()
 
         def solve(ind):
             for i in range(ind):
                 for j in range(ind):
                     if arr[i] * arr[j] == arr[ind]:
-                        fact[ind] += 1
+                        fact[ind] += 1 + (fact[i] - 1) + (fact[j] - 1)
 
         for i in range(1, len(arr)):
             solve(i)
         return sum(fact)
 
 
-print(Solution().numFactoredBinaryTrees(arr = [2,4,5,10]))
+print(Solution().numFactoredBinaryTrees(arr = [2,4,8]))
