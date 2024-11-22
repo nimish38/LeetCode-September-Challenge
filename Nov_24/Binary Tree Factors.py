93d00ac16052834b1c4be47ce1b7712cde42d3cd
Nimish <nimish.v.shinde60@gmail.com>
2024-11-22 21:48:21 +0530

    Brute force check product

--- Nov_24/Binary Tree Factors.py
+++ Nov_24/Binary Tree Factors.py
@@ -1,7 +1,16 @@
 class Solution:
-    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        cnt, fact = len(arr), [0]*(len(arr))
-        for i in range(1, len(arr)):
-            fact[i] = solve(i)
+    def numFactoredBinaryTrees(self, arr) -> int:
+        fact = [1]*(len(arr))
 
-        return sum(fact) + cnt
+        def solve(ind):
+            for i in range(ind):
+                for j in range(ind):
+                    if arr[i] * arr[j] == arr[ind]:
+                        fact[ind] += 1
+
+        for i in range(1, len(arr)):
+            solve(i)
+        return sum(fact)
+
+
+print(Solution().numFactoredBinaryTrees(arr = [2,4,5,10]))
