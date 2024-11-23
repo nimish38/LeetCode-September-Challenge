0e263eb93741981d6ac35d334a5c3bba66f3ea07
Nimish <nimish.v.shinde60@gmail.com>
2024-11-23 13:15:19 +0530

    using dictionary and mod operator

--- Nov_24/Binary Tree Factors.py
+++ Nov_24/Binary Tree Factors.py
@@ -1,17 +1,21 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr) -> int:
-        fact = [1]*(len(arr))
-        arr.sort()
+        fact, n = {}, len(arr)
+        for val in arr:
+            fact[val] = 1
 
         def solve(ind):
-            for i in range(ind):
-                for j in range(ind):
-                    if arr[i] * arr[j] == arr[ind]:
-                        fact[ind] += 1 + (fact[i] - 1) + (fact[j] - 1)
+            for i in range(n):
+                if i == ind:
+                    continue
+                if arr[ind] % arr[i] == 0:
+                    x = arr[ind] // arr[i]
+                    if x in fact:
+                        fact[arr[ind]] += 1 + (fact[arr[i]] - 1) + (fact[x] - 1)
 
-        for i in range(1, len(arr)):
+        for i in range(len(arr)):
             solve(i)
-        return sum(fact)
+        return sum(fact.values())
 
 
 print(Solution().numFactoredBinaryTrees(arr = [2,4,8]))
