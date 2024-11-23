aa3a8b92f594bdf09eeab3624a726b01c845471e
Nimish <nimish.v.shinde60@gmail.com>
2024-11-23 13:37:49 +0530

    sort and modulo

--- Nov_24/Binary Tree Factors.py
+++ Nov_24/Binary Tree Factors.py
@@ -1,13 +1,12 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr) -> int:
         fact, n = {}, len(arr)
+        arr.sort()
         for val in arr:
             fact[val] = 1
 
         def solve(ind):
-            for i in range(n):
-                if i == ind:
-                    continue
+            for i in range(ind):
                 if arr[ind] % arr[i] == 0:
                     x = arr[ind] // arr[i]
                     if x in fact:
@@ -15,7 +14,7 @@ class Solution:
 
         for i in range(len(arr)):
             solve(i)
-        return sum(fact.values())
+        return sum(fact.values()) % ((10**9) + 7)
 
 
 print(Solution().numFactoredBinaryTrees(arr = [2,4,8]))
