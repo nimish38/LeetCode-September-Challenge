f65e4b8545018be7c9d9824397fe0679c5cbf65c
Nimish <nimish.v.shinde60@gmail.com>
2024-11-24 13:50:22 +0530

    storing tuple of index in heap

--- Nov_24/Final array state.py
+++ Nov_24/Final array state.py
@@ -3,11 +3,16 @@ import heapq
 
 class Solution:
     def getFinalState(self, nums, k: int, multiplier: int):
-        heapq.heapify(nums)
+        arr = []
+        for i in range(len(nums)):
+            arr.append((nums[i], i))
+        heapq.heapify(arr)
         for _ in range(k):
-            val = heapq.heappop(nums)
-            heapq.heappush(nums, val * multiplier)
+            val, ind = heapq.heappop(arr)
+            heapq.heappush(arr, (val * multiplier, ind))
 
+        for _ in range(len(nums)):
+            nums[arr[_][1]] = arr[_][0]
         return nums
 
 print(Solution().getFinalState(nums = [2,1,3,5,6], k = 5, multiplier = 2))
