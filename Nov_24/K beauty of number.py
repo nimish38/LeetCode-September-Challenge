03cafa96f48deec79f7c16765201c41f79b8569e
Nimish <nimish.v.shinde60@gmail.com>
2024-11-25 22:16:45 +0530

    test solution

--- Nov_24/K beauty of number.py
+++ Nov_24/K beauty of number.py
@@ -1,8 +1,10 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
         nums, cnt = str(num), 0
-        for i in range(0, len(nums) - k):
+        for i in range(0, len(nums) - k + 1):
             val = int(nums[i: i + k])
             if val != 0 and num % val == 0:
                 cnt += 1
         return cnt
+
+print(Solution().divisorSubstrings(num = 240, k = 2))
