fbe6f584c2cfd6bbc2dc775f1358436138391112
Nimish <nimish.v.shinde60@gmail.com>
2024-11-26 13:47:02 +0530

    test and debug

--- Nov_24/Amost unique subarray.py
+++ Nov_24/Amost unique subarray.py
@@ -6,15 +6,11 @@ class Solution:
                 unique = set(nums[i: i + k])
                 if len(unique) >= m:
                     curr = maxsum = sum(nums[i:i+k])
-
             else:
-                unique.remove(nums[i - 1])
-                curr -= nums[i - 1]
-                unique.add(nums[i + k])
-                curr += nums[i + k]
-
+                unique = set(nums[i: i + k])
+                curr = curr - nums[i - 1] + nums[i + k - 1]
                 if len(unique) >= m:
                     maxsum = max(curr, maxsum)
-
         return maxsum
 
+print(Solution().maxSum(nums = [5,9,9,2,4,5,4], m = 1, k = 3))
