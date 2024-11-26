103a111bdc7a1d59dbef5f671438b3daa87379a5
Nimish <nimish.v.shinde60@gmail.com>
2024-11-26 14:09:49 +0530

    store frequency in dict

--- Nov_24/Amost unique subarray.py
+++ Nov_24/Amost unique subarray.py
@@ -1,16 +1,43 @@
 class Solution:
-    def maxSum(self, nums, m: int, k: int) -> int:
-        unique, curr, maxsum, n = set(), 0, 0,  len(nums)
-        for i in range(n - k + 1):
-            if curr == 0:
-                unique = set(nums[i: i + k])
-                if len(unique) >= m:
-                    curr = maxsum = sum(nums[i:i+k])
+    # def isAlmostDistinct(self, freq: dict, m):
+    #     for v in freq.values():
+    #         if v > 1: return False
+    #     return True
+
+    def firstWindow(self, nums, k):
+        freq = {}
+        s = 0
+        for i in range(k):
+            s += nums[i]
+            if nums[i] in freq:
+                freq[nums[i]] += 1
             else:
-                unique = set(nums[i: i + k])
-                curr = curr - nums[i - 1] + nums[i + k - 1]
-                if len(unique) >= m:
-                    maxsum = max(curr, maxsum)
-        return maxsum
+                freq[nums[i]] = 1
+
+        return freq, s
+
+    def maxSum(self, nums, m: int, k: int) -> int:
+        freq, s = self.firstWindow(nums, k)
+        maxs = s if len(freq) >= m else 0
+
+        for i in range(k, len(nums)):
+            added = nums[i]
+            removed = nums[i - k]
+
+            s = s + added - removed
+
+            if freq[removed] == 1:
+                del freq[removed]
+            else:
+                freq[removed] -= 1
+
+            if added in freq:
+                freq[added] += 1
+            else:
+                freq[added] = 1
+
+            if len(freq) >= m and s > maxs: maxs = s
+
+        return maxs
 
 print(Solution().maxSum(nums = [5,9,9,2,4,5,4], m = 1, k = 3))
