5f2e2304a3f1e5712be6c3554f89397e2427b1f9
Nimish <nimish.v.shinde60@gmail.com>
2024-11-28 23:12:10 +0530

    wraparound the next list

--- Nov_24/Linked list intersection.py
+++ Nov_24/Linked list intersection.py
@@ -6,30 +6,17 @@ class ListNode:
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode):
-        l1, l2,curr = 0, 0, headA
-        while curr:
-            l1 += 1
-            curr = curr.next
-
-        curr = headB
-        while curr:
-            l2 += 1
-            curr = curr.next
-
+        if not headA or not headB:
+            return None
         t1, t2 = headA, headB
-        if l1 > l2:
-            while l1 > l2:
-                t1 = t1.next
-                l1 -= 1
-
-        if l2 > l1:
-            while l2 > l1:
-                t2 = t2.next
-                l2 -= 1
-
         while t1 != t2:
-            t1 = t1.next
-            t2 = t2.next
+            t1, t2 = t1.next, t2.next
+            if t1 == t2:
+                return t1
+            if not t1:
+                t1 = headB
+            if not t2:
+                t2 = headA
         return t1
 
 
