5cd06ef1b755fb57d026bc1b840ef18380544d00
Nimish <nimish.v.shinde60@gmail.com>
2024-11-30 22:41:54 +0530

    Added memoization

--- Nov_24/Buy n sell stock cooldown.py
+++ Nov_24/Buy n sell stock cooldown.py
@@ -1,17 +1,21 @@
 class Solution:
     def maxProfit(self, prices) -> int:
-        n = len(prices)
+        n, memo = len(prices), []
+        for _ in range(n):
+            memo.append([-1] * 2)
 
         def solve(ind, buy):
             if ind >= n:
                 return 0
-            if buy:
-                take = solve(ind + 1, 0) - prices[ind]
-                hold = solve(ind + 1, 1)
-            else:
-                take = solve(ind + 2, 1) + prices[ind]
-                hold = solve(ind + 1, 0)
-            return max(take, hold)
+            if memo[ind][buy] == -1:
+                if buy:
+                    take = solve(ind + 1, 0) - prices[ind]
+                    hold = solve(ind + 1, 1)
+                else:
+                    take = solve(ind + 2, 1) + prices[ind]
+                    hold = solve(ind + 1, 0)
+                memo[ind][buy] = max(take, hold)
+            return memo[ind][buy]
 
         return solve(0, 1)
 
