b8dc58f3a0dd04c81f028146a1b13395f8731787
Nimish <nimish.v.shinde60@gmail.com>
2024-12-01 14:29:17 +0530

    not allowed

--- Nov_24/Sort colours.py
+++ Nov_24/Sort colours.py
@@ -1,22 +1,6 @@
 class Solution:
     def sortColors(self, nums) -> None:
-        zero, one, n = 0, 0, len(nums)
-        for num in nums:
-            if num == 0:
-                zero += 1
-            if num == 1:
-                one += 1
-
-        for i in range(n):
-            if zero > 0:
-                nums[i] = 0
-                zero -= 1
-            elif one > 0:
-                nums[i] = 1
-                one -= 1
-            else:
-                nums[i] = 2
-        return nums
+        nums.sort()
 
 
 print(Solution().sortColors(nums = [2,0,2,1,1,0]))
