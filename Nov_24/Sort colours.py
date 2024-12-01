810ab6f49600e691dbcdd0a53e800d664a98e316
Nimish <nimish.v.shinde60@gmail.com>
2024-12-01 14:44:04 +0530

    three pointer approach

--- Nov_24/Sort colours.py
+++ Nov_24/Sort colours.py
@@ -1,6 +1,17 @@
 class Solution:
     def sortColors(self, nums) -> None:
-        nums.sort()
-
+        i, j, k = 0, 0, len(nums) - 1
+        while j <= k:
+            if nums[j] == 1:
+                j += 1
+            elif nums[j] == 2:
+                nums[j], nums[k] = nums[k], nums[j]
+                k -= 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j += 1
+        return nums
+    
 
 print(Solution().sortColors(nums = [2,0,2,1,1,0]))
