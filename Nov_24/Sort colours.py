c9215bd83a475822eb0c649fc46f7af3f6ef14b4
Nimish <nimish.v.shinde60@gmail.com>
2024-12-01 13:26:07 +0530

    test and debug

--- Nov_24/Sort colours.py
+++ Nov_24/Sort colours.py
@@ -6,7 +6,16 @@ class Solution:
                 zero += 1
             if num == 1:
                 one += 1
-        nums = [0]*zero + [1]*one + [2]*(n - zero - one)
+
+        for i in range(n):
+            if zero > 0:
+                nums[i] = 0
+                zero -= 1
+            elif one > 0:
+                nums[i] = 1
+                one -= 1
+            else:
+                nums[i] = 2
         return nums
 
 
