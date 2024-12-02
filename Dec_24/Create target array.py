213f2467f133be59418950b072e708dc6c2b08b4
Nimish <nimish.v.shinde60@gmail.com>
2024-12-02 22:24:50 +0530

    compare minimum element with sum

---
+++ Dec_24/Create target array.py
@@ -0,0 +1,22 @@
+import heapq
+
+
+class Solution:
+    def isPossible(self, target) -> bool:
+        n, curr, mini = len(target), len(target), 1
+        heapq.heapify(target)
+
+        while len(target) > 0:
+            x = heapq.heappop(target)
+            if curr > x:
+                return False
+            elif curr < x:
+                heapq.heappush(target, x)
+            curr += curr - mini
+
+        if len(target) == 0:
+            return True
+        return False
+
+
+print(Solution().isPossible())
