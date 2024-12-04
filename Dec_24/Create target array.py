2de209220d5f39dd72b8d4d99ec59d79cc589eb6
Nimish <nimish.v.shinde60@gmail.com>
2024-12-04 22:10:42 +0530

    build unit array from target

--- Dec_24/Create target array.py
+++ Dec_24/Create target array.py
@@ -3,20 +3,14 @@ import heapq
 
 class Solution:
     def isPossible(self, target) -> bool:
-        n, curr, mini = len(target), len(target), 1
-        heapq.heapify(target)
-
-        while len(target) > 0:
-            x = heapq.heappop(target)
-            if curr > x:
-                return False
-            elif curr < x:
-                heapq.heappush(target, x)
-            curr += curr - mini
-
-        if len(target) == 0:
-            return True
-        return False
+        curr = sum(target)
+        heapq._heapify_max(target)
+        val = heapq._heappop_max(target)
+        while val > 1:
+            curr -= val
+            heapq.heappush(val - curr)
+            val = heapq._heappop_max(target)
+        return val == 1
 
 
 print(Solution().isPossible(target = [9,3,5]))
