83dc7c81e375cd9ca275d32b0e1dc49176d360be
Nimish <nimish.v.shinde60@gmail.com>
2024-12-08 21:03:04 +0530

    check for every power of 2 value

--- Dec_24/Reordered power of two.py
+++ Dec_24/Reordered power of two.py
@@ -8,5 +8,12 @@ class Solution:
                 arr[num % 10] += 1
                 num //= 10
             return arr
-        
+
+        val, given = 1, checkdigits(n)
+        for _ in range(31):
+            if checkdigits(val ** _) == given:
+                return True
+        return False
+
+            
 print(Solution().reorderedPowerOf2(2041))
