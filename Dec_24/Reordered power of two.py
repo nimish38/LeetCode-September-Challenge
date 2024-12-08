c4edcfc0da02dc2168f4f138ba6eaece94d21c4d
Nimish <nimish.v.shinde60@gmail.com>
2024-12-08 21:09:13 +0530

    check for every power of 2 value

--- Dec_24/Reordered power of two.py
+++ Dec_24/Reordered power of two.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         def checkdigits(num):
-            arr = defaultdict(0)
+            arr = defaultdict(int)
             while num > 0:
                 arr[num % 10] += 1
                 num //= 10
@@ -11,9 +11,9 @@ class Solution:
 
         val, given = 1, checkdigits(n)
         for _ in range(31):
-            if checkdigits(val ** _) == given:
+            if checkdigits(2 ** _) == given:
                 return True
         return False
 
-            
+
 print(Solution().reorderedPowerOf2(2041))
