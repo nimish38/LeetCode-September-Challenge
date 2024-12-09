22b52f8903a77a80ecbeb71e221305f23504f731
Nimish <nimish.v.shinde60@gmail.com>
2024-12-09 22:17:49 +0530

    get compatibility

--- Dec_24/Compatibility Score.py
+++ Dec_24/Compatibility Score.py
@@ -1,6 +1,14 @@
 class Solution:
     def maxCompatibilitySum(self, students, mentors) -> int:
         maxsum, seen, n = 0, set(), len(students)
+
+        def compatibility(a, b):
+            val = 0
+            for i in range(len(a)):
+                if a[i] == b[i]:
+                    val += 1
+            return val
+
         for i in range(n):
             curr, ind = 0, -1
             for j in range(n):
