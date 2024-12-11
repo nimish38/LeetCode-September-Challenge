0c2e2d91639394579d2bc81907b97345a918c513
Nimish <nimish.v.shinde60@gmail.com>
2024-12-11 21:42:49 +0530

    using hashmap

--- Dec_24/Find lucky integer.py
+++ Dec_24/Find lucky integer.py
@@ -4,10 +4,11 @@ class Solution:
         cnt, best = defaultdict(int), -1
         for val in arr:
             cnt[val] += 1
-
+        
         for val in cnt:
             if cnt[val] == val and val > best:
                 best = val
-        return best
-                
+        return best                
+
+
 print(Solution().findLucky(arr = [1,2,2,3,3,3]))
