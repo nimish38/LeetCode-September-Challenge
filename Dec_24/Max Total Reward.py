aa4507955c41447097d1449392f6e988b7b10659
Nimish <nimish.v.shinde60@gmail.com>
2024-12-12 20:27:58 +0530

    memoization with 2 vars

--- Dec_24/Max Total Reward.py
+++ Dec_24/Max Total Reward.py
@@ -1,19 +1,20 @@
 class Solution:
     def maxTotalReward(self, rewardValues) -> int:
-        n = len(rewardValues)
-        memo = [-1] * n
+        memo, n = [], len(rewardValues)
         rewardValues.sort()
+        for _ in range(n):
+            memo.append([-1] * rewardValues[-1] * 2)
 
         def solve(ind, curr):
             if ind >= n:
                 return 0
-            if memo[ind] == -1:
+            if memo[ind][curr] == -1:
                 take, skip = 0, 0
                 if rewardValues[ind] > curr:
                     take = rewardValues[ind] + solve(ind + 1, curr + rewardValues[ind])
                 skip = solve(ind + 1, curr)
-                memo[ind] = max(take, skip)
-            return memo[ind]
+                memo[ind][curr] = max(take, skip)
+            return memo[ind][curr]
 
         return solve(0, 0)
 
