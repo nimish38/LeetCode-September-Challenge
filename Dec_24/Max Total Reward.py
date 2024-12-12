7f0fc96836ecd54912b4eeabcc4830deb5f1aeeb
Nimish <nimish.v.shinde60@gmail.com>
2024-12-12 19:31:18 +0530

    sort and test solution

--- Dec_24/Max Total Reward.py
+++ Dec_24/Max Total Reward.py
@@ -6,6 +6,7 @@ class Solution:
         def solve(ind, curr):
             if ind >= n:
                 self.best = max(self.best, curr)
+                return 0
             take, skip = 0, 0
             if rewardValues[ind] > curr:
                 take = solve(ind + 1, curr + rewardValues[ind])
@@ -14,3 +15,5 @@ class Solution:
 
         solve(0, 0)
         return self.best
+
+print(Solution().maxTotalReward(rewardValues = [1,6,4,3,2]))
