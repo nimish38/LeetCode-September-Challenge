86742446ec18f29f2c39a150fd31af2228eece19
Nimish <nimish.v.shinde60@gmail.com>
2024-12-13 22:40:57 +0530

    use dictionary to store ratios

--- Dec_24/Interchangeable Rectangles.py
+++ Dec_24/Interchangeable Rectangles.py
@@ -1,13 +1,14 @@
+from collections import defaultdict
 class Solution:
     def interchangeableRectangles(self, rectangles) -> int:
-        n, cnt = len(rectangles), 0
+        n, ratios, cnt = len(rectangles), defaultdict(int), 0
         for i in range(n):
-            rectangles[i] = rectangles[i][0] / rectangles[i][1]
-
-        for i in range(n):
-            for j in range(i + 1, n):
-                if rectangles[j] == rectangles[i]:
-                    cnt += 1
+            val = rectangles[i][0] / rectangles[i][1]
+            ratios[val] += 1
+        for count in ratios:
+            cnt += (ratios[count] * (ratios[count] - 1)) // 2
         return cnt
+
+
 
 print(Solution().interchangeableRectangles(rectangles = [[4,8],[3,6],[10,20],[15,30]]))
