a3b3825ed697a1853b3c0f6e8256ab808c6f2301
Nimish <nimish.v.shinde60@gmail.com>
2024-12-13 22:35:11 +0530

    brute force

--- Dec_24/Interchangeable Rectangles.py
+++ Dec_24/Interchangeable Rectangles.py
@@ -1,8 +1,13 @@
 class Solution:
     def interchangeableRectangles(self, rectangles) -> int:
-        n = len(rectangles)
+        n, cnt = len(rectangles), 0
         for i in range(n):
             rectangles[i] = rectangles[i][0] / rectangles[i][1]
-        print(rectangles)
 
-Solution().interchangeableRectangles(rectangles = [[4,8],[3,6],[10,20],[15,30]])
+        for i in range(n):
+            for j in range(i + 1, n):
+                if rectangles[j] == rectangles[i]:
+                    cnt += 1
+        return cnt
+
+print(Solution().interchangeableRectangles(rectangles = [[4,8],[3,6],[10,20],[15,30]]))
