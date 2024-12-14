72bf576f8a35ea83f9c0b8653a6ebff5ea9aea0f
Nimish <nimish.v.shinde60@gmail.com>
2024-12-14 14:31:19 +0530

    test

--- Dec_24/Transpose Matrix.py
+++ Dec_24/Transpose Matrix.py
@@ -4,8 +4,8 @@ class Solution:
         for i in range(len(matrix[0])):
             row = []
             for j in range(len(matrix)):
-                row.append(matrix[i][j])
+                row.append(matrix[j][i])
             res.append(row)
         return res
 
-    
+print(Solution().transpose(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
