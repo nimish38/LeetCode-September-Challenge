509713302dea993ee132779f6e8d756703181ce6
Nimish <nimish.v.shinde60@gmail.com>
2024-12-15 13:24:10 +0530

    shrinking window

---
+++ Dec_24/Min Area Rectangle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumArea(self, grid) -> int:
+        r1, r2, c1, c2 = 0, len(grid) - 1, 0, len(grid[0]) - 1
+
+        while r1 <= r2 and sum(grid[r1]) == 0:
+            r1 += 1
+
+        while r2 > r1 and sum(grid[r2]) == 0:
+            r2 -= 1
+
+        while c1 <= c2 and checksum(c1) == 0:
+            c1 += 1
+
+        while c2 > c1 and checksum(c2) == 0:
+            c2 -= 1
+
+        return max(0, (c2 - c1) * (r2 - r1))
