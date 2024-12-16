4d671e86901b889c5862aee810f26f88a2a9b591
Nimish <nimish.v.shinde60@gmail.com>
2024-12-16 22:21:20 +0530

    two pointer score

---
+++ Dec_24/Max score after split.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        n = len(s)
+        i, j, zero, one, score = 0, n - 1, 0, 0, [0] * n
+
+        while i < n:
+            if s[i] == '0':
+                zero += 1
+            if s[j] == '1':
+                one += 1
+            score[i], score[j] = score[i] + zero, score[j] + one
+            i += 1
+            j -= 1
+
+        if zero == 0 or one == 0:
+            return max(score) - 1
+        return max(score)
