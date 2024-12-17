fa7d0bba1c44b39f04a51ce419f84323b96f435c
Nimish <nimish.v.shinde60@gmail.com>
2024-12-17 23:53:33 +0530

    TWo pass with prefix sum of 1

--- Dec_24/Max score after split.py
+++ Dec_24/Max score after split.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        n, score = len(s), 0
+        n, score, ones, zero, one = len(s), 0, s.count('1'), 0, 0
         for i in range(n - 1):
-            zero, one = 0, 0
-            for j in range(i + 1):
-                if s[j] == '0':
-                    zero += 1
-            for j in range(i + 1, n):
-                if s[j] == '1':
-                    one += 1
-            score = max(score, one + zero)
+            if s[i] == '0':
+                zero += 1
+            if s[i] == '1':
+                one += 1
+            score = max(score, zero + ones - one)
         return score
 
-print(Solution().maxScore(s = "111"))
+print(Solution().maxScore(s = "00111"))
