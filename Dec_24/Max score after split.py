4ceec8748ff822cb72a86866899c93483a281133
Nimish <nimish.v.shinde60@gmail.com>
2024-12-18 00:01:27 +0530

    single pass optimised

--- Dec_24/Max score after split.py
+++ Dec_24/Max score after split.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        n, score, ones, zero, one = len(s), 0, s.count('1'), 0, 0
+        n, score, zero, one = len(s), 0, 0, 0
         for i in range(n - 1):
             if s[i] == '0':
                 zero += 1
             if s[i] == '1':
                 one += 1
-            score = max(score, zero + ones - one)
-        return score
+            score = max(score, zero - one)
+        if s[-1] == '1':
+            one += 1
+        return score + one
 
 print(Solution().maxScore(s = "00111"))
