42848d669f4091d413a38ed00ac97030c0954de5
Nimish <nimish.v.shinde60@gmail.com>
2024-12-19 21:26:13 +0530

    two pointer approach

---
+++ Dec_24/Interval intersection.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intervalIntersection(self, firstList, secondList):
+        res, i, j = [], 0, 0
+
+        while i < len(firstList) and j < len(secondList):
+            fs, fe, ss, se = firstList[i][0], firstList[i][1], secondList[j][0], secondList[j][1]
+            start, end = max(fs, ss), min(fe, se)
+            if start <= end:
+                res.append([start, end])
+            if fe < se:
+                i += 1
+            else:
+                j += 1
+        return res
+
