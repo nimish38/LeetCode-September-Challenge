47ba83467a53ef978c87228d8294b9604d49c5b3
Nimish <nimish.v.shinde60@gmail.com>
2024-12-25 00:09:32 +0530

    use size to divide array into 4

--- Dec_24/Construct Quad Tree.py
+++ Dec_24/Construct Quad Tree.py
@@ -14,23 +14,24 @@ class Solution:
     def construct(self, grid) -> 'Node':
         n = len(grid)
 
-        def solve(rowStart, rowEnd, colStart, colEnd):
-            if rowStart == rowEnd and colStart == colEnd:
-                return Node(grid[rowStart][colStart], True, None, None, None, None)
+        def solve(row, col, size):
+            if size == 1:
+                return Node(grid[row][col], True, None, None, None, None)
 
-            tl = solve(rowStart, rowEnd // 2, colStart, colEnd // 2)
-            tr = solve(rowStart, rowEnd // 2, (colEnd // 2) + 1, colEnd)
-            bl = solve((rowEnd // 2) + 1, rowEnd, colStart, colEnd // 2)
-            br = solve((rowEnd // 2) + 1, rowEnd, (colEnd // 2) + 1, colEnd)
+            tl = solve(row, col, size // 2)
+            tr = solve(row, col + size // 2, size // 2)
+            bl = solve(row + size // 2, col, size // 2)
+            br = solve(row + size // 2, col + size // 2, size // 2 )
             quad = Node(0, False, tl, tr, bl, br)
 
             if tl.isLeaf and tr.isLeaf and bl.isLeaf and br.isLeaf and (tl.val == tr.val == bl.val == br.val):
                 quad.isLeaf = True
                 quad.val = tl.val
+                quad.topRight = quad.topLeft = quad.bottomRight = quad.bottomLeft = None
             return quad
 
-        return solve(0, n - 1, 0, n - 1)
+        return solve(0, 0, n)
 
 
-x = Solution().construct(grid = [[0,1],[1,0]])
+x = Solution().construct(grid = [[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]])
 print(x)
