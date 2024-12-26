35093532a7448d0017be32f31d0868896485ce50
Nimish <nimish.v.shinde60@gmail.com>
2024-12-26 14:00:38 +0530

    test and debug

--- Dec_24/First and last element.py
+++ Dec_24/First and last element.py
@@ -4,8 +4,11 @@ class Solution:
         while i < len(nums) and nums[i] <= target:
             if nums[i] == target:
                 if start == -1:
-                    start = i
+                    start = end = i
                 else:
                     end = i
             i += 1
         return [start, end]
+
+
+print(Solution().searchRange( nums = [5,7,7,8,8,10], target = 8))
