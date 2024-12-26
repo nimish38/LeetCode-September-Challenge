ba9b4923caecae11082de8290e810942455094ef
Nimish <nimish.v.shinde60@gmail.com>
2024-12-26 14:13:42 +0530

    binary search on first hit

--- Dec_24/First and last element.py
+++ Dec_24/First and last element.py
@@ -1,14 +1,18 @@
 class Solution:
     def searchRange(self, nums, target):
-        i, start, end = 0, -1, -1
-        while i < len(nums) and nums[i] <= target:
-            if nums[i] == target:
-                if start == -1:
-                    start = end = i
-                else:
-                    end = i
-            i += 1
+        i, j, start, end = 0, len(nums), -1, -1
+        while i < j:
+            mid = (i + j) // 2
+            if nums[mid] == target:
+                start, end = mid
+                if mid > 0 and nums[mid - 1] == target:
+                    start = get_left(i, mid - 1)
+                if mid < len(nums) - 1 and nums[mid + 1] == target:
+                    end = get_right(mid + 1, j)
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
         return [start, end]
-
 
 print(Solution().searchRange( nums = [5,7,7,8,8,10], target = 8))
