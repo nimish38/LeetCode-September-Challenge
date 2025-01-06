74352c382a42901c82e35ba226367b8384855d91
Nimish <nimish.v.shinde60@gmail.com>
2025-01-06 21:12:30 +0530

    explore attempt

--- 2025/Jan_25/Minesweeper.py
+++ 2025/Jan_25/Minesweeper.py
@@ -1,11 +1,36 @@
 class Solution:
     def updateBoard(self, board, click):
+
+        def get_count(r, c):
+            rt, rb, cl, cr, cnt = max(0, r - 1), min(len(board), r + 1), max(0, c - 1), min(len(board[0]), c + 1), 0
+            for i in range(rt, rb + 1):
+                for j in range(cl, cr + 1):
+                    if i == r and j == c:
+                        continue
+                    if board[i][j] == 'M':
+                        cnt += 1
+            return cnt
+
+        def explore(r, c):
+            rt, rb, cl, cr = max(0, r - 1), min(len(board), r + 1), max(0, c - 1), min(len(board[0]), c + 1)
+            for i in range(rt, rb + 1):
+                for j in range(cl, cr + 1):
+                    if i == r and j == c:
+                        continue
+                    val = get_count(i, j)
+                    if val == 0:
+                        board[i][j] = 'B'
+                        self.st.append((i, j))
+                    else:
+                        board[i][j] = val
+
+
         if board[click[0]][click[1]] == 'M':
             board[click[0]][click[1]] = 'X'
         else:
             self.st = [(click[0], click[1])]
             while self.st:
                 row, col = self.st.pop()
-                val = explore(row, col)
-                board[row][col] = val
+                value = explore(row, col)
+                board[row][col] = value
         return board
