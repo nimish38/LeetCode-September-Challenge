3f2de4640ad1f6956f3ddc46938934e01c323476
Nimish <nimish.v.shinde60@gmail.com>
2025-01-06 20:54:38 +0530

    perform recursive explore

--- 2025/Jan_25/Minesweeper.py
+++ 2025/Jan_25/Minesweeper.py
@@ -2,4 +2,10 @@ class Solution:
     def updateBoard(self, board, click):
         if board[click[0]][click[1]] == 'M':
             board[click[0]][click[1]] = 'X'
+        else:
+            self.st = [(click[0], click[1])]
+            while self.st:
+                row, col = self.st.pop()
+                val = explore(row, col)
+                board[row][col] = val
         return board
