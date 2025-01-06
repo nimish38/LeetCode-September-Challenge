776bb8dc352b25decf5ff92eaa2bb0dd966fa324
Nimish <nimish.v.shinde60@gmail.com>
2025-01-06 21:30:12 +0530

    adjacent explore simulates click

--- 2025/Jan_25/Minesweeper.py
+++ 2025/Jan_25/Minesweeper.py
@@ -1,36 +1,26 @@
 class Solution:
     def updateBoard(self, board, click):
-
         def get_count(r, c):
             rt, rb, cl, cr, cnt = max(0, r - 1), min(len(board), r + 1), max(0, c - 1), min(len(board[0]), c + 1), 0
             for i in range(rt, rb + 1):
                 for j in range(cl, cr + 1):
-                    if i == r and j == c:
-                        continue
                     if board[i][j] == 'M':
                         cnt += 1
             return cnt
 
-        def explore(r, c):
-            rt, rb, cl, cr = max(0, r - 1), min(len(board), r + 1), max(0, c - 1), min(len(board[0]), c + 1)
-            for i in range(rt, rb + 1):
-                for j in range(cl, cr + 1):
-                    if i == r and j == c:
-                        continue
-                    val = get_count(i, j)
-                    if val == 0:
-                        board[i][j] = 'B'
-                        self.st.append((i, j))
-                    else:
-                        board[i][j] = val
-
-
         if board[click[0]][click[1]] == 'M':
             board[click[0]][click[1]] = 'X'
         else:
-            self.st = [(click[0], click[1])]
-            while self.st:
-                row, col = self.st.pop()
-                value = explore(row, col)
-                board[row][col] = value
+            r, c = click[0], click[1]
+            mines = get_count(r, c)
+            if mines:
+                board[r][c] = str(mines)
+            else:
+                board[r][c] = 'B'
+                rt, rb, cl, cr, cnt = max(0, r - 1), min(len(board), r + 1), max(0, c - 1), min(len(board[0]), c + 1), 0
+                for i in range(rt, rb + 1):
+                    for j in range(cl, cr + 1):
+                        if board[i][j] != 'B':
+                            self.updateBoard(board, [i, j])
         return board
+
