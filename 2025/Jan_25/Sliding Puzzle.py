c32c2b21781eeefe1545b990305122c32ca211d7
Nimish <nimish.v.shinde60@gmail.com>
2025-01-08 22:04:11 +0530

    perform bfs and check if target reached

--- 2025/Jan_25/Sliding Puzzle.py
+++ 2025/Jan_25/Sliding Puzzle.py
@@ -11,7 +11,8 @@ class Solution:
         q, curr, target = deque(),"" , "123450"
         swaps, cnt = {0: [1, 3], 1: [0, 2, 4], 2: [1, 5], 3: [0, 4], 4: [3, 5, 1], 5: [4, 2]}, 0
         for _ in range(2):
-            curr += ''.join(board[_])
+            for j in range(3):
+                curr += str(board[_][j])
         q.append(curr)
 
         while q and cnt < 6:
@@ -22,8 +23,11 @@ class Solution:
                 ind = zero_ind(val)
 
                 for adj in swaps[ind]:
-                    state = val
+                    state = list(val)
                     state[ind], state[adj] = state[adj], state[ind]
-                    q.append(state)
+                    q.append(''.join(state))
             cnt += 1
         return -1
+
+
+print(Solution().slidingPuzzle([[4,1,2],[5,0,3]]))
