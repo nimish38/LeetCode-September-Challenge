4e0ed8c13ca574f584dac0978a6e0cca515d64df
Nimish <nimish.v.shinde60@gmail.com>
2025-01-10 21:21:01 +0530

    sliding window updating non grumpy tot

--- 2025/Jan_25/Grumpy Bookstore Owner.py
+++ 2025/Jan_25/Grumpy Bookstore Owner.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSatisfied(self, customers, grumpy, minutes: int) -> int:
-        gr_tot, ngr_tot, i, j = 0, 0, 0, 0
+        gr_tot, ngr_tot, i, res, j = 0, 0, 0, 0, 0
         for k in range(len(customers)):
             if j < minutes and grumpy[k]:
                 gr_tot += customers[k]
@@ -8,4 +8,13 @@ class Solution:
             if not grumpy[k]:
                 ngr_tot += customers[k]
 
-        
+        res, i = gr_tot + ngr_tot, 1
+        while j < len(customers):
+            if grumpy[i - 1]:
+                gr_tot -= customers[i - 1]
+            if grumpy[j]:
+                gr_tot += customers[j]
+            res = max(res, gr_tot + ngr_tot)
+            i += 1
+            j += 1
+        return res
