c5d3d7bb6abfa83451758bdd8b2ae898e3f894bb
Nimish <nimish.v.shinde60@gmail.com>
2025-01-11 14:18:40 +0530

    remaining lines logic

--- 2025/Jan_25/Text Justification.py
+++ 2025/Jan_25/Text Justification.py
@@ -1,24 +1,39 @@
 class Solution:
     def fullJustify(self, words, maxWidth: int):
 
-        def build_sentence(sent, last):
+        def build_sentence(sent, last, txt):
             cnt, line = len(sent) - 1, ''
             if last:
+                _ = 0
                 for _ in range(cnt):
                     line += sent[_] + ' '
                 line += sent[_]
                 remaining = maxWidth - len(line)
                 line += ' ' * remaining
-                return line
-            if cnt > 0:
-
+            else:
+                if cnt > 0:
+                    extra = maxWidth - txt - cnt
+                    pad = extra // cnt
+                    left_pad = extra % cnt
+                    for i in range(cnt):
+                        line += sent[i] + ' ' + (' ' * pad)
+                        if left_pad > 0:
+                            line += ' '
+                            left_pad -= 1
+                    line += sent[i]
+                else:
+                    line += sent[0] + ' ' * (maxWidth - txt)
+            return line
 
         word, res, minspaces = 0, '', 0
         while word < len(words):
             curr, available = [], maxWidth
-            while word < len(words) and available > len(words[word]):
+            while word < len(words) and available - (len(curr) - 1) >= len(words[word]):
                 curr.append(words[word])
-                available = available - len(words[word]) - (len(curr) - 1)
+                available = available - len(words[word])
                 word += 1
-            res += build_sentence(curr, word == len(words)) + '\n'
+            res += build_sentence(curr, word == len(words), available) + '\n'
         return res
+
+
+print(Solution().fullJustify(words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16))
