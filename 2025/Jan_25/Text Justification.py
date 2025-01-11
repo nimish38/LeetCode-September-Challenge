f841f61398a71b4820dd8592929e4fa13c4700c2
Nimish <nimish.v.shinde60@gmail.com>
2025-01-11 13:52:20 +0530

    last line alignment logic

--- 2025/Jan_25/Text Justification.py
+++ 2025/Jan_25/Text Justification.py
@@ -1,11 +1,24 @@
 class Solution:
     def fullJustify(self, words, maxWidth: int):
+
+        def build_sentence(sent, last):
+            cnt, line = len(sent) - 1, ''
+            if last:
+                for _ in range(cnt):
+                    line += sent[_] + ' '
+                line += sent[_]
+                remaining = maxWidth - len(line)
+                line += ' ' * remaining
+                return line
+            if cnt > 0:
+
+
         word, res, minspaces = 0, '', 0
         while word < len(words):
             curr, available = [], maxWidth
-            while available > len(words[word]):
+            while word < len(words) and available > len(words[word]):
                 curr.append(words[word])
                 available = available - len(words[word]) - (len(curr) - 1)
                 word += 1
-            build_sentence(curr)
+            res += build_sentence(curr, word == len(words)) + '\n'
         return res
