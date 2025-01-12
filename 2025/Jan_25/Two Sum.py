2f08fc60fef540f570027508af0d8b6e57524394
Nimish <nimish.v.shinde60@gmail.com>
2025-01-12 13:49:17 +0530

    O(n) space

--- 2025/Jan_25/Two Sum.py
+++ 2025/Jan_25/Two Sum.py
@@ -1,9 +1,14 @@
+from collections import defaultdict
+
+
 class Solution:
     def twoSum(self, nums, target: int):
+        val = defaultdict(int)
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            if target - nums[i] in val:
+                return [val[target - nums[i]], i]
+            else:
+                val[nums[i]] = i
 
 
 print(Solution().twoSum(nums = [3,2,4], target = 6))
