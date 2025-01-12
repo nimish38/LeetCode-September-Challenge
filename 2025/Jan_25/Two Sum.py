34963921069a19310e4dcf6221398a1b5af4d6b8
Nimish <nimish.v.shinde60@gmail.com>
2025-01-12 13:41:57 +0530

    test

--- 2025/Jan_25/Two Sum.py
+++ 2025/Jan_25/Two Sum.py
@@ -4,3 +4,6 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+print(Solution().twoSum(nums = [3,2,4], target = 6))
