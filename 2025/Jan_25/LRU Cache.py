4004e291f6790aa5152283d132b989dabf47f60e
Nimish <nimish.v.shinde60@gmail.com>
2025-01-14 22:49:26 +0530

    Fix bug

--- 2025/Jan_25/LRU Cache.py
+++ 2025/Jan_25/LRU Cache.py
@@ -11,27 +11,27 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if len(self.st) == self.cap:
+        if key in self.dic:
+            self.st.remove(key)
+
+        elif len(self.st) == self.cap:
             val = self.st[0]
             del self.st[0]
             del self.dic[val]
-            self.st.append(key)
-            self.dic[key] = value
-        else:
-            if key in self.dic:
-                self.st.remove(key)
-            self.dic[key] = value
-            self.st.append(key)
+        self.st.append(key)
+        self.dic[key] = value
+
 
 
 obj = LRUCache(2)
-print(obj.put(1, 1))
-print(obj.put(2, 2))
-print(obj.get(1))
-print(obj.put(3, 3))
 print(obj.get(2))
-print(obj.put(4, 4))
+print(obj.put(2, 6))
+# print(obj.put(2, 2))
 print(obj.get(1))
-print(obj.get(3))
-print(obj.get(4))
+print(obj.put(1, 5))
+# print(obj.get(2))
+print(obj.put(1, 2))
+print(obj.get(1))
+print(obj.get(2))
+# print(obj.get(4))
 
