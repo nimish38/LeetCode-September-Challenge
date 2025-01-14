dd63361088ab480a03e7b47a278e739060e97e2d
Nimish <nimish.v.shinde60@gmail.com>
2025-01-14 21:44:04 +0530

    test and debug

--- 2025/Jan_25/LRU Cache.py
+++ 2025/Jan_25/LRU Cache.py
@@ -11,14 +11,27 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if len(self.st) == self.capacity:
+        if len(self.st) == self.cap:
             val = self.st[0]
             del self.st[0]
             del self.dic[val]
             self.st.append(key)
-            self.dic[key] = val
+            self.dic[key] = value
         else:
             if key in self.dic:
                 self.st.remove(key)
             self.dic[key] = value
             self.st.append(key)
+
+
+obj = LRUCache(2)
+print(obj.put(1, 1))
+print(obj.put(2, 2))
+print(obj.get(1))
+print(obj.put(3, 3))
+print(obj.get(2))
+print(obj.put(4, 4))
+print(obj.get(1))
+print(obj.get(3))
+print(obj.get(4))
+
