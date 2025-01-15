588136e948b085e43d16de7028aa62eeb0c9341e
Nimish <nimish.v.shinde60@gmail.com>
2025-01-15 21:45:21 +0530

    explore airports

--- 2025/Jan_25/Recontruct Itinerary.py
+++ 2025/Jan_25/Recontruct Itinerary.py
@@ -4,12 +4,29 @@ import bisect
 
 class Solution:
     def findItinerary(self, tickets):
-        airports = defaultdict(list)
+        airports, cnt = defaultdict(list), len(tickets)
         for travel in tickets:
             a, b = travel[0], travel[1]
             bisect.insort(airports[a], b)
+        target = cnt + 1 - (cnt // 2)
 
-        return solve('JFK')
+        def solve(src, route):
+            if len(route) == target - 1:
+                return route.append(src)
+
+            for i in range(airports[src]):
+                val = airports[src][i]
+                if val != '#':
+                    airports[src][i] = '#'
+                    travel = solve(val, route.append(src))
+                    if travel:
+                        return travel
+                    else:
+                        airports[src][i] = val
+            return []
+
+
+        return solve('JFK', [])
 
 
 print(Solution().findItinerary(tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
