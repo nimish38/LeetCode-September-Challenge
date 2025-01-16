1e68b95e273f02e1c42f9da599e8fa715655e1ed
Nimish <nimish.v.shinde60@gmail.com>
2025-01-16 20:48:10 +0530

    iterative accepted solution

--- 2025/Jan_25/Recontruct Itinerary.py
+++ 2025/Jan_25/Recontruct Itinerary.py
@@ -4,29 +4,37 @@ import bisect
 
 class Solution:
     def findItinerary(self, tickets):
-        airports, target = defaultdict(list), len(tickets) + 1
+        airports, target, res, st = defaultdict(list), len(tickets) + 1, [], ['JFK']
         for travel in tickets:
             a, b = travel[0], travel[1]
-            bisect.insort(airports[a], b)
+            airports[a].append(b)
+        for city in airports:
+            airports[city].sort(reverse=True)
 
-        def solve(src, route):
-            route.append(src)
-            if len(route) == target:
-                return route
+        # def solve(src, route):
+        #     route.append(src)
+        #     if len(route) == target:
+        #         self.res = route
+        #         return True
+        #
+        #     for i in range(len(airports[src])):
+        #         val = airports[src][-1]
+        #         airports[src].pop()
+        #         if solve(val, route):
+        #             return True
+        #         airports[src].append(val)
+        #     route.pop()
+        #     return False
 
-            for i in range(len(airports[src])):
-                val = airports[src][i]
-                if val != '#':
-                    airports[src][i] = '#'
-                    travell = solve(val, route)
-                    if travell:
-                        return travell
-                    else:
-                        airports[src][i] = val
-            route.pop()
-            return []
+        while st:
+            city = st[-1]
+            if city in airports and airports[city]:
+                st.append(airports[city].pop())
+            else:
+                # this gets all cities that have 0 in degree one by one
+                res.append(st.pop())
+        return res[::-1]
 
-        return solve('JFK', [])
 
 
 print(Solution().findItinerary(tickets = [["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
