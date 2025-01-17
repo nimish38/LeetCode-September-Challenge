d39dffee397cfb91f1c7ece08a331320b75827da
Nimish <nimish.v.shinde60@gmail.com>
2025-01-17 22:01:15 +0530

    check 3 directions for 0's

--- 2025/Jan_25/Square Submatrices.py
+++ 2025/Jan_25/Square Submatrices.py
@@ -3,6 +3,24 @@ class Solution:
         m, n, ones = len(matrix), len(matrix[0]), []
         sides, cnt = min(m, n), 0
 
+        def row(a, b, val):
+            for _ in range(val):
+                if not matrix[a][b + _]:
+                    return False
+            return True
+
+        def col(a, b, val):
+            for _ in range(val):
+                if not matrix[a + _][b]:
+                    return False
+            return True
+
+        def diag(a, b, val):
+            for _ in range(val):
+                if not matrix[a + _][b + _]:
+                    return False
+            return True
+
         for side in range(sides, 0, -1):
             for i in range(0, m - side):
                 for j in range(0, n - side):
