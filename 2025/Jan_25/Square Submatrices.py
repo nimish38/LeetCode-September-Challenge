11c80bf7ac8bbf4b8720e3d415782c891691ceef
Nimish <nimish.v.shinde60@gmail.com>
2025-01-17 23:15:37 +0530

    Brute force test solution

--- 2025/Jan_25/Square Submatrices.py
+++ 2025/Jan_25/Square Submatrices.py
@@ -3,37 +3,25 @@ class Solution:
         m, n, ones = len(matrix), len(matrix[0]), []
         sides, cnt = min(m, n), 0
 
-        def row(a, b, val):
-            for _ in range(val):
-                if not matrix[a][b + _]:
-                    return False
-            return True
-
-        def col(a, b, val):
-            for _ in range(val):
-                if not matrix[a + _][b]:
-                    return False
-            return True
-
-        def diag(a, b, val):
-            for _ in range(val):
-                if not matrix[a + _][b + _]:
-                    return False
+        def checkSquare(a, b, val):
+            for x in range(val):
+                for y in range(val):
+                    if not matrix[a + x][b + y]:
+                        return False
             return True
 
         for side in range(sides, 0, -1):
             for i in range(0, m - side + 1):
                 for j in range(0, n - side + 1):
-                    if row(i, j, side) and col(i, j, side) and diag(i, j, side):
+                    if checkSquare(i, j, side):
                         cnt += 1
-
-
         return cnt
 
 
-print(Solution().countSquares(matrix =
-[
-  [0,1,1,1],
-  [1,1,1,1],
-  [0,1,1,1]
-]))
+print(Solution().countSquares(matrix = [
+    [1,1,0,0,1],
+    [1,0,1,1,1],
+    [1,1,1,1,1],
+    [1,0,1,0,1],
+    [0,0,1,0,1]]
+))
