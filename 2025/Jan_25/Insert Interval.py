2bd433ef93fa9426ac916612907c5ee05b7c3129
Nimish <nimish.v.shinde60@gmail.com>
2025-01-18 14:03:47 +0530

    test and debug

--- 2025/Jan_25/Insert Interval.py
+++ 2025/Jan_25/Insert Interval.py
@@ -4,10 +4,14 @@ class Solution:
         while i < n and intervals[i][1] <= newStart:
             res.append(intervals[i])
             i += 1
-        newStart = min(newStart, intervals[i - 1][0])
+        newStart = min(newStart, intervals[i][0])
         while i < n and intervals[i][0] <= newEnd:
             i += 1
         newEnd = max(newEnd, intervals[i - 1][1])
         res.append([newStart, newEnd])
-        res.extend(intervals[i :])
+        res.extend(intervals[i:])
+        return res
+
+
+print(Solution().insert(intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]))
 
