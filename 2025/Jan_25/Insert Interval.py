5b718a3b36b1919d2505e4f82dc0bf0895bf9362
Nimish <nimish.v.shinde60@gmail.com>
2025-01-18 14:08:58 +0530

    edge cases

--- 2025/Jan_25/Insert Interval.py
+++ 2025/Jan_25/Insert Interval.py
@@ -1,10 +1,13 @@
 class Solution:
     def insert(self, intervals, newInterval):
+        if not intervals:
+            return [newInterval]
         i, n, newStart, newEnd, res = 0, len(intervals), newInterval[0], newInterval[1], []
-        while i < n and intervals[i][1] <= newStart:
+        while i < n and intervals[i][1] < newStart:
             res.append(intervals[i])
             i += 1
-        newStart = min(newStart, intervals[i][0])
+        if i < n:
+            newStart = min(newStart, intervals[i][0])
         while i < n and intervals[i][0] <= newEnd:
             i += 1
         newEnd = max(newEnd, intervals[i - 1][1])
