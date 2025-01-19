513540c972b80d2ce50a57ff5da2b0e8266ca1ce
Nimish <nimish.v.shinde60@gmail.com>
2025-01-19 20:43:13 +0530

    check tree similarity

--- 2025/Jan_25/Duplicate Trees.py
+++ 2025/Jan_25/Duplicate Trees.py
@@ -9,6 +9,23 @@ class TreeNode:
 class Solution:
     def findDuplicateSubtrees(self, root):
         nodes, st, res = {}, [root]
+
+        def checkDuplicate(a, b):
+            x, y = [a], [b]
+            while a and b:
+                p, q = x.pop(), y.pop()
+                if p.val != q.val:
+                    return False
+                if (not p.left and q.left) or (not q.left and p.left) or (not p.right and q.right) or (not q.right and p.right):
+                    return False
+                if p.left and q.left:
+                    x.append(p.left)
+                    y.append(q.left)
+                if q.right and p.right:
+                    x.append(p.right)
+                    y.append(q.right)
+            return True
+        
         while st:
             value = st.pop()
             if value.val in nodes and checkDuplicate(value, nodes[value.val]):
