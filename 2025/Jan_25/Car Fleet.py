333406de5aabafa6efe127d9b95112f42e587a99
Nimish <nimish.v.shinde60@gmail.com>
2025-01-22 22:48:50 +0530

    test solution

--- 2025/Jan_25/Car Fleet.py
+++ 2025/Jan_25/Car Fleet.py
@@ -4,14 +4,14 @@ class Solution:
         for i in range(len(speed)):
             combo.append((position[i], speed[i]))
         combo.sort(key=lambda x: x[0])
-
         st, i = [(target - combo[i][0]) / combo[i][1]], i - 1
         while i >= 0:
             time = (target - combo[i][0]) / combo[i][1]
             if time > st[-1]:
                 st.append(time)
-
+            i -= 1
         return len(st)
 
 
+print(Solution().carFleet(target = 100, position = [0,2,4], speed = [4,2,1]))
 
