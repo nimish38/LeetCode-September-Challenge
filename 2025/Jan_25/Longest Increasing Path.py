47e976b985e1d3b48dccfea6451cc14d10b5f1b3
Nimish <nimish.v.shinde60@gmail.com>
2025-01-23 21:38:34 +0530

    added memoization

--- 2025/Jan_25/Longest Increasing Path.py
+++ 2025/Jan_25/Longest Increasing Path.py
@@ -1,16 +1,20 @@
 class Solution:
     def longestIncreasingPath(self, matrix):
-        m, n, cnt = len(matrix), len(matrix[0]), 0
+        m, n, cnt, memo = len(matrix), len(matrix[0]), 0, []
+        for _ in range(m):
+            memo.append([-1] * n)
 
         def solve(i, j):
-            val, adj, temp = matrix[i][j],[(0, -1), (0, 1), (-1, 0), (1, 0)], 0
-            matrix[i][j] = -1
-            for a, b in adj:
-                x, y = i + a, j + b
-                if 0 <= x < m and 0 <= y < n and matrix[x][y] > val:
-                    temp = max(temp, solve(x, y))
-            matrix[i][j] = val
-            return 1 + temp
+            if memo[i][j] == -1:
+                val, adj, temp = matrix[i][j],[(0, -1), (0, 1), (-1, 0), (1, 0)], 0
+                matrix[i][j] = -1
+                for a, b in adj:
+                    x, y = i + a, j + b
+                    if 0 <= x < m and 0 <= y < n and matrix[x][y] > val:
+                        temp = max(temp, solve(x, y))
+                matrix[i][j] = val
+                memo[i][j] = 1 + temp
+            return memo[i][j]
 
         for i in range(m):
             for j in range(n):
