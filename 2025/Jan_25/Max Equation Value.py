e97918b8f18342be1d5464cfb3dd5b38070bdb5c
Nimish <nimish.v.shinde60@gmail.com>
2025-01-24 22:39:22 +0530

    brute force

---
+++ 2025/Jan_25/Max Equation Value.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxValueOfEquation(self, points, k: int) -> int:
+        i, j, n, best = 0, 1, len(points), float('-inf')
+        while i < n - 1:
+            j = i + 1
+            while j < n and (points[j][0] - points[i][0]) <= k:
+                best = max(best, points[i][1] + points[j][1] + (points[j][0] - points[i][0]))
+                j += 1
+            i += 1
+        return best
