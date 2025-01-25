5078d68e98d4a41332c080c2d3723ca464989a37
Nimish <nimish.v.shinde60@gmail.com>
2025-01-25 14:13:35 +0530

    debug and fix, heapq is always minheap

--- 2025/Jan_25/Max Equation Value.py
+++ 2025/Jan_25/Max Equation Value.py
@@ -2,16 +2,14 @@ import heapq
 
 class Solution:
     def findMaxValueOfEquation(self, points, k: int) -> int:
-        j, n, best, pq = 0, len(points), float('-inf'), []
-        heapq.heapify(pq)
+        n, best, pq = len(points), float('-inf'), []
         for i in range(n):
             while pq and points[i][0] - pq[0][1] > k:
                 heapq.heappop(pq)
             if pq:
-                best = max(best, points[i][0] + points[i][1] + pq[0][0])
-            heapq.heappush((points[i][1] - points[i][0], points[i][0]))
-
+                best = max(best, points[i][0] + points[i][1] + (-1 * pq[0][0]))
+            heapq.heappush(pq, ((points[i][1] - points[i][0]) * -1, points[i][0]))
         return best
 
 
-print(Solution().findMaxValueOfEquation(points = [[0,0],[3,0],[9,2]], k = 3))
+print(Solution().findMaxValueOfEquation([[-19,-12],[-13,-18],[-12,18],[-11,-8],[-8,2],[-7,12],[-5,16],[-3,9],[1,-7],[5,-4],[6,-20],[10,4],[16,4],[19,-9],[20,19]], k = 6))
