89f4a8d4570fd80be2b57e44418ae1e45d16baf4
Nimish <nimish.v.shinde60@gmail.com>
2025-01-26 16:28:48 +0530

    compute difference one step earlier

--- 2025/Jan_25/Path with min effort.py
+++ 2025/Jan_25/Path with min effort.py
@@ -1,20 +1,22 @@
 class Solution:
     def minimumEffortPath(self, heights) -> int:
-        m, n = len(heights), len(heights[0])
+        m, n, memo = len(heights), len(heights[0]), []
+        for _ in range(m):
+            memo.append([-1] * n)
 
-        def solve(i, j, par):
+        def solve(i, j):
             if i == m - 1 and j == n - 1:
-                return abs(par - heights[i][j])
+                return 0
             adj, eff = [(1, 0), (-1, 0), (0, 1), (0, -1)], float('inf')
             val, heights[i][j] = heights[i][j], 'V'
             for a, b in adj:
                 x, y = i + a, j + b
                 if 0 <= x < m and 0 <= y < n and heights[x][y] != 'V':
-                    eff = min(eff, max(abs(par - val), solve(x, y, val)))
+                    eff = min(eff, max(abs(val - heights[x][y]), solve(x, y)))
             heights[i][j] = val
             return eff
 
-        return solve(0, 0, heights[0][0])
+        return solve(0, 0)
 
 
 print(Solution().minimumEffortPath(heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]]))
