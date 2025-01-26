7b135fcc6748e921e00b064bd96bf3f6724218d4
Nimish <nimish.v.shinde60@gmail.com>
2025-01-26 16:58:07 +0530

    added memoization but wrong answer

--- 2025/Jan_25/Path with min effort.py
+++ 2025/Jan_25/Path with min effort.py
@@ -5,18 +5,21 @@ class Solution:
             memo.append([-1] * n)
 
         def solve(i, j):
-            if i == m - 1 and j == n - 1:
-                return 0
-            adj, eff = [(1, 0), (-1, 0), (0, 1), (0, -1)], float('inf')
-            val, heights[i][j] = heights[i][j], 'V'
-            for a, b in adj:
-                x, y = i + a, j + b
-                if 0 <= x < m and 0 <= y < n and heights[x][y] != 'V':
-                    eff = min(eff, max(abs(val - heights[x][y]), solve(x, y)))
-            heights[i][j] = val
-            return eff
+            if memo[i][j] == -1:
+                if i == m - 1 and j == n - 1:
+                    memo[i][j] = 0
+                else:
+                    adj, eff = [(1, 0), (-1, 0), (0, 1), (0, -1)], float('inf')
+                    val, heights[i][j] = heights[i][j], 'V'
+                    for a, b in adj:
+                        x, y = i + a, j + b
+                        if 0 <= x < m and 0 <= y < n and heights[x][y] != 'V':
+                            eff = min(eff, max(abs(val - heights[x][y]), solve(x, y)))
+                    heights[i][j] = val
+                    memo[i][j] = eff
+            return memo[i][j]
 
         return solve(0, 0)
 
 
-print(Solution().minimumEffortPath(heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]]))
+print(Solution().minimumEffortPath(heights = [[2, 3],[4, 2]]))
