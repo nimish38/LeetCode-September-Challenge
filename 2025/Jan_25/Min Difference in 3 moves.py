951462e314ed96f39fc9387a4bd84ecef2fe24cf
Nimish <nimish.v.shinde60@gmail.com>
2025-01-29 00:14:59 +0530

    sort only relevant values

--- 2025/Jan_25/Min Difference in 3 moves.py
+++ 2025/Jan_25/Min Difference in 3 moves.py
@@ -1,11 +1,16 @@
+import heapq
+
+
 class Solution:
     def minDifference(self, nums) -> int:
         if len(nums) < 5:
             return 0
-        nums.sort()
+        a = heapq.nsmallest(4, nums)
+        b = heapq.nlargest(4, nums)[::-1]
+
         res, n = float('inf'), len(nums)
         for i in range(4):
-            res = min(res, nums[n - 4 + i] - nums[i])
+            res = min(res, b[i] - a[i])
         return res
 
 
