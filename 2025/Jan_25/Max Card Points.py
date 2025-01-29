734409e5ec9e59c578837ef53ba493bddfd64080
Nimish <nimish.v.shinde60@gmail.com>
2025-01-29 22:07:05 +0530

    sort only relevant values

---
+++ 2025/Jan_25/Max Card Points.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, cardPoints, k: int) -> int:
+        res = 0
+        for i in range(k):
+            res += cardPoints[i]
+        if len(cardPoints) == k:
+            return sum(cardPoints)
+        j, best = len(cardPoints) - 1, res
+        while k > 0:
+            res = res - cardPoints[i] + cardPoints[j]
+            best = max(res, best)
+            i -= 1
+            j -= 1
+        return best
