4015a4bfb970343df29ed70a936310478e2eaff0
Nimish <nimish.v.shinde60@gmail.com>
2025-01-31 22:54:12 +0530

    test and debug

--- 2025/Jan_25/Employee Importance.py
+++ 2025/Jan_25/Employee Importance.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 class Employee:
     def __init__(self, id: int, importance: int, subordinates):
         self.id = id
@@ -9,12 +7,16 @@ class Employee:
 
 class Solution:
     def getImportance(self, employees, id) -> int:
-        hashmap, st, total = defaultdict(list), [], 0
+        hashmap, st, total = {}, [], 0
         for emp in employees:
-            hashmap[emp.id].append(emp.importance, emp.subordinates)
+            hashmap[emp.id] = [emp.importance, emp.subordinates]
         st.append(id)
         while st:
             node = st.pop()
             total += hashmap[node][0]
             st.extend(hashmap[node][1])
         return total
+
+
+a, b, c = Employee(1,5,[2,3]), Employee(2,3,[]), Employee(3,3,[])
+print(Solution().getImportance([a, b, c], 1))
