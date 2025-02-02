ac792ae9453f6ad4d7de488ff7cedef9cff00f09
Nimish <nimish.v.shinde60@gmail.com>
2025-02-02 14:04:06 +0530

    added memoization

--- 2025/Feb_25/Coin Change.py
+++ 2025/Feb_25/Coin Change.py
@@ -1,17 +1,23 @@
 class Solution:
     def coinChange(self, coins, amount: int) -> int:
+        memo = [-2] * (amount + 1)
+
         def solve(amt):
-            if amt == 0:
-                return 0
-            res = float('inf')
-            for c in coins:
-                if amt >= c:
-                    res = min(res, solve(amt - c) + 1)
-            return res
+            if memo[amt] == -2:
+                if amt == 0:
+                    memo[amt] = 0
+                else:
+                    res = float('inf')
+                    for c in coins:
+                        if amt >= c:
+                            res = min(res, solve(amt - c) + 1)
+                    memo[amt] = res
+            return memo[amt]
+
         result = solve(amount)
         if result == float('inf'):
                 return -1
         return result
 
 
-print(Solution().coinChange(coins = [2], amount = 3))
+print(Solution().coinChange(coins = [1,5,2], amount = 11))
