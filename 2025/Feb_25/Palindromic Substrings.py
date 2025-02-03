3e1d7786d4bff9c8ee6054a775bd344e2ee39f99
Nimish <nimish.v.shinde60@gmail.com>
2025-02-03 21:40:53 +0530

    debug and test

--- 2025/Feb_25/Palindromic Substrings.py
+++ 2025/Feb_25/Palindromic Substrings.py
@@ -1,9 +1,12 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         cnt, n = len(s), len(s)
-        for length in range(1, n):
-            for i in range(n - length):
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
                 val = s[i : i + length]
                 if val == val[::-1]:
                     cnt += 1
         return cnt
+
+
+print(Solution().countSubstrings(s = "aaa"))
