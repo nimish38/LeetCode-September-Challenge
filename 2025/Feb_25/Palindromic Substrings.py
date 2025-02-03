22e2c7e303f49f3e4285c26f501ca905109236df
Nimish <nimish.v.shinde60@gmail.com>
2025-02-03 22:32:36 +0530

    brute force plus memoization

--- 2025/Feb_25/Palindromic Substrings.py
+++ 2025/Feb_25/Palindromic Substrings.py
@@ -1,10 +1,22 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        cnt, n = len(s), len(s)
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                val = s[i : i + length]
-                if val == val[::-1]:
+        cnt, n, memo = 0, len(s), []
+        for _ in range(n):
+            memo.append([-2] * n)
+
+        def check(a, b):
+            if a > b:
+                return True
+            if memo[a][b] == -1:
+                if s[a] == s[b]:
+                    memo[a][b] = check(a + 1, b - 1)
+                else:
+                    memo[a][b] = False
+            return memo[a][b]
+
+        for i in range(n):
+            for j in range(i, n):
+                if check(i, j):
                     cnt += 1
         return cnt
 
