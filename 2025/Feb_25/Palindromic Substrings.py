a2430f038eea4e18edc3fb6de95b7fabefd85e79
Nimish <nimish.v.shinde60@gmail.com>
2025-02-03 23:09:55 +0530

    Dp blue print bottom up

--- 2025/Feb_25/Palindromic Substrings.py
+++ 2025/Feb_25/Palindromic Substrings.py
@@ -1,24 +1,23 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         cnt, n, memo = 0, len(s), []
-        for _ in range(n):
-            memo.append([-2] * n)
+        for _ in range(n + 1):
+            memo.append([False] * (n + 1))
 
-        def check(a, b):
-            if a > b:
-                return True
-            if memo[a][b] == -1:
-                if s[a] == s[b]:
-                    memo[a][b] = check(a + 1, b - 1)
+        for length in range(1, n + 1):
+            i = 0
+            while i + length - 1 < n:
+                j = i + length - 1
+                if i == j:
+                    memo[i][j] = True
+                elif i + 1 == j:
+                    memo[i][j] = s[i] == s[j]
                 else:
-                    memo[a][b] = False
-            return memo[a][b]
-
-        for i in range(n):
-            for j in range(i, n):
-                if check(i, j):
+                    memo[i][j] = (s[i] == s[j]) and memo[i + 1][j - 1]
+                if memo[i][j]:
                     cnt += 1
+                i += 1
         return cnt
 
 
-print(Solution().countSubstrings(s = "aaa"))
+print(Solution().countSubstrings(s = "abc"))
