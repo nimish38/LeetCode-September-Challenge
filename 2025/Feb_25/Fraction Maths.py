9923e68329f958949183c568e7bab767f26e0621
Nimish <nimish.v.shinde60@gmail.com>
2025-02-04 23:10:18 +0530

    test solutions

--- 2025/Feb_25/Fraction Maths.py
+++ 2025/Feb_25/Fraction Maths.py
@@ -1,18 +1,19 @@
 class Solution:
     def fractionAddition(self, expression: str) -> str:
-        res, num, den, sign, i = 0, 0, 1, 1
-        if expression[0] == '-':
-            sign, num, den = -1, expression[1], expression[3]
-            res += (num / den) * sign
-            i = 4
+        res, num, den, sign, i = 0, 0, 0, 1, 1
+        if expression[0] != '-':
+            num, den = int(expression[0]), int(expression[2])
+            res += (num / den)
+            i = 3
         while i < len(expression):
             if expression[i] == '-':
                 sign = -1
             else:
                 sign = 1
-            num, den = expression[i + 1], expression[i + 3]
+            num, den = int(expression[i + 1]), int(expression[i + 3])
             res += (num / den) * sign
             i += 4
         return res
 
-    
+
+print(Solution().fractionAddition(expression = "1/3-1/2"))
