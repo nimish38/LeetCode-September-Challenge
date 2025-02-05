7411c555510b13fad069ed8ca1f83f8e3835c3ae
Nimish <nimish.v.shinde60@gmail.com>
2025-02-05 22:44:02 +0530

    use memoization

--- 2025/Feb_25/Knight Probability Chess.py
+++ 2025/Feb_25/Knight Probability Chess.py
@@ -1,15 +1,19 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-
+        memo = {}
         def solve(r, c, k):
-            if r < 0 or r >= n or c < 0 or c >= n:
-                return 0
-            if k == 0:
-                return 1
-            res, moves = 0.0, [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
-            for i, j in moves:
-                res += solve(r + i, c + j, k - 1)
-            return res / 8
+            val = str(r) + '_' + str(c) + '_' + str(k)
+            if val not in memo:
+                if r < 0 or r >= n or c < 0 or c >= n:
+                    memo[val] = 0
+                elif k == 0:
+                    memo[val] = 1
+                else:
+                    res, moves = 0.0, [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
+                    for i, j in moves:
+                        res += solve(r + i, c + j, k - 1)
+                    memo[val] = res / 8
+            return memo[val]
         return solve(row, column, k)
 
 
