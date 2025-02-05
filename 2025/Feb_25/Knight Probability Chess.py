1ade33a3b02c6e54f673a5ac4b1c328063b1c17c
Nimish <nimish.v.shinde60@gmail.com>
2025-02-05 22:40:53 +0530

    dynamic programming

--- 2025/Feb_25/Knight Probability Chess.py
+++ 2025/Feb_25/Knight Probability Chess.py
@@ -1,22 +1,16 @@
-from collections import deque
-
-
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        st, res = deque(), 1
-        st.append((row, column))
-        while k > 0 and st:
-            curr = 0
-            moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
-            for _ in range(len(st)):
-                a, b = st.popleft()
-                for x, y in moves:
-                    if 0 <= a + x < n and 0 <= b + y < n:
-                        st.append((a + x, b + y))
-                        curr += 1
-            res *= (curr / 8)
-            k -= 1
-        return res
+
+        def solve(r, c, k):
+            if r < 0 or r >= n or c < 0 or c >= n:
+                return 0
+            if k == 0:
+                return 1
+            res, moves = 0.0, [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
+            for i, j in moves:
+                res += solve(r + i, c + j, k - 1)
+            return res / 8
+        return solve(row, column, k)
 
 
 print(Solution().knightProbability( n = 3, k = 2, row = 0, column = 0))
