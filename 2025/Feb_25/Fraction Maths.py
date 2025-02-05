8ce5de0dbad8788bff2c05e00b48e3d8d3e595e9
Nimish <nimish.v.shinde60@gmail.com>
2025-02-05 21:34:12 +0530

    Get gcd to reduce fraction

--- 2025/Feb_25/Fraction Maths.py
+++ 2025/Feb_25/Fraction Maths.py
@@ -1,3 +1,4 @@
+import math
 class Solution:
     def fractionAddition(self, expression: str) -> str:
         num, den, i = 0, 1, 0
@@ -7,14 +8,14 @@ class Solution:
                 numer += expression[i]
                 i += 1
             i += 1
-            while expression[i] not in ['+', '-']:
+            while i < len(expression) and expression[i] not in ['+', '-']:
                 denom += expression[i]
                 i += 1
             numer, denom = int(numer), int(denom)
             num = (num * denom) + (den * numer)
             den = den * denom
-            i += 1
-        return str(num) + '/' + str(den)
+        gcd = abs(math.gcd(num, den))
+        return str(num // gcd) + '/' + str(den // gcd)
 
 
-print(Solution().fractionAddition(expression = "-5/2+10/3+7/9"))
+print(Solution().fractionAddition(expression = "-1/2+1/2+1/3"))
