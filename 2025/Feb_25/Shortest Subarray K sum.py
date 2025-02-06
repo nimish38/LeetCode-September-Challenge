23ea93fcbff03ee49892de4edc46b3812aef317e
Nimish <nimish.v.shinde60@gmail.com>
2025-02-06 19:33:06 +0530

    brute force with adding in order

--- 2025/Feb_25/Shortest Subarray K sum.py
+++ 2025/Feb_25/Shortest Subarray K sum.py
@@ -2,8 +2,12 @@ class Solution:
     def shortestSubarray(self, nums, k: int) -> int:
         n = len(nums)
         for length in range(1, n + 1):
+            val = None
             for i in range(n - length + 1):
-                val = sum(nums[i: i + length])
+                if not val:
+                    val = sum(nums[i: i + length])
+                else:
+                    val = val - nums[i - 1] + nums[i + length - 1]
                 if val >= k:
                     return length
         return -1
