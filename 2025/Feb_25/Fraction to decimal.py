8e792b9dec277269f9227fe770fa2ab0cf116869
Nimish <nimish.v.shinde60@gmail.com>
2025-02-08 14:30:45 +0530

    store remainders and quotients

--- 2025/Feb_25/Fraction to decimal.py
+++ 2025/Feb_25/Fraction to decimal.py
@@ -1,22 +1,25 @@
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        res, remainder = '', {}
+        res, remainder, quotients = '', {}, set()
         if numerator < denominator:
             res += '0.'
         else:
             res += str(numerator // denominator) + '.'
             numerator = numerator % denominator
+        remainder[numerator] = 1
         numerator *= 10
         while numerator > 0 and numerator not in remainder:
-            quo = numerator // denominator
-            remainder[quo] = 1
-            if numerator < denominator:
-                numerator *= 10
-            else:
-                numerator %= denominator
-        res += ''.join(str(x) for x in remainder.keys())
+            quo, rem = numerator // denominator, numerator % denominator
+            remainder[rem] = 1
+            quotients.add(quo)
+            numerator = rem * 10
+        if numerator == 0:
+            res += ''.join(str(x) for x in quotients)
+        else:
+            val = ''.join(str(x) for x  in quotients)
+            res += '(' + val + ')'
         return res
 
 
-print(Solution().fractionToDecimal(numerator = 1, denominator = 2))
+print(Solution().fractionToDecimal(numerator = 4, denominator = 333))
 
