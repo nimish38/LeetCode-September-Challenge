5f1bd7d5a283aa2096207f30adb8dfe228d4304e
Nimish <nimish.v.shinde60@gmail.com>
2025-02-09 13:56:18 +0530

    store max building heights left and right

---
+++ 2025/Feb_25/Trapping rain water.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trap(self, height) -> int:
+        n, water = len(height), 0
+        left, right = [0] * n, [0] * n
+        for i in range(1, n):
+            left[i] = max(left[i - 1], height[i])
+            right[n - 1 - i] = max(right[n - i], height[n - i - 1])
+
+        for i in range(n):
+            water += max(min(left[i], right[i]) - height[i], 0)
+        return water
