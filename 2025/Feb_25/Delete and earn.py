25801e32c4f008107576dbff558c33ef27c1c0fa
Nimish <nimish.v.shinde60@gmail.com>
2025-02-11 22:56:55 +0530

    added memoization

--- 2025/Feb_25/Delete and earn.py
+++ 2025/Feb_25/Delete and earn.py
@@ -3,21 +3,25 @@ from collections import Counter
 
 class Solution:
     def deleteAndEarn(self, nums) -> int:
-        cnt , n = Counter(nums), len(nums)
+        cnt = Counter(nums)
         nums = sorted(list(set(nums)))
+        n = len(nums)
+        memo = [-1] * n
 
         def solve(i):
             if i >= n:
                 return 0
-            take = nums[i] * cnt[nums[i]]
-            if i < n - 1 and nums[i] != nums[i + 1] - 1:
-                take += solve(i + 1)
-            else:
-                take += solve(i + 2)
-            skip = solve(i + 1)
-            return max(take, skip)
+            if memo[i] == -1:
+                take = nums[i] * cnt[nums[i]]
+                if i < n - 1 and nums[i] != nums[i + 1] - 1:
+                    take += solve(i + 1)
+                else:
+                    take += solve(i + 2)
+                skip = solve(i + 1)
+                memo[i] = max(take, skip)
+            return memo[i]
 
         return solve(0)
 
 
-print(Solution().deleteAndEarn(nums = [3,4,2]))
+print(Solution().deleteAndEarn(nums = [2,2,3,3,3,4]))
