fe69005d3bbd33ed31e2bd10c94a7ef5434a7104
Nimish <nimish.v.shinde60@gmail.com>
2025-02-11 23:12:03 +0530

    bottom up approach wth 2 var

--- 2025/Feb_25/Delete and earn.py
+++ 2025/Feb_25/Delete and earn.py
@@ -5,23 +5,17 @@ class Solution:
     def deleteAndEarn(self, nums) -> int:
         cnt = Counter(nums)
         nums = sorted(list(set(nums)))
-        n = len(nums)
-        memo = [-1] * n
+        n, prev1, prev2, ans = len(nums), 0, 0, 0
 
-        def solve(i):
-            if i >= n:
-                return 0
-            if memo[i] == -1:
-                take = nums[i] * cnt[nums[i]]
-                if i < n - 1 and nums[i] != nums[i + 1] - 1:
-                    take += solve(i + 1)
-                else:
-                    take += solve(i + 2)
-                skip = solve(i + 1)
-                memo[i] = max(take, skip)
-            return memo[i]
-
-        return solve(0)
+        for i in range(n):
+            curr = nums[i] * cnt[nums[i]]
+            if i > 0 and nums[i] != nums[i - 1] + 1:
+                curr = curr + prev1
+            else:
+                curr = max(curr + prev2, prev1)
+            ans = max(ans, curr)
+            prev2, prev1 = prev1, curr
+        return ans
 
 
 print(Solution().deleteAndEarn(nums = [2,2,3,3,3,4]))
