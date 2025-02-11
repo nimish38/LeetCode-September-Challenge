8d0da3e2c1327daa87591ae17cd2fb3a3f25f057
Nimish <nimish.v.shinde60@gmail.com>
2025-02-11 22:50:05 +0530

    recursively explore

--- 2025/Feb_25/Delete and earn.py
+++ 2025/Feb_25/Delete and earn.py
@@ -1,41 +1,23 @@
+from collections import Counter
+
+
 class Solution:
     def deleteAndEarn(self, nums) -> int:
-        counter = {}
-        for num in nums:
-            if num in counter:
-                counter[num] += 1
-            else:
-                counter[num] = 1
+        cnt , n = Counter(nums), len(nums)
+        nums = sorted(list(set(nums)))
 
-        def solve(cnt):
-            if not cnt:
+        def solve(i):
+            if i >= n:
                 return 0
-            score, best = 0, -1
-            while cnt.values():
-                val = list(cnt.keys())[0]
-                prev, pcnt, nex, ncnt, score = val - 1, 0, val + 1, 0, val
-                cnt[val] -= 1
-                if cnt[val] == 0:
-                    del cnt[val]
-                if prev in cnt:
-                    pcnt = cnt[prev]
-                    del cnt[prev]
-                if nex in cnt:
-                    ncnt = cnt[nex]
-                    del cnt[nex]
-                score += solve(cnt)
-                if pcnt > 0:
-                    cnt[prev] = pcnt
-                if ncnt > 0:
-                    cnt[nex] = ncnt
-                if val not in cnt:
-                    cnt[val] = 1
-                else:
-                    cnt[val] += 1
-                best = max(best, score)
-            return best
+            take = nums[i] * cnt[nums[i]]
+            if i < n - 1 and nums[i] != nums[i + 1] - 1:
+                take += solve(i + 1)
+            else:
+                take += solve(i + 2)
+            skip = solve(i + 1)
+            return max(take, skip)
 
-        return solve(counter)
+        return solve(0)
 
 
 print(Solution().deleteAndEarn(nums = [3,4,2]))
