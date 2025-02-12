21a003f98815ecf8369ce9454bd94703905a9917
Nimish <nimish.v.shinde60@gmail.com>
2025-02-12 22:41:20 +0530

    shrinking and expanding window

--- 2025/Feb_25/Min Size subarray sum.py
+++ 2025/Feb_25/Min Size subarray sum.py
@@ -1,16 +1,17 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums) -> int:
-        n = len(nums)
-        for leng in range(1, n + 1):
-            curr = 0
-            for i in range(n - leng + 1):
-                if i == 0:
-                    curr = sum(nums[: leng])
-                else:
-                    curr = curr + nums[i + leng - 1] - nums[i - 1]
-                if curr >= target:
-                    return leng
-        return 0
+    def minSubArrayLen(self, target, nums):
+        i, j, minwindow = 0, 0, float('inf')
+        curr_sum = 0
+        while j < len(nums):
+            curr_sum += nums[j]
+            while curr_sum >= target:
+                minwindow = min(minwindow, j - i + 1)
+                curr_sum -= nums[i]
+                i += 1
+            j += 1
+        if minwindow == float('inf'):
+            return 0
+        return minwindow
 
 
 print(Solution().minSubArrayLen(target = 15, nums = [1,2,3,4,5]))
