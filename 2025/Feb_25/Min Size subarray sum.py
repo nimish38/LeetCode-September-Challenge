0e8dd58c97db18adbb4894a66470cdc0aae9e15f
Nimish <nimish.v.shinde60@gmail.com>
2025-02-12 22:32:07 +0530

    brute force test and fix

--- 2025/Feb_25/Min Size subarray sum.py
+++ 2025/Feb_25/Min Size subarray sum.py
@@ -2,13 +2,16 @@ class Solution:
     def minSubArrayLen(self, target: int, nums) -> int:
         n = len(nums)
         for leng in range(1, n):
-            for i in range(n - leng):
+            curr = 0
+            for i in range(n - leng + 1):
                 if i == 0:
-                    curr = sum[i: i + leng]
+                    curr = sum(nums[: leng])
                 else:
                     curr = curr + nums[i + leng - 1] - nums[i - 1]
                 if curr >= target:
                     return leng
+        return 0
 
-        
+
+print(Solution().minSubArrayLen(target = 7, nums = [2,3,1,2,4,3]))
 
