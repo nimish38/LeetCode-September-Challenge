bfff3f6688d2f20fa54b74bfa183e3bd1fffa7e4
Nimish <nimish.v.shinde60@gmail.com>
2025-02-13 19:57:09 +0530

    test and fix

--- 2025/Feb_25/First Unique character.py
+++ 2025/Feb_25/First Unique character.py
@@ -1,15 +1,16 @@
-from collections import defaultdict
-
-
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        chars = defaultdict(int)
+        chars = {}
         for i, c in enumerate(s):
-            chars[c] += 1
+            if c in chars:
+                chars[c][0] = [2, 1]
+            else:
+                chars[c] = [1, i]
 
         for i in chars:
-            if chars[i] == 1:
-                return i
+            if chars[i][0] == 1:
+                return chars[i][1]
         return -1
 
 
+print(Solution().firstUniqChar(s = "loveleetcode"))
