80c276bfd5bc9aa311168f64c444187bb3b0057a
Nimish <nimish.v.shinde60@gmail.com>
2025-02-14 20:38:02 +0530

    test and fix

--- 2025/Feb_25/Find Pivot Index.py
+++ 2025/Feb_25/Find Pivot Index.py
@@ -1,12 +1,14 @@
 class Solution:
     def pivotIndex(self, nums) -> int:
         n = len(nums)
-        left = right = [0] * n
+        left, right = [0] * n, [0] * n
         for i in range(1, n):
             left[i] = left[i - 1] + nums[i - 1]
             right[n - i - 1] = right[n - i] + nums[n - i]
-
         for i in range(n):
             if left[i] == right[i]:
                 return i
         return -1
+
+
+print(Solution().pivotIndex(nums = [1,7,3,6,5,6]))
