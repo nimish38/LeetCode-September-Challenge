83cbd4c451f4fceb244056d9d31c197b4d425590
Nimish <nimish.v.shinde60@gmail.com>
2025-02-14 20:31:02 +0530

    extra 2n space

---
+++ 2025/Feb_25/Find Pivot Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def pivotIndex(self, nums) -> int:
+        n = len(nums)
+        left = right = [0] * n
+        for i in range(1, n):
+            left[i] = left[i - 1] + nums[i - 1]
+            right[n - i - 1] = right[n - i] + nums[n - i]
+
+        for i in range(n):
+            if left[i] == right[i]:
+                return i
+        return -1
