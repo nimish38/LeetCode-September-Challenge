e297fef00013117c5dda4d18e4ec05f5dff72ccd
Nimish <nimish.v.shinde60@gmail.com>
2025-02-15 13:35:34 +0530

    double heap solution

--- 2025/Feb_25/Median of 2 sorted arrays.py
+++ 2025/Feb_25/Median of 2 sorted arrays.py
@@ -1,12 +1,18 @@
+import heapq
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         m, n = len(nums1), len(nums2)
-        nums1 += nums2
-        nums1.sort()
+        leftmax, rightmin = [], []
+
+        make_heap(nums1, m)
+        make_heap(nums2, n)
+
         if (m + n) % 2:
-            return nums1[(m + n) // 2]
+            return leftmax[0]
         else:
-            return (nums1[(m + n) // 2] + nums1[((m + n) // 2) - 1]) / 2
+            return (leftmax[0] + rightmin[0]) / 2
 
 
 print(Solution().findMedianSortedArrays(nums1 = [1,2], nums2 = [3,4]))
