daf09131ba9796971e99464ea7a173c7ff1d37ea
Nimish <nimish.v.shinde60@gmail.com>
2025-02-15 13:43:52 +0530

    double heap solution with one max and other min

--- 2025/Feb_25/Median of 2 sorted arrays.py
+++ 2025/Feb_25/Median of 2 sorted arrays.py
@@ -6,9 +6,25 @@ class Solution:
         m, n = len(nums1), len(nums2)
         leftmax, rightmin = [], []
 
-        make_heap(nums1, m)
-        make_heap(nums2, n)
+        def make_heap(arr):
+            for val in arr:
+                if len(leftmax) == 0:
+                    heapq.heappush(leftmax, -1 * val)
 
+                elif val > (-1 * leftmax[0]):
+                    heapq.heappush(rightmin, val)
+                    if len(rightmin) > len(leftmax):
+                        pop = heapq.heappop(rightmin)
+                        heapq.heappush(leftmax, -1 * pop)
+
+                else:
+                    heapq.heappush(leftmax, -1 * val)
+                    if len(leftmax) - len(rightmin) > 1:
+                        pop = heapq.heappop(leftmax)
+                        heapq.heappush(rightmin, -1 * pop)
+
+        make_heap(nums1)
+        make_heap(nums2)
         if (m + n) % 2:
             return leftmax[0]
         else:
