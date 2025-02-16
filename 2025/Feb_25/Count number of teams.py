ed44406cab3157d6daad6a4f4f1bbaab4a9b3b27
Nimish <nimish.v.shinde60@gmail.com>
2025-02-16 13:12:47 +0530

    find left and right for each index

--- 2025/Feb_25/Count number of teams.py
+++ 2025/Feb_25/Count number of teams.py
@@ -3,12 +3,20 @@ class Solution:
         cnt, n = 0, len(rating)
         for j in range(1, n - 1):
             leftsmall, leftlarge, rightsmall, rightlarge = 0, 0, 0, 0
+
             for i in range(j):
                 if rating[i] < rating[j]:
                     leftsmall += 1
                 if rating[i] > rating[j]:
                     leftlarge += 1
-            
+
+            for i in range(j + 1, n):
+                if rating[i] > rating[j]:
+                    rightlarge += 1
+                if rating[i] < rating[j]:
+                    rightsmall += 1
+
+            cnt += (leftsmall * rightlarge) + (leftlarge * rightsmall)
         return cnt
 
 
