f0ebfa13d3be8865f2d63463789002c59bdbdeb3
Nimish <nimish.v.shinde60@gmail.com>
2025-02-17 22:42:02 +0530

    check if position and direction is repeated

--- 2025/Feb_25/Robot in circle.py
+++ 2025/Feb_25/Robot in circle.py
@@ -1,6 +1,6 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        face, pos, vals = 'N', [0, 0], {'00': 1}
+        face, pos, vals = 'N', [0, 0], {'00N': 1}
         moves = {
                     'N': {'G': [0, 1, 'N'], 'L': [0, 0, 'W'], 'R': [0, 0, 'E']},
                     'E': {'G': [1, 0, 'E'], 'L': [0, 0, 'N'], 'R': [0, 0, 'S']},
@@ -13,11 +13,11 @@ class Solution:
                 change = moves[face][c]
                 x, y, face = change[0], change[1], change[2]
                 pos[0], pos[1] = pos[0] + x, pos[1] + y
-                new_pos = str(pos[0]) + str(pos[1])
-                if new_pos in vals:
-                    return True
-                vals[new_pos] = 1
+                new_pos = str(pos[0]) + str(pos[1]) + face
+            if new_pos in vals:
+                return True
+            vals[new_pos] = 1
         return False
 
 
-print(Solution().isRobotBounded(instructions = "GL"))
+print(Solution().isRobotBounded(instructions = "GG"))
