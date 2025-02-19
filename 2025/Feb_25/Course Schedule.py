d4d3ee36b4789ca22efeeea3a27ac12a9d0004d3
Nimish <nimish.v.shinde60@gmail.com>
2025-02-19 22:51:48 +0530

    Topological sort

--- 2025/Feb_25/Course Schedule.py
+++ 2025/Feb_25/Course Schedule.py
@@ -1,27 +1,47 @@
+from collections import deque, defaultdict
+
+
 class Solution:
     def canFinish(self, numCourses, prerequisites) -> bool:
-        adj, inrecursion, vis = {}, [False]*numCourses, [False]*numCourses
+        adj, indegree = defaultdict(list), [0] * numCourses
         for pre in prerequisites:
             take, req = pre[0], pre[1]
             if req in adj:
                 adj[req].append(take)
-            else:
-                adj[req] = [take]
+            indegree[take] += 1
 
-        def isCycle(node):
-            vis[node], inrecursion[node] = True, True
-            for nei in adj[node]:
-                if not vis[nei] and isCycle(nei):
-                    return True
-                elif inrecursion[nei]:
-                    return True
-            inrecursion[node] = False
-            return False
+        # def isCycle(node):
+        #     vis[node], inrecursion[node] = True, True
+        #     if node in adj:
+        #         for nei in adj[node]:
+        #             if not vis[nei] and isCycle(nei):
+        #                 return True
+        #             elif inrecursion[nei]:
+        #                 return True
+        #     inrecursion[node] = False
+        #     return False
 
-        for i in range(numCourses):
-            if not vis[i] and isCycle(i):
-                return False
-        return True
+        def topologicalSort():
+            cnt, que = 0, deque()
+            for i in range(numCourses):
+                if indegree[i] == 0:
+                    cnt += 1
+                    que.append(i)
+            while que:
+                node = que.popleft()
+                for neighbor in adj[node]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        cnt += 1
+                        que.append(neighbor)
+            return cnt == numCourses
+
+        return topologicalSort()
+
+        # for i in range(numCourses):
+        #     if not vis[i] and isCycle(i):
+        #         return False
+        # return True
 
 
 print(Solution().canFinish(numCourses = 20, prerequisites =[[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]]))
