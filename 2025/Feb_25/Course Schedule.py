955f859884844618c6e9e94f863775b21e3891e5
Nimish <nimish.v.shinde60@gmail.com>
2025-02-19 22:10:28 +0530

    explore each course one by one

--- 2025/Feb_25/Course Schedule.py
+++ 2025/Feb_25/Course Schedule.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFinish(self, numCourses, prerequisites) -> bool:
-        adj, st = {}, []
+        adj, st, vis = {}, [], {}
         for pre in prerequisites:
             take, req = pre[0], pre[1]
             if take in adj:
@@ -10,4 +10,21 @@ class Solution:
 
         for val in range(numCourses):
             if val not in adj:
-                st.append(val)
+                vis[val] = 1
+
+        while len(vis) < numCourses - 1:
+            before = len(vis)
+            for node in adj:
+                flag = 0
+                for req in adj[node]:
+                    if req not in vis:
+                        flag = 1
+                if not flag:
+                    vis[node] = 1
+                    del adj[node]
+            if len(vis) == before:
+                return False
+        return True
+
+
+
