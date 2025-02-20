0dc1d5401a5aee8a3b5fa001803a13c6d5161638
Nimish <nimish.v.shinde60@gmail.com>
2025-02-20 22:33:55 +0530

    make linked list from list

--- 2025/Feb_25/Merge K Sorted List.py
+++ 2025/Feb_25/Merge K Sorted List.py
@@ -6,15 +6,28 @@ class ListNode:
 
 
 class Solution:
-    def mergeKLists(self, lists):
+    def makeLists(self, lists):
+        def build(val):
+            dummy = curr = ListNode(-1)
+            for v in val:
+                curr.next = ListNode(v)
+                curr = curr.next
+            return dummy.next
+        lis = []
+        for a in lists:
+            lis.append(build(a))
+        return lis
 
+    def mergeKLists(self, lists):
         def merge(l1, l2):
             dummy = curr = ListNode(-1)
             while l1 and l2:
                 if l1.val < l2.val:
                     curr.next = l1
+                    l1 = l1.next
                 else:
                     curr.next = l2
+                    l2 = l2.next
                 curr = curr.next
             if l1:
                 curr.next = l1
@@ -32,3 +45,8 @@ class Solution:
             for i in range(2, k):
                 l = merge(l, lists[i])
             return l
+
+
+a = Solution().makeLists(lists = [[1,4,5],[1,3,4],[2,6]])
+x = Solution().mergeKLists(a)
+print(x)
