8ce0008ecd22971b58905789226393f9016853fd
Nimish <nimish.v.shinde60@gmail.com>
2025-02-20 22:22:48 +0530

    mergw 2 lists

--- 2025/Feb_25/Merge K Sorted List.py
+++ 2025/Feb_25/Merge K Sorted List.py
@@ -7,6 +7,21 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists):
+
+        def merge(l1, l2):
+            dummy = curr = ListNode(-1)
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                else:
+                    curr.next = l2
+                curr = curr.next
+            if l1:
+                curr.next = l1
+            if l2:
+                curr.next = l2
+            return dummy.next
+
         k = len(lists)
         if k == 0:
             return []
@@ -17,4 +32,3 @@ class Solution:
             for i in range(2, k):
                 l = merge(l, lists[i])
             return l
-        
