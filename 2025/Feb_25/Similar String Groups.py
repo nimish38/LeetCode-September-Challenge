ffba1ac784cc6ee35ccadef3a9cf32498aab94b1
Nimish <nimish.v.shinde60@gmail.com>
2025-02-21 22:32:10 +0530

    update similarity function and then dfs

--- 2025/Feb_25/Similar String Groups.py
+++ 2025/Feb_25/Similar String Groups.py
@@ -4,24 +4,32 @@ from collections import defaultdict
 class Solution:
     def numSimilarGroups(self, strs) -> int:
         def isSimilar(s1, s2):
-            k, n = 2, len(s1)
-            def getPos(st, c):
-                for i in range(n):
-                    if st[i] == c:
-                        return i
-            while k > 0:
-                for i in range(n):
-                    if s1[i] != s2[i]:
-                        pos = getPos(s2, s1[i])
-                        s2[i], s2[pos] = s2[pos], s2[i]
-                        k -= 1
-                        break
-                if s1 == s2:
+            for i in range(len(s1)):
+                if s1[i] != s2[i]:
+                    pos = s2.index(s1[i])
+                    s2[i], s2[pos] = s2[pos], s2[i]
+                    if s1 != s2:
+                        return False
                     return True
-            return False
+            return True
 
-        groups = defaultdict(set)
+        groups, strs, vis = defaultdict(list), map(list, strs), [False]*len(strs)
         for i in range(len(strs)):
-            for j in range(len(strs)):
-                if isSimilar(strs[i], strs[j]):
-                    groups[strs[i]].add(strs[j])
+            for j in range(i + 1, len(strs)):
+                if isSimilar(list(strs[i]), list(strs[j])):
+                    groups[i].append(j)
+                    groups[j].append(i)
+
+        st, cnt, i = [], 0, 0
+        while i < len(strs):
+            if not vis[i]:
+                vis[i] = True
+                cnt += 1
+                for j in groups[i]:
+                    if not vis[j]:
+                        vis[j] = True
+            i += 1
+        return cnt
+
+
+print(Solution().numSimilarGroups(strs = ["tars","rats","arts","star"]))
