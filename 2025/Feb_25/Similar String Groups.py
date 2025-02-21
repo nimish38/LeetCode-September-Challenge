67056b852afbd7e7d9a1166e46701f7b0870a804
Nimish <nimish.v.shinde60@gmail.com>
2025-02-21 22:42:45 +0530

    update similarity function

--- 2025/Feb_25/Similar String Groups.py
+++ 2025/Feb_25/Similar String Groups.py
@@ -4,30 +4,28 @@ from collections import defaultdict
 class Solution:
     def numSimilarGroups(self, strs) -> int:
         def isSimilar(s1, s2):
+            diff = 0
             for i in range(len(s1)):
                 if s1[i] != s2[i]:
-                    pos = s2.index(s1[i])
-                    s2[i], s2[pos] = s2[pos], s2[i]
-                    if s1 != s2:
-                        return False
-                    return True
-            return True
-
-        groups, strs, vis = defaultdict(list), map(list, strs), [False]*len(strs)
+                    diff += 1
+            return diff == 2 or diff == 0
+        groups, strs, vis = defaultdict(list), list(map(list, strs)), [False]*len(strs)
         for i in range(len(strs)):
             for j in range(i + 1, len(strs)):
                 if isSimilar(list(strs[i]), list(strs[j])):
                     groups[i].append(j)
                     groups[j].append(i)
-
-        st, cnt, i = [], 0, 0
+        cnt, i = 0, 0
+        def dfs(ind):
+            if not vis[ind]:
+                vis[ind] = True
+                for j in groups[ind]:
+                    if not vis[j]:
+                        dfs(j)
         while i < len(strs):
             if not vis[i]:
-                vis[i] = True
+                dfs(i)
                 cnt += 1
-                for j in groups[i]:
-                    if not vis[j]:
-                        vis[j] = True
             i += 1
         return cnt
 
