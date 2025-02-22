0c4ac3caed8edca49b2b28b3686e8d3e3b68600c
Nimish <nimish.v.shinde60@gmail.com>
2025-02-22 14:02:27 +0530

    start at top right optimal solution

--- 2025/Feb_25/Search Matrix.py
+++ 2025/Feb_25/Search Matrix.py
@@ -1,28 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix, target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
-        rowStart, colStart, rowEnd, colEnd = -1, 0, -1, 0
+        row, col = 0, n - 1
 
-        for i in range(n):
-            if colStart == -1 and matrix[0][i] <= target <= matrix[m - 1][i]:
-                colStart, colEnd = i, i
-            elif colStart != -1 and matrix[0][i] <= target <= matrix[m - 1][i]:
-                colEnd = i
-
-        for i in range(m):
-            if rowStart == -1 and matrix[i][colStart] <= target <= matrix[i][colEnd]:
-                rowStart, rowEnd = i, i
-            elif rowStart != -1 and  matrix[i][colStart] <= target <= matrix[i][colEnd]:
-                rowEnd = i
-
-        for i in range(rowStart, rowEnd + 1):
-            for j in range(colStart, colEnd + 1):
-                if matrix[i][j] == target:
-                    return True
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
         return False
 
 
-print(Solution().searchMatrix(matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20))
+print(Solution().searchMatrix(matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5))
 
 
 
