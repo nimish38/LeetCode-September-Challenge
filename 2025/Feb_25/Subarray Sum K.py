17d063664d6f84ad7a0d80fa8c7712bcdd6da22d
Nimish <nimish.v.shinde60@gmail.com>
2025-02-24 21:55:02 +0530

    brute force sliding window test

--- 2025/Feb_25/Subarray Sum K.py
+++ 2025/Feb_25/Subarray Sum K.py
@@ -1,13 +1,16 @@
 class Solution:
     def subarraySum(self, nums, k: int) -> int:
         cnt, n = 0, len(nums)
-        for length in range(1, n - 1):
+        for length in range(1, n):
             summ = 0
-            for i in range(n - i):
+            for i in range(n - length + 1):
                 if i == 0:
                     summ = sum(nums[i : i + length])
                 else:
-                    summ = summ - nums[i - 1] + nums[i + length]
+                    summ = summ - nums[i - 1] + nums[i + length - 1]
                 if summ == k:
                     cnt += 1
         return cnt
+
+
+print(Solution().subarraySum(nums = [1,2,3], k = 3))
