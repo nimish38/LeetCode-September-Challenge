5c420686772f6a75793c37092e6e2725ca5a38ec
Nimish <nimish.v.shinde60@gmail.com>
2025-02-24 22:57:59 +0530

    store prefix sum in map

--- 2025/Feb_25/Subarray Sum K.py
+++ 2025/Feb_25/Subarray Sum K.py
@@ -1,20 +1,15 @@
 class Solution:
     def subarraySum(self, nums, k: int) -> int:
-        cnt, n = 0, len(nums)
-
-        if n == 1 and nums[0] == k:
-            return 1
-
-        for length in range(1, n):
-            summ = 0
-            for i in range(n - length + 1):
-                if i == 0:
-                    summ = sum(nums[i : i + length])
-                else:
-                    summ = summ - nums[i - 1] + nums[i + length - 1]
-                if summ == k:
-                    cnt += 1
+        cnt, n, curr,  prefix_sum = 0, len(nums), 0, {0: 1}
+        for i in range(n):
+            curr += nums[i]
+            if curr - k in prefix_sum:
+                cnt += prefix_sum[curr - k]
+            if curr in prefix_sum:
+                prefix_sum[curr] += 1
+            else:
+                prefix_sum[curr] = 1
         return cnt
 
 
-print(Solution().subarraySum(nums = [1], k = 3))
+print(Solution().subarraySum(nums = [1,2,3], k = 3))
