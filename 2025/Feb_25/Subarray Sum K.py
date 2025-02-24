e44e3d450e866dedd4df30f6a72d64a835a14fa2
Nimish <nimish.v.shinde60@gmail.com>
2025-02-24 21:58:05 +0530

    edge case

--- 2025/Feb_25/Subarray Sum K.py
+++ 2025/Feb_25/Subarray Sum K.py
@@ -1,6 +1,10 @@
 class Solution:
     def subarraySum(self, nums, k: int) -> int:
         cnt, n = 0, len(nums)
+
+        if n == 1 and nums[0] == k:
+            return 1
+
         for length in range(1, n):
             summ = 0
             for i in range(n - length + 1):
@@ -13,4 +17,4 @@ class Solution:
         return cnt
 
 
-print(Solution().subarraySum(nums = [1,2,3], k = 3))
+print(Solution().subarraySum(nums = [1], k = 3))
