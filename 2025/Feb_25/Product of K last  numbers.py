368b3b688d2a3f8b86d3dfc2577e0ed030441b2d
Nimish <nimish.v.shinde60@gmail.com>
2025-02-26 22:32:30 +0530

    prefix product store debug and fix

--- 2025/Feb_25/Product of K last  numbers.py
+++ 2025/Feb_25/Product of K last  numbers.py
@@ -9,26 +9,28 @@ class ProductOfNumbers:
             self.prev = 1
         else:
             self.nums.append(num * self.prev)
-            self.prev = num
+            self.prev = self.nums[-1]
 
     def getProduct(self, k: int) -> int:
         n = len(self.nums)
-        if n == k:
+        if self.nums[n - k] == 0:
+            return 0
+        den = n - k - 1
+        if den < 0 or self.nums[den] == 0:
             return self.nums[-1]
-        den = self.nums[n - 1 - k]
-        if den == 0:
-            return self.nums[n - 1]
-        return self.nums[n - 1] // den
+        return self.nums[-1] // self.nums[den]
 
 
 productOfNumbers = ProductOfNumbers()
-productOfNumbers.add(3)
 productOfNumbers.add(0)
-productOfNumbers.add(2)
-productOfNumbers.add(5)
-productOfNumbers.add(4)
-print(productOfNumbers.getProduct(2))
+productOfNumbers.add(0)
+productOfNumbers.add(9)
 print(productOfNumbers.getProduct(3))
-print(productOfNumbers.getProduct(4))
 productOfNumbers.add(8)
-print(productOfNumbers.getProduct(2))
+productOfNumbers.add(3)
+productOfNumbers.add(8)
+print(productOfNumbers.getProduct(5))
+print(productOfNumbers.getProduct(4))
+print(productOfNumbers.getProduct(6))
+productOfNumbers.add(8)
+productOfNumbers.add(8)
