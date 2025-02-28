dab138efd17961bb9068030bfec815f7ea503d5d
Nimish <nimish.v.shinde60@gmail.com>
2025-02-28 22:58:59 +0530

    use bisectleft instead of binary search

--- 2025/Feb_25/Pick index with weight.py
+++ 2025/Feb_25/Pick index with weight.py
@@ -1,33 +1,37 @@
-import random
+import random, bisect
 
 
 class Solution:
 
     def __init__(self, w):
-        self.buckets, self.val = [], 0
+        self.buckets = [0]
         for v in w:
-            self.buckets.append(self.val + v)
-            self.val = v
+            self.buckets.append(self.buckets[-1] + v)
 
     def pickIndex(self) -> int:
         def findBucket(v):
             if v < self.buckets[0]:
                 return 0
-            l, r = 0, len(self.buckets)
-            m = (l + r) // 2
-            while l < r:
+            l, r = 0, len(self.buckets) - 1
+
+            while l <= r:
+                m = (l + r) // 2
                 if self.buckets[m] < v:
                     l = m + 1
-                elif self.buckets[m] == v or self.buckets[m - 1] < v <= self.buckets[m]:
+                elif self.buckets[m] == v or self.buckets[m - 1] <= v < self.buckets[m]:
                     return m
                 else:
                     r = m - 1
 
-        randomval = random.randrange(0, self.val)
-        return findBucket(randomval)
+        randomval = random.randint(1, self.buckets[-1])
+        # ind = findBucket(randomval)
+        ind = bisect.bisect_left(self.buckets, randomval)
+        return ind - 1
 
 
 obj = Solution(w=[1, 3])
+print(obj.pickIndex())
+print(obj.pickIndex())
 print(obj.pickIndex())
 print(obj.pickIndex())
 print(obj.pickIndex())
