dc057611d9767f8b2842bdbe1caa84b01132744b
Nimish <nimish.v.shinde60@gmail.com>
2025-03-02 15:26:36 +0530

    take and not take approach

--- 2025/Mar_25/Subsets.py
+++ 2025/Mar_25/Subsets.py
@@ -2,15 +2,17 @@ class Solution:
     def subsets(self, nums):
         n, res = len(nums), []
 
-        def solve(curr, elem):
-            if len(elem) == 0:
+        def solve(curr, ind):
+            if ind >= n:
+                res.append(list(curr))
                 return
-            curr.append(elem.pop())
-            res.append(curr)
-            for i in range(len(elem)):
-                res.append(curr.append(elem[i]))
-                curr.pop()
-            solve(curr, elem)
-            
-        solve([], nums)
+            curr.append(nums[ind])
+            solve(curr, ind + 1)
+            curr.pop()
+            solve(curr, ind + 1)
+
+        solve([], 0)
         return res
+
+
+print(Solution().subsets(nums = [1,2,3]))
