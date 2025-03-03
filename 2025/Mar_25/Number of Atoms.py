42ef6171594f4b8c6393884b0bac0534b63b6174
Nimish <nimish.v.shinde60@gmail.com>
2025-03-03 22:50:02 +0530

    first attempt with dictionary and stack

---
+++ 2025/Mar_25/Number of Atoms.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        curr, mol, num, st, i = defaultdict(int), '', '', [], 0
+        while i < len(formula):
+            c = formula[i]
+            if c.isnumeric():
+                num += c
+            if c.isalpha():
+                if mol and num:
+                    curr[mol] += int(num)
+                    mol, num = '', ''
+                if c.isupper() and mol:
+                    curr[mol] += 1
+                mol += c
+            if c == '(':
+                st.append(curr)
+                curr, mol, num = defaultdict(int), '', '', []
+            if c == ')':
+                suffix = ''
+                while i < len(formula) and formula[i].isnumeric():
+                    suffix += formula[i]
+                    i += 1
+                if suffix:
+                    prev = st.pop()
+                    for ele in prev:
+                        prev[ele] *= prev[ele]
+                    curr = st.pop()
+                    for ele in prev:
+                        curr[ele] += prev[ele]
+                    mol, num = '', ''
+                i -= 1
+            i += 1
+
+        res, keys = '', list(curr.keys())
+        keys.sort()
+        for val in keys:
+            res += val + str(curr[val])
+        return res
+
+
+
