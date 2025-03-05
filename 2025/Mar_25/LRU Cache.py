87ac56001f97b9e5f1786947a4e104fbfcc47e7d
Nimish <nimish.v.shinde60@gmail.com>
2025-03-05 22:23:12 +0530

    using doubly linked list data structure

--- 2025/Mar_25/LRU Cache.py
+++ 2025/Mar_25/LRU Cache.py
@@ -1,26 +1,41 @@
+class DLL:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+        self.prev = None
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.map, self.st, self.cap = {}, [], capacity
+        self.map, self.cap = {}, capacity
+        self.head, self.tail = DLL('a'), DLL('z')
 
     def get(self, key: int) -> int:
         if key not in self.map:
             return -1
-        for i in range(len(self.st)):
-            if self.st[i] == key:
-                del self.st[i]
-                break
-            self.st.append(key)
-        return self.map[key]
+        add, val = self.map[key]
+        self.remove_node(add)
+        self.push_back(key, val)
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
-            self.map[key] = value
-            self.st.append(key)
+            add, val = self.map[key]
+            self.remove_node(add)
+            self.push_back(key, val)
 
         elif len(self.map) == self.cap:
-            del self.map[self.st[0]]
-            del self.st[0]
-            self.map[key] = value
-            self.st.append(key)
-            
+            self.remove_node(self.head.next)
+        self.push_back(key, value)
+
+
+lru = LRUCache(2)
+lru.put(1, 1)
+lru.put(2, 2)
+print(lru.get(1))
+lru.put(3, 3)
+print(lru.get(2))
+lru.put(4,4)
+print(lru.get(1))
+print(lru.get(3))
+print(lru.get(4))
