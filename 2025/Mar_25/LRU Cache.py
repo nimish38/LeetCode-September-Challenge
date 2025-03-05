8a40b2e113e96e1ad67b9f94b25698c9eaf6ba36
Nimish <nimish.v.shinde60@gmail.com>
2025-03-05 22:39:07 +0530

    write remove and push functions

--- 2025/Mar_25/LRU Cache.py
+++ 2025/Mar_25/LRU Cache.py
@@ -10,6 +10,7 @@ class LRUCache:
     def __init__(self, capacity: int):
         self.map, self.cap = {}, capacity
         self.head, self.tail = DLL('a'), DLL('z')
+        self.head.next, self.tail.prev = self.tail, self.head
 
     def get(self, key: int) -> int:
         if key not in self.map:
@@ -17,16 +18,27 @@ class LRUCache:
         add, val = self.map[key]
         self.remove_node(add)
         self.push_back(key, val)
+        return val
 
     def put(self, key: int, value: int) -> None:
         if key in self.map:
             add, val = self.map[key]
             self.remove_node(add)
-            self.push_back(key, val)
-
         elif len(self.map) == self.cap:
             self.remove_node(self.head.next)
         self.push_back(key, value)
+
+    def remove_node(self, node):
+        p, n = node.prev, node.next
+        p.next, n.prev = n, p
+        del self.map[node.key]
+
+    def push_back(self, k, v):
+        new_node = DLL(k)
+        last = self.tail.prev
+        last.next, new_node.prev = new_node, last
+        new_node.next, self.tail.prev = self.tail, new_node
+        self.map[k] = (new_node, v)
 
 
 lru = LRUCache(2)
