a596e929da3d1d5c7c0a6b45d27944c1ddc333c6
Nimish <nimish.v.shinde60@gmail.com>
2025-03-07 23:26:06 +0530

    use binary search to find next job

--- 2025/Mar_25/Maximum profit in job.py
+++ 2025/Mar_25/Maximum profit in job.py
@@ -1,19 +1,28 @@
 class Solution:
     def jobScheduling(self, startTime, endTime, profit) -> int:
-        jobs = []
-        for i in range(len(profit)):
+        jobs, n = [], len(profit)
+        for i in range(n):
             jobs.append([startTime[i], endTime[i], profit[i]])
         jobs.sort(key=lambda x: x[0])
 
-        def solve(i, minStart):
+        def solve(i):
+            def findNextJob(i, target):
+                j, res = n - 1, n
+                while i <= j:
+                    mid = (i + j) // 2
+                    if jobs[mid][0] < target:
+                        i = mid + 1
+                    else:
+                        res = mid
+                        j = mid - 1
+                return res
             if i >= len(profit):
                 return 0
-            take, skip = 0, 0
-            if jobs[i][0] >= minStart:
-                take = jobs[i][2] + solve(i + 1, jobs[i][1])
-            skip = solve(i + 1, minStart)
+            next = findNextJob(i + 1, jobs[i][1])
+            take = jobs[i][2] + solve(next)
+            skip = solve(i + 1)
             return max(take, skip)
-        return solve(0, 0)
+        return solve(0)
 
 
 print(Solution().jobScheduling(startTime = [6,15,7,11,1,3,16,2], endTime = [19,18,19,16,10,8,19,8], profit = [2,9,1,19,5,7,3,19]))
