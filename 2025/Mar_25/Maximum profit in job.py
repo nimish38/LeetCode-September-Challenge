6e107d1ff8855ed2a0a4f5ee428eaeed6ad4cfcf
Nimish <nimish.v.shinde60@gmail.com>
2025-03-07 22:18:11 +0530

    sort on start time and combine vals

--- 2025/Mar_25/Maximum profit in job.py
+++ 2025/Mar_25/Maximum profit in job.py
@@ -1,14 +1,19 @@
 class Solution:
     def jobScheduling(self, startTime, endTime, profit) -> int:
+        jobs = []
+        for i in range(len(profit)):
+            jobs.append([startTime[i], endTime[i], profit[i]])
+        jobs.sort(key=lambda x: x[0])
+
         def solve(i, minStart):
             if i >= len(profit):
                 return 0
             take, skip = 0, 0
-            if startTime[i] >= minStart:
-                take = profit[i] + solve(i + 1, endTime[i])
+            if jobs[i][0] >= minStart:
+                take = jobs[i][2] + solve(i + 1, jobs[i][1])
             skip = solve(i + 1, minStart)
             return max(take, skip)
         return solve(0, 0)
 
 
-print(Solution().jobScheduling(startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70]))
+print(Solution().jobScheduling(startTime = [6,15,7,11,1,3,16,2], endTime = [19,18,19,16,10,8,19,8], profit = [2,9,1,19,5,7,3,19]))
