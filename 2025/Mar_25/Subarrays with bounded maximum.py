fa93c2028baee92a889a4718c77cbe3bcf0ab3b8
Nimish <nimish.v.shinde60@gmail.com>
2025-03-09 13:56:48 +0530

    debug and fix logic

--- 2025/Mar_25/Subarrays with bounded maximum.py
+++ 2025/Mar_25/Subarrays with bounded maximum.py
@@ -1,12 +1,20 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums, left: int, right: int) -> int:
-        cnt, curr = 0, 0
+        cnt, curr, currmax = 0, 0, -1
         for val in nums:
-            if left <= val <= right:
-                curr += 1
+            currmax = max(currmax, val)
+            if left <= currmax <= right:
+                if left <= val <= right:
+                    curr += 1
+                else:
+                    cnt += (curr * (curr + 1)) // 2
+                    cnt += 1
+                    curr = 0
             else:
                 cnt += (curr * (curr + 1)) // 2
-                curr = 0
+                curr, currmax = 0, -1
         cnt += (curr * (curr + 1)) // 2
         return cnt
 
+
+print(Solution().numSubarrayBoundedMax(nums = [2,9,2,5,6], left = 2, right = 8))
