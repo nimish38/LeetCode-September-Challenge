36cd9a7892f829ee2dfdee069218e344415f94a3
Nimish <nimish.v.shinde60@gmail.com>
2025-03-09 14:10:10 +0530

    count outbound but still satisfying elements

--- 2025/Mar_25/Subarrays with bounded maximum.py
+++ 2025/Mar_25/Subarrays with bounded maximum.py
@@ -1,19 +1,21 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums, left: int, right: int) -> int:
-        cnt, curr, currmax = 0, 0, -1
+        cnt, curr, currmax, out = 0, 0, -1, 0
         for val in nums:
             currmax = max(currmax, val)
             if left <= currmax <= right:
                 if left <= val <= right:
-                    curr += 1
+                    cnt -= (out * (out + 1)) // 2
+                    out = 0
                 else:
-                    cnt += (curr * (curr + 1)) // 2
-                    cnt += 1
-                    curr = 0
+                    out += 1
+                curr += 1
             else:
                 cnt += (curr * (curr + 1)) // 2
-                curr, currmax = 0, -1
+                cnt -= (out * (out + 1)) // 2
+                curr, currmax, out = 0, -1, 0
         cnt += (curr * (curr + 1)) // 2
+        cnt -= (out * (out + 1)) // 2
         return cnt
 
 
