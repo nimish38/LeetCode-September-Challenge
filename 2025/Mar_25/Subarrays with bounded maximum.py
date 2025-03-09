e347690c1a785ce90ab68cd699706df97e27b21a
Nimish <nimish.v.shinde60@gmail.com>
2025-03-09 14:36:33 +0530

    sliding window

--- 2025/Mar_25/Subarrays with bounded maximum.py
+++ 2025/Mar_25/Subarrays with bounded maximum.py
@@ -1,21 +1,12 @@
 class Solution:
     def numSubarrayBoundedMax(self, nums, left: int, right: int) -> int:
-        cnt, curr, currmax, out = 0, 0, -1, 0
-        for val in nums:
-            currmax = max(currmax, val)
-            if left <= currmax <= right:
-                if left <= val <= right:
-                    cnt -= (out * (out + 1)) // 2
-                    out = 0
-                else:
-                    out += 1
-                curr += 1
-            else:
-                cnt += (curr * (curr + 1)) // 2
-                cnt -= (out * (out + 1)) // 2
-                curr, currmax, out = 0, -1, 0
-        cnt += (curr * (curr + 1)) // 2
-        cnt -= (out * (out + 1)) // 2
+        cnt, curr, i = 0, 0, 0
+        for j in range(len(nums)):
+            if nums[j] > right:
+                curr, i = 0, j + 1
+            elif left <= nums[j] <= right:
+                curr = j - i + 1
+            cnt += curr
         return cnt
 
 
