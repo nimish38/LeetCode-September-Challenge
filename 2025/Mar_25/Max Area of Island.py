a730c5ec48f3078f0c432a0ce82256fd3f87ba05
Nimish <nimish.v.shinde60@gmail.com>
2025-03-10 23:03:04 +0530

    duplicate value in stack fix

--- 2025/Mar_25/Max Area of Island.py
+++ 2025/Mar_25/Max Area of Island.py
@@ -4,14 +4,15 @@ class Solution:
 
         def solve(i, j):
             cnt, adj, st = 0, [(1, 0), (-1, 0), (0, 1), (0, -1)], [(i, j)]
+            grid[i][j] = 'V'
             while st:
                 a, b = st.pop()
-                grid[a][b] = 'V'
                 cnt += 1
                 for x, y in adj:
                     p, q = a + x, b + y
                     if 0 <= p < m and 0 <= q < n and grid[p][q] == 1:
                         st.append((p, q))
+                        grid[p][q] = 'V'
             return cnt
 
         for i in range(m):
@@ -21,6 +22,4 @@ class Solution:
         return res
 
 
-print(Solution().maxAreaOfIsland(grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],
-                                         [0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],
-                                         [0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
+print(Solution().maxAreaOfIsland(grid = [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]))
