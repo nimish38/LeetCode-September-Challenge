044dbf43206e4cc5b43196c7e52336e64be59aed
Nimish <nimish.v.shinde60@gmail.com>
2025-03-11 23:51:17 +0530

    test solution and fix

--- 2025/Mar_25/K closest points to origin.py
+++ 2025/Mar_25/K closest points to origin.py
@@ -1,12 +1,15 @@
 class Solution:
     def kClosest(self, points, k: int):
-        dis = []
+        dis, res = [], []
         for x, y in points:
             val = (x ** 2) + (y ** 2)
             dis.append([val, [x, y]])
 
         dis.sort(key=lambda x: x[0])
-        return dis[:k]
 
+        for i in range(k):
+            res.append(dis[i][1])
+        return res
+    
 
-print(Solution)
+print(Solution().kClosest(points = [[1,3],[-2,2]], k = 1))
