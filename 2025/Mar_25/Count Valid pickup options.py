5d75eec94bb15c20638d0e04d45cea5c04ff500d
Nimish <nimish.v.shinde60@gmail.com>
2025-03-12 22:53:07 +0530

    test and debug

--- 2025/Mar_25/Count Valid pickup options.py
+++ 2025/Mar_25/Count Valid pickup options.py
@@ -1,7 +1,10 @@
 class Solution:
     def countOrders(self, n: int) -> int:
         cnt = 1
-        for i in range(2, n):
-            spaces = (n - 1) * 2 + 1
+        for i in range(2, n + 1):
+            spaces = (i - 1) * 2 + 1
             cnt *= spaces * (spaces + 1) // 2
-        return cnt
+        return cnt % (10**9 + 7)
+
+
+print(Solution().countOrders(n = 3))
