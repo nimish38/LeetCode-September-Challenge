091515fa888d0576553d80b22fc91b7c7497b790
Nimish <nimish.v.shinde60@gmail.com>
2025-03-13 22:30:09 +0530

    Kahns algorithm topological sort

--- 2025/Mar_25/Course Schedule 2.py
+++ 2025/Mar_25/Course Schedule 2.py
@@ -1,19 +1,26 @@
-from collections import defaultdict
+from collections import deque, defaultdict
 
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites):
-        indegree, adjecancy, res = [0] * (numCourses - 1), defaultdict(list), []
+        indegree, adjacency, res, que = [0] * numCourses, defaultdict(list), [], deque()
         for a, b in prerequisites:
-            adjecancy[a].append(b)
             indegree[a] += 1
+            adjacency[b].append(a)
         for i in range(numCourses):
             if indegree[i] == 0:
                 res.append(i)
-        for a, b in prerequisites:
-            indegree[a] -= 1
-            if indegree[a] == 0:
-                res.append(a)
+                que.append(i)
+        while que:
+            out = que.popleft()
+            for nei in adjacency[out]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    res.append(nei)
+                    que.append(nei)
         if len(res) == numCourses:
             return res
         return []
+
+
+print(Solution().findOrder(numCourses = 3, prerequisites = [[2,1],[1,0]]))
