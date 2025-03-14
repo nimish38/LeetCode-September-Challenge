e6d797d4d27da6e40c38b56b3e585d126babac27
Nimish <nimish.v.shinde60@gmail.com>
2025-03-14 22:25:41 +0530

    use min heap

--- 2025/Mar_25/Max Team Performance.py
+++ 2025/Mar_25/Max Team Performance.py
@@ -1,12 +1,20 @@
+import heapq
+
+
 class Solution:
     def maxPerformance(self, n: int, speed, efficiency, k: int) -> int:
-        ratio, sum, minEff = [], 0, float('-inf')
+        combo, curr, bestsum, heap = [], 0, 0, []
         for i in range(n):
-            ratio.append((speed[i] / efficiency[i], i))
-        ratio.sort(key=lambda x: x[0], reverse=True)
-        for i in range(k):
-            sum += sum[ratio[i][1]]
-            minEff = min(minEff, efficiency[ratio[i][1]])
-        return sum * minEff
+            combo.append((efficiency[i], speed[i]))
+        combo.sort(key=lambda x: x[0], reverse=True)
+        for i in range(n):
+            if len(heap) >= k:
+                curr -= heapq.heappop(heap)
+            curr += combo[i][1]
+            heapq.heappush(heap, combo[i][1])
+            bestsum = max(bestsum, curr * combo[i][0])
+        return bestsum % (10**9 + 7)
 
-    
+
+print(Solution().maxPerformance(n = 6, speed = [2,10,3,1,5,8], efficiency = [5,4,3,9,7,2], k = 2))
+
