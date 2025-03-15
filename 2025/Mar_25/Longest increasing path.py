16d4528c368416a077425a0e5aea5ea3c73033d4
Nimish <nimish.v.shinde60@gmail.com>
2025-03-15 14:21:52 +0530

    dp recursive approach

---
+++ 2025/Mar_25/Longest increasing path.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestIncreasingPath(self, matrix) -> int:
+        m, n, res = len(matrix), len(matrix[0]), 0
+
+        def solve(i, j):
+            curr, adj = 0, [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            for p, q in adj:
+                x, y = i + p, j + q
+                if 0 <= x < m and 0 <= y < n and matrix[x][y] > matrix[i][j]:
+                    curr = max(curr, solve(x, y))
+            return 1 + curr
+
+        for i in range(m):
+            for j in range(n):
+                res = max(res, solve(i, j))
+        return res
+
+
