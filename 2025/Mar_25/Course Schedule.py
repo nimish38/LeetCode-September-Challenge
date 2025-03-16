e16894c79f86e514d661e4cc608387df8ff4dc90
Nimish <nimish.v.shinde60@gmail.com>
2025-03-16 21:50:06 +0530

    topological sort

--- 2025/Mar_25/Course Schedule.py
+++ 2025/Mar_25/Course Schedule.py
@@ -1,10 +1,22 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites) -> bool:
-        indegree, adjacency = [0] * numCourses, defaultdict(list)
+        indegree, adjacency, que, cnt = [0] * numCourses, defaultdict(list), deque(), 0
         for a, b in prerequisites:
             indegree[a] += 1
             adjacency[b].append(a)
-        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                que.append(i)
+                cnt += 1
+        while que:
+            course = que.popleft()
+            for nei in adjacency[course]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    que.append(nei)
+                    cnt += 1
+        return cnt == numCourses
+
