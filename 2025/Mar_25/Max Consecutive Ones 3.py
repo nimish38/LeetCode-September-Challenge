a3ce9caaa73df02c02544940c540c87e2167c5f5
Nimish <nimish.v.shinde60@gmail.com>
2025-03-20 22:38:23 +0530

    brute force with finding subarrays with max k 0

--- 2025/Mar_25/Max Consecutive Ones 3.py
+++ 2025/Mar_25/Max Consecutive Ones 3.py
@@ -1,30 +1,16 @@
 class Solution:
     def longestOnes(self, nums, k: int) -> int:
-        res, n = 0, len(nums)
-
-        def value(arr):
-            i, curr, best = 0, 0, 0
-            while i < n:
-                if arr[i]:
-                    curr += 1
+        best, n = 0, len(nums)
+        for i in range(n):
+            zero = 0
+            for j in range(i, n):
+                if not nums[j]:
+                    zero += 1
+                if zero <= k:
+                    best = max(best, j - i + 1)
                 else:
-                    best, curr = max(best, curr), 0
-                i += 1
-            return max(best, curr)
-
-        def solve(i, rem):
-            if i >= n or rem == 0:
-                return value(nums)
-            flip, dont = 0, 0
-            if not nums[i]:
-                nums[i] = 1
-                flip = solve(i + 1, rem - 1)
-                nums[i] = 0
-            dont = solve(i + 1, rem)
-            return max(flip, dont)
-
-        res = solve(0, k)
-        return res
+                    break
+        return best
 
 
-print(Solution().longestOnes(nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3))
+print(Solution().longestOnes([1,1,1,0,0,0,1,1,1,1,0], k = 2))
