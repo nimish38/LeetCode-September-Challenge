7de3956ed26fc4c17b737b3bcae108f4e363412b
Nimish <nimish.v.shinde60@gmail.com>
2025-03-21 22:32:13 +0530

    stack solution

--- 2025/Mar_25/Remove Adjacent Duplicates.py
+++ 2025/Mar_25/Remove Adjacent Duplicates.py
@@ -1,16 +1,17 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
-        muta, n = list(s), len(s)
-        while True:
-            start, i = len(muta), 0
-            while i + k <= len(muta):
-                val = muta[i: i + k]
-                if len(set(val)) == 1:
-                    del muta[i: i + k]
-                    i = max(-1, i - k)
-                i += 1
-            if start == len(muta):
-                return ''.join(muta)
+        st = []
+        for c in s:
+            if len(st) >= k - 1:
+                curr = set(st[len(st) - k + 1:])
+                curr.add(c)
+                if len(curr) == 1:
+                    del st[len(st) - k + 1:]
+                else:
+                    st.append(c)
+            else:
+                st.append(c)
+        return ''.join(st)
 
 
 print(Solution().removeDuplicates(s = "yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy", k = 4))
