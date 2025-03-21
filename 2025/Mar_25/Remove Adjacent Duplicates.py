e3b07f975362860939197c89d8eadec7345f9544
Nimish <nimish.v.shinde60@gmail.com>
2025-03-21 21:23:34 +0530

    list manipulation attempt and fix

--- 2025/Mar_25/Remove Adjacent Duplicates.py
+++ 2025/Mar_25/Remove Adjacent Duplicates.py
@@ -3,12 +3,15 @@ class Solution:
         muta, n = list(s), len(s)
         while True:
             start, i = len(muta), 0
-            while i + k < n:
-                if len(set(muta[i: i + k])) == 1:
+            while i + k < len(muta):
+                val = muta[i: i + k]
+                if len(set(val)) == 1:
                     del muta[i : i + k]
-                    i = 0
+                    i = -1
+                i += 1
             if start == len(muta):
                 return ''.join(muta)
 
-        
+
+print(Solution().removeDuplicates(s = "pbbcggttciiippooaais", k = 2))
 
