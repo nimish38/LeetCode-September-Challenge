db3f43fb18b1fa0f338b543774698ce59be86ee0
Nimish <nimish.v.shinde60@gmail.com>
2025-03-21 22:42:14 +0530

    stack solution with frequency store

--- 2025/Mar_25/Remove Adjacent Duplicates.py
+++ 2025/Mar_25/Remove Adjacent Duplicates.py
@@ -2,16 +2,21 @@ class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         st = []
         for c in s:
-            if len(st) >= k - 1:
-                curr = set(st[len(st) - k + 1:])
-                curr.add(c)
-                if len(curr) == 1:
-                    del st[len(st) - k + 1:]
-                else:
-                    st.append(c)
+            if not st:
+                st.append([c, 1])
             else:
-                st.append(c)
-        return ''.join(st)
+                val, cnt = st[-1]
+                if val == c:
+                    if cnt == k - 1:
+                        st.pop()
+                    else:
+                        st[-1][1] += 1
+                else:
+                    st.append([c, 1])
+        res = ''
+        for c, v in st:
+            res += (c * v)
+        return res
 
 
 print(Solution().removeDuplicates(s = "yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy", k = 4))
