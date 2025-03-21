f5e2a04c174f9ec36d1244dc1b405a8c9f6cf9da
Nimish <nimish.v.shinde60@gmail.com>
2025-03-21 21:29:16 +0530

    go back only k steps

--- 2025/Mar_25/Remove Adjacent Duplicates.py
+++ 2025/Mar_25/Remove Adjacent Duplicates.py
@@ -3,15 +3,15 @@ class Solution:
         muta, n = list(s), len(s)
         while True:
             start, i = len(muta), 0
-            while i + k < len(muta):
+            while i + k <= len(muta):
                 val = muta[i: i + k]
                 if len(set(val)) == 1:
-                    del muta[i : i + k]
-                    i = -1
+                    del muta[i: i + k]
+                    i = max(-1, i - k)
                 i += 1
             if start == len(muta):
                 return ''.join(muta)
 
 
-print(Solution().removeDuplicates(s = "pbbcggttciiippooaais", k = 2))
+print(Solution().removeDuplicates(s = "yfttttfbbbbnnnnffbgffffgbbbbgssssgthyyyy", k = 4))
 
