f8533b5998143080addd0e27b3036beb258c98e1
Nimish <nimish.v.shinde60@gmail.com>
2025-03-22 13:03:44 +0530

    test and fix

--- 2025/Mar_25/Diagonal Traverse.py
+++ 2025/Mar_25/Diagonal Traverse.py
@@ -1,26 +1,32 @@
 class Solution:
     def findDiagonalOrder(self, mat):
         m, n, res, i, j = len(mat), len(mat[0]), [], 0, 0
-        while i != m - 1 and j != n - 1:
+        while not (i == m - 1 and j == n - 1):
             res.append(mat[i][j])
 
-            while i - 1 > 0 and j + 1 < n:
+            while i - 1 >= 0 and j + 1 < n:
                 i -= 1
                 j += 1
                 res.append(mat[i][j])
 
-            if i - 1 > 0:
+            if j + 1 < n:
                 j += 1
             else:
                 i += 1
             res.append(mat[i][j])
 
-            while i + 1 < m and j - 1 > 0:
+            while i + 1 < m and j - 1 >= 0:
                 i += 1
                 j -= 1
                 res.append(mat[i][j])
 
-            if i + 1 > 0:
-                j += 1
-            else:
+            if i + 1 < m:
                 i += 1
+            else:
+                j += 1
+
+        res.append(mat[i][j])
+        return res
+
+
+print(Solution().findDiagonalOrder(mat = [[1,2,3],[4,5,6],[7,8,9]]))
