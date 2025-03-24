5925141a49b021bd12c4412c3470601e39442bfb
Nimish <nimish.v.shinde60@gmail.com>
2025-03-24 22:43:19 +0530

    attempt with two lists

---
+++ 2025/Mar_25/Permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums):
+        res, n = [], len(nums)
+
+        def solve(arr, combo):
+            if len(combo) == n:
+                res.append(combo)
+                return
+            for i in range(len(arr)):
+                x = arr.pop(i)
+                combo.append(x)
+                solve(arr, combo)
+                arr.insert(i, x)
+                combo.pop()
+
+        solve(nums, [])
+        return res
