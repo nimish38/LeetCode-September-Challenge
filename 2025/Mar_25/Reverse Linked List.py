5807df0cb62311072f43536cdad3b0c7c2a01dd1
Nimish <nimish.v.shinde60@gmail.com>
2025-03-25 20:03:28 +0530

    build linked list

--- 2025/Mar_25/Reverse Linked List.py
+++ 2025/Mar_25/Reverse Linked List.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, val, next = None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head):
         if not head or head.next:
@@ -7,4 +13,9 @@ class Solution:
             n, c.next = c.next, p
             p, c = c, n
         return p
-    
+
+
+a, b, c, d, e = Node(1), Node(2), Node(3), Node(4), Node(5)
+a.next, b.next, c.next, d.next = b, c, d, e
+x = Solution().reverseList(a)
+print(x)
