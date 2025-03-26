07320145660aa1dbe9731e67acb030531804a680
Nimish <nimish.v.shinde60@gmail.com>
2025-03-26 22:37:53 +0530

    test and fix

--- 2025/Mar_25/Verifying Alien Dictionary.py
+++ 2025/Mar_25/Verifying Alien Dictionary.py
@@ -9,14 +9,17 @@ class Solution:
                     return True
                 elif pos[first[i]] > pos[second[i]]:
                     return False
-            if len(second) > run:
+            if len(second) < run:
                 return False
             return True
 
         for i in range(len(order)):
             pos[order[i]] = i
 
-        for i in range(len(words)):
+        for i in range(1, len(words)):
             if not lexo(words[i - 1], words[i]):
                 return False
         return True
+
+
+print(Solution().isAlienSorted(words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"))
