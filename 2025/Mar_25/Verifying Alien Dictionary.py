1b206b346fcf26a02d692d951627eaf7fc62666c
Nimish <nimish.v.shinde60@gmail.com>
2025-03-26 22:33:00 +0530

    lexographic order check

--- 2025/Mar_25/Verifying Alien Dictionary.py
+++ 2025/Mar_25/Verifying Alien Dictionary.py
@@ -1,6 +1,18 @@
 class Solution:
     def isAlienSorted(self, words, order: str) -> bool:
         pos = {}
+
+        def lexo(first, second):
+            run = min(len(first), len(second))
+            for i in range(run):
+                if pos[first[i]] < pos[second[i]]:
+                    return True
+                elif pos[first[i]] > pos[second[i]]:
+                    return False
+            if len(second) > run:
+                return False
+            return True
+
         for i in range(len(order)):
             pos[order[i]] = i
 
