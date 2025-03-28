160fa440f49301b98a628522cce328d05b95955e
Nimish <nimish.v.shinde60@gmail.com>
2025-03-28 21:45:36 +0530

    test solution

--- 2025/Mar_25/Product of array except self.py
+++ 2025/Mar_25/Product of array except self.py
@@ -3,9 +3,10 @@ class Solution:
         left, right, ans = 1, 1, [1] * len(nums)
         for i, val in enumerate(nums):
             ans[i], left = left, left * val
-
         for i in range(len(nums) - 1, -1, -1):
             ans[i] *= right
             right *= nums[i]
-
         return ans
+
+
+print(Solution().productExceptSelf(nums = [-1,1,0,-3,3]))
