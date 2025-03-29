74b8632324bb7cfcde49012908f9cef9ac92486f
Nimish <nimish.v.shinde60@gmail.com>
2025-03-29 14:25:25 +0530

    2 pointer approach

---
+++ 2025/Mar_25/Valid Palindrome 2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                left, right = s[i + 1: j], s[i: j]
+                if left == left[:: -1] or right == right[:: -1]:
+                    return True
+                return False
+        return True
