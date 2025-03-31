66a2e9ca961a4e7d7ebbbd534b38040715e64e09
Nimish <nimish.v.shinde60@gmail.com>
2025-03-31 14:14:39 +0530

    set string digits

--- 2025/Mar_25/Add strings.py
+++ 2025/Mar_25/Add strings.py
@@ -1,43 +1,10 @@
+import sys
+
+
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        i, j, carry, res = len(num1) - 1, len(num2) - 1, False, ''
-        while i >= 0 and j >= 0:
-            val = int(num1[i]) + int(num2[j])
-            if carry:
-                val += 1
-                carry = False
-            if val > 9:
-                carry = True
-                res = str(val % 10) + res
-            else:
-                res = str(val) + res
-            i -= 1
-            j -= 1
-        while i >= 0:
-            val = int(num1[i])
-            if carry:
-                val += 1
-                carry = False
-            if val > 9:
-                carry = True
-                res = str(val % 10) + res
-            else:
-                res = str(val) + res
-            i -= 1
-        while j >= 0:
-            val = int(num2[j])
-            if carry:
-                val += 1
-                carry = False
-            if val > 9:
-                carry = True
-                res = str(val % 10) + res
-            else:
-                res = str(val) + res
-            j -= 1
-        if carry:
-            return '1' + res
-        return res
+        sys.set_int_max_str_digits(50000)
+        return str(int(num1)+int(num2))
 
 
 print(Solution().addStrings(num1 = "11", num2 = "123"))
