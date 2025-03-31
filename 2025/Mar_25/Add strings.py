050f98e96a195732d4cc6b5ceb317d672b3a7187
Nimish <nimish.v.shinde60@gmail.com>
2025-03-31 14:09:33 +0530

    digit and carry addition

--- 2025/Mar_25/Add strings.py
+++ 2025/Mar_25/Add strings.py
@@ -1,6 +1,37 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1) + int(num2))
+        i, j, carry, res = len(num1) - 1, len(num2) - 1, False, ''
+        while i >= 0 and j >= 0:
+            val = int(num1[i]) + int(num2[j])
+            if carry:
+                val += 1
+                carry = False
+            if val > 9:
+                carry = True
+                res = str(val % 10) + res
+            i -= 1
+            j -= 1
+        while i >= 0:
+            val = int(num1[i])
+            if carry:
+                val += 1
+                carry = False
+            if val > 9:
+                carry = True
+                res = str(val % 10) + res
+            i -= 1
+        while j >= 0:
+            val = int(num1[j])
+            if carry:
+                val += 1
+                carry = False
+            if val > 9:
+                carry = True
+                res = str(val % 10) + res
+            j -= 1
+        if carry:
+            return '1' + res
+        return res
 
 
 print(Solution().addStrings(num1 = "11", num2 = "123"))
