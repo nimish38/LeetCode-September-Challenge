b32966e04334ac986157d62ab8b2ea46f83bb7af
Nimish <nimish.v.shinde60@gmail.com>
2025-04-01 22:40:51 +0530

    using builtin functions

--- 2025/Apr_25/Add binary.py
+++ 2025/Apr_25/Add binary.py
@@ -1,34 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        i, j, res, carry = len(a) - 1, len(b) - 1, '', 0
-        while i >= 0 and j >= 0:
-            val = int(a[i]) + int(b[j]) + carry
-            res = str(val % 2) + res
-            if val > 1:
-                carry = 1
-            else:
-                carry = 0
-            i -= 1
-            j -= 1
+        return bin(int(a, 2) + int(b, 2))[2:]
 
-        while i >= 0:
-            val = int(a[i]) + carry
-            res = str(val % 2) + res
-            if val > 1:
-                carry = 1
-            else:
-                carry = 0
-            i -= 1
 
-        while j >= 0:
-            val = int(b[j]) + carry
-            res = str(val % 2) + res
-            if val > 1:
-                carry = 1
-            else:
-                carry = 0
-            j -= 1
-
-        if carry:
-            return '1' + res
-        return res
+print(Solution().addBinary(a = "11", b = "1"))
