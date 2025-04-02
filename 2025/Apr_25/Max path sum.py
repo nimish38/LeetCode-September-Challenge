a55650dd280a23a0008226096a489962a3d494a0
Nimish <nimish.v.shinde60@gmail.com>
2025-04-02 22:50:28 +0530

    return only feasible path extensions

--- 2025/Apr_25/Max path sum.py
+++ 2025/Apr_25/Max path sum.py
@@ -7,14 +7,18 @@ class TreeNode:
 
 class Solution:
     def maxPathSum(self, root):
-        left, right, self.best = 0, 0, 0
+        self.maxsum = 0
 
         def solve(node):
             if not node:
                 return 0
-            leftsum, rightsum = solve(node.left), solve(node.right)
-            self.best = max(self.best, node.val + leftsum, node.val + rightsum)
-            return self.best
 
-        left, right = solve(root.left), solve(root.right)
-        return max(left + right + root.val, left, right, self.best)
+            leftval, rightval = solve(node.left), solve(node.right)
+            downward_path = leftval + rightval + node.val
+            one_side_path = max(leftval, rightval) + node.val
+            only_root = node.val
+            self.maxsum = max(self.maxsum, downward_path, one_side_path, only_root)
+            return max(one_side_path, only_root)
+
+        solve(root)
+        return self.maxsum
