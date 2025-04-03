8cd65c31c8b19ffced99d84cdc439b44e435dee0
Nimish <nimish.v.shinde60@gmail.com>
2025-04-03 21:03:01 +0530

    using extra string

--- 2025/Apr_25/Valid Palindrome.py
+++ 2025/Apr_25/Valid Palindrome.py
@@ -1,11 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i, j = 0, len(s) - 1
-        while i > j:
-            if not s[i].isalnum():
-                i += 1
-            if not s[j].isalnum():
-                j -= 1
-            elif s[i] == s[j]:
-                return False
-        return True
+        s, temp = s.lower(), ''
+        for c in s:
+            if c.isalnum():
+                temp += c
+        return temp == temp[::-1]
+
+
+print(Solution().isPalindrome(s = "A man, a plan, a canal: Panama"))
