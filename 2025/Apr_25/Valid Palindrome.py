e2c7135f016795d889159fdb3bc589d3aa16879e
Nimish <nimish.v.shinde60@gmail.com>
2025-04-03 21:11:03 +0530

    two pointer solution

--- 2025/Apr_25/Valid Palindrome.py
+++ 2025/Apr_25/Valid Palindrome.py
@@ -1,10 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s, temp = s.lower(), ''
-        for c in s:
-            if c.isalnum():
-                temp += c
-        return temp == temp[::-1]
+        i, j, s = 0, len(s) - 1, s.lower()
+        while i < j:
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i] == s[j]:
+                    i += 1
+                    j -= 1
+                else:
+                    print(i, j, s[i], s[j])
+                    return False
+            else:
+                if not s[i].isalnum():
+                    i += 1
+                if not s[j].isalnum():
+                    j -= 1
+        return True
 
 
 print(Solution().isPalindrome(s = "A man, a plan, a canal: Panama"))
