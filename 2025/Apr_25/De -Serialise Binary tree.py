07d4d71ab4cedfc3360cfe5040a461071154feb6
nimish38 <nimish.v.shinde60@gmail.com>
2025-04-29 22:54:21 +0530

    make serialization recursive as well

--- 2025/Apr_25/De -Serialise Binary tree.py
+++ 2025/Apr_25/De -Serialise Binary tree.py
@@ -9,26 +9,39 @@ class TreeNode(object):
 class Codec:
 
     def serialize(self, root):
-        res, st = '', deque([root])
-        while st:
-            node = st.popleft()
-            if not node:
-                res += '#$'
+        self.res = ''
+        def build_str(node):
+            self.res += '#' + str(node.val)
+            if node.left:
+                build_str(node.left)
             else:
-                res += '#' + str(node.val)
-                st.append(node.left)
-                st.append(node.right)
-        return res
+                self.res += '#$'
+            if node.right:
+                build_str(node.right)
+            else:
+                self.res += '#$'
+        build_str(root)
+        return self.res
 
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-
-        :type data: str
-        :rtype: TreeNode
-        """
+        data, self.ind = data.split('#'), 1
+        
+        def build_tree():
+            if data[self.ind] != '$':
+                node = TreeNode(int(data[self.ind]))
+                self.ind += 1
+                node.left = build_tree()
+                self.ind += 1
+                node.right = build_tree()
+                return node
+            return None
+        
+        return build_tree()
 
 a, b, c, d, e = TreeNode(1), TreeNode(2), TreeNode(3), TreeNode(4), TreeNode(5)
 a.left, a.right, c.left, c.right = b, c, d, e
 x = Codec().serialize(a)
 print(x)
+y = Codec().deserialize(x)
+print(y)
