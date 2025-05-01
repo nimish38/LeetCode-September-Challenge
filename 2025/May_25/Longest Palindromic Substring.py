ba736af5c93af4eeb5fd7b9304ba4dac9dbf9803
nimish38 <nimish.v.shinde60@gmail.com>
2025-05-01 18:50:39 +0530

    using expansion logic on each index

--- 2025/May_25/Longest Palindromic Substring.py
+++ 2025/May_25/Longest Palindromic Substring.py
@@ -1,12 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        for j in range(n, -1, -1):
-            for i in range(n - j + 1):
-                curr = s[i: i + j]
-                # print(curr)
-                if curr == curr[::-1]:
-                    return curr
-        return ''
+        n, best, res = len(s), 0, ''
+        def expand(i, j):
+            left, right = i, j
+            while left > 0 and right < n -1 and s[left - 1] == s[right + 1]:
+                left -= 1
+                right += 1
+            return s[left: right + 1]
+
+        for ind in range(n):
+            odd = expand(ind, ind)
+            if len(odd) > best:
+                best, res = len(odd), odd
+
+            if ind < n - 1 and s[ind] == s[ind + 1]:
+                even = expand(ind, ind + 1)
+                if len(even) > best:
+                    best, res = len(even), even
+        return res
 
 print(Solution().longestPalindrome(s = "cbbd"))
