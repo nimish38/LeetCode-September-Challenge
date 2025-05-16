f3078ba67c7ee8f213ce2cdae18a49df50ab6f9b
nimish38 <nimish.v.shinde60@gmail.com>
2025-05-16 22:46:58 +0530

    DP, move one index at a time

---
+++ 2025/May_25/Longest Common Subseq.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        m, n = len(text1), len(text2)
+        memo = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j -1]:
+                    memo[i][j] = 1 + memo[i - 1][j - 1]
+                else:
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+        return memo[m][n]
+
