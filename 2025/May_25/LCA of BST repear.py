54f290b2e33c04112e565d99eb9559257ae2f8ee
nimish38 <nimish.v.shinde60@gmail.com>
2025-05-30 00:27:40 +0530

    solved for both binary tree and bst

---
+++ 2025/May_25/LCA of BST repear.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left, right = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        if not left:
+            return right
+        return left
