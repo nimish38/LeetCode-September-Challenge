2542a91242a7903a38b1abcb1a899c329dff6e1f
nimish38 <nimish.v.shinde60@gmail.com>
2025-06-18 00:58:54 +0530

    finding peak and binary search in each half

--- 2025/Jun_25/Find in Mountain array.py
+++ 2025/Jun_25/Find in Mountain array.py
@@ -19,19 +19,19 @@ class Solution(object):
         l, r = 0, mountainArr.length() - 1
 
         def findPeak(l, r):
-            best = -1
+            best, bestVal = -1, -1
             while l < r:
                 mid = l + ((r - l) // 2)
                 one, two = mountainArr.get(mid), mountainArr.get(mid + 1)
                 if one > two:
-                    best = mid
+                    best, bestVal = mid, one
                     r = mid
                 else:
                     l = mid + 1
-            return best
+            return best, bestVal
 
         def binary_search(l, r, asc):
-            while l < r:
+            while l <= r:
                 mid = l + ((r - l) // 2)
                 val = mountainArr.get(mid)
                 if val == target:
@@ -48,13 +48,15 @@ class Solution(object):
                         r = mid - 1
             return -1
 
-        peak = findPeak(l, r)
-        found = binary_search(l, peak, True)
-        if found == -1:
-            return binary_search(peak + 1, r, False)
+        peak, value = findPeak(l, r)
+        found, left, right = -1, mountainArr.get(l), mountainArr.get(r)
+        if left <= target <= value:
+            found = binary_search(l, peak, True)
+        if found == -1 and value > target >= right:
+            found =  binary_search(peak + 1, r, False)
         return found
 
 
-a = [1,2,3,4,5,3,1]
+a = [1,5,2]
 mount = MountainArray(a)
 print(Solution().findInMountainArray(2, mount))
