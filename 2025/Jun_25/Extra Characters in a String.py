e6aa5bc12489e47e31e3bc6eef89eaf28164b92d
nimish38 <nimish.v.shinde60@gmail.com>
2025-06-19 21:46:47 +0530

    Bottom up approach

--- 2025/Jun_25/Extra Characters in a String.py
+++ 2025/Jun_25/Extra Characters in a String.py
@@ -1,19 +1,16 @@
 class Solution(object):
     def minExtraChar(self, s, dictionary):
         dictionary, n = {word: 1 for word in dictionary}, len(s)
-        memo = [-1] * n
-        def solve(ind):
-            if ind >= n:
-                return 0
-            if memo[ind] == -1:
-                res, val = n, ''
-                for i in range(ind, n):
-                    val += s[i]
-                    if val in dictionary:
-                        res = min(res, solve(i + 1))
-                res = min(res, 1 + solve(ind + 1))
-                memo[ind] = res
-            return memo[ind]
-        return solve(0)
+        memo = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            memo[i] = 1 + memo[i + 1]
+            curr = ''
+            for j in range(i, n):
+                curr += s[j]
+                if curr in dictionary:
+                    memo[i] = min(memo[i], memo[j + 1])
+        return memo[0]
+
 
 print(Solution().minExtraChar(s = "sayhelloworld", dictionary = ["hello","world"]))
