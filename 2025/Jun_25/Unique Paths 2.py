6bad93d546916eaf3aef7f8c45c75760d4e69da0
nimish38 <nimish.v.shinde60@gmail.com>
2025-06-26 21:37:01 +0530

    by bottom up dp

--- 2025/Jun_25/Unique Paths 2.py
+++ 2025/Jun_25/Unique Paths 2.py
@@ -1,20 +1,22 @@
 class Solution(object):
     def uniquePathsWithObstacles(self, obstacleGrid):
+        if obstacleGrid[-1][-1] or obstacleGrid[0][0]:
+            return 0
         m, n, cnt = len(obstacleGrid), len(obstacleGrid[0]), 0
-        memo = [[-1] * n for _ in range(m)]
-        def dfs(i, j):
-            if i == m - 1 and j == n - 1:
-                return 1
-            if memo[i][j] == -1:
-                obstacleGrid[i][j], val = 1, 0
-                for x, y in [(1, 0), (0, 1)]:
-                    a, b = i + x, j + y
-                    if m > a >= 0 and 0 <= b < n and obstacleGrid[a][b] == 0:
-                        val += dfs(a, b)
-                obstacleGrid[i][j], memo[i][j] = 0, val
-            return memo[i][j]
-        if not obstacleGrid[0][0]:
-            cnt = dfs(0, 0)
-        return cnt
+        memo = [[0] * n for _ in range(m)]
+        for _ in range(m):
+            if obstacleGrid[_][0]:
+                break
+            memo[_][0] = 1
+        for _ in range(n):
+            if obstacleGrid[0][_]:
+                break
+            memo[0][_] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if not obstacleGrid[i][j]:
+                    memo[i][j] += memo[i - 1][j] + memo[i][j - 1]
+        return memo[-1][-1]
 
 print(Solution().uniquePathsWithObstacles(obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]))
