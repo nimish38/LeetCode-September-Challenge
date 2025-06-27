869ef2c5b98b04d74139a9c06c9f251a8f8fc595
nimish38 <nimish.v.shinde60@gmail.com>
2025-06-27 21:01:52 +0530

    bottom up dp space optimized

--- 2025/Jun_25/House Robber.py
+++ 2025/Jun_25/House Robber.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def rob(self, nums):
-        memo = [-1] * len(nums)
-        def solve(i):
-            if i >= len(nums):
-                return 0
-            if memo[i] == -1:
-                memo[i] = max(solve(i + 1), nums[i] + solve(i + 2))
-            return memo[i]
-        return solve(0)
+class Solution:
+    def rob(self, nums) -> int:
+        rob1, rob2 = 0, 0
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
 
 print(Solution().rob(nums = [2,7,9,3,1]))
