e811bf5766afdb630466df142da28d496acfc865
nimish38 <nimish.v.shinde60@gmail.com>
2025-06-27 21:50:26 +0530

    greedy approach

---
+++ 2025/Jun_25/Valid Parenthesis String.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def checkValidString(self, s):
+        st, opencnt, starcnt = [], 0, 0
+        for c in s:
+            if c == ')':
+                if not st:
+                    return False
+                if st[-1] == '(':
+                    st.pop()
+                else:
+                    if len(st) >= 2 and st[-2] == '(':
+                        val = st.pop()
+                        st.pop()
+                        st.append(val)
+                    else:
+                        val = st.pop() - 1
+                        if val > 0:
+                            st.append(val)
+            elif c == '*':
+                if not st or st[-1] == '(':
+                    st.append(1)
+                else:
+                    st.append(st.pop() + 1)
+            else:
+                st.append(c)
+
+        for _ in st:
+            if _ == '(':
+                opencnt += 1
+            else:
+                starcnt += _
+
+        if opencnt == 0 or opencnt == starcnt:
+            return True
+        return False
+
+print(Solution().checkValidString(s = "((()(()()()*()(((((*)()*(**(())))))(())()())(((())())())))))))(((((())*)))()))(()((*()*(*)))(*)())"))
