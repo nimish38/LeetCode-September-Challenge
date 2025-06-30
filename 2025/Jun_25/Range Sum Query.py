cbf55f3f5843ea3991324b2ad25388bcfd6e1f13
nimish38 <nimish.v.shinde60@gmail.com>
2025-07-01 01:03:32 +0530

    its m X n matrix

--- 2025/Jun_25/Range Sum Query.py
+++ 2025/Jun_25/Range Sum Query.py
@@ -1,13 +1,13 @@
 class NumMatrix(object):
 
     def __init__(self, matrix):
-        n, self.mat = len(matrix), matrix
-        for i in range(n):
+        m, n, self.mat = len(matrix), len(matrix[0]), matrix
+        for i in range(m):
             for j in range(1, n):
                 self.mat[i][j] += self.mat[i][j - 1]
 
         for j in range(n):
-            for i in range(1, n):
+            for i in range(1, m):
                 self.mat[i][j] += self.mat[i - 1][j]
 
     def sumRegion(self, row1, col1, row2, col2):
