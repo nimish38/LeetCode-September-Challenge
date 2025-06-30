ebae0980544f0af2a228c2c2ccd46afeb89c6b20
nimish38 <nimish.v.shinde60@gmail.com>
2025-07-01 01:00:44 +0530

    test and debug

--- 2025/Jun_25/Range Sum Query.py
+++ 2025/Jun_25/Range Sum Query.py
@@ -13,10 +13,15 @@ class NumMatrix(object):
     def sumRegion(self, row1, col1, row2, col2):
         upperRight, upperLeft, bottomLeft = 0, 0, 0
         if row1 > 0:
-            upperRight = self.mat[row1][col2]
+            upperRight = self.mat[row1 - 1][col2]
         if col1 > 0:
             if row1 > 0:
                 upperLeft = self.mat[row1 - 1][col1 - 1]
             bottomLeft = self.mat[row2][col1 - 1]
         return (self.mat[row2][col2] - upperRight) - (bottomLeft - upperLeft)
 
+matrix = [[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]
+num = NumMatrix(matrix)
+print(num.sumRegion(2, 1, 4, 3))
+print(num.sumRegion(1, 1, 2, 2))
+print(num.sumRegion(1, 2, 2, 4))
