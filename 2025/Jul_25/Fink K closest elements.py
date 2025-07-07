04af43f88f45bd834ae3ff68b8258b1088780692
nimish38 <nimish.v.shinde60@gmail.com>
2025-07-07 23:19:32 +0530

    using priority queue solution

--- 2025/Jul_25/Fink K closest elements.py
+++ 2025/Jul_25/Fink K closest elements.py
@@ -1,19 +1,15 @@
-from collections import defaultdict
-
+import heapq
 
 class Solution(object):
     def findClosestElements(self, arr, k, x):
-        cnt, res = defaultdict(list), []
+        heap, res = [], []
         for num in arr:
-            cnt[abs(num - x)].append(num)
-        for key in sorted(list(cnt.keys())):
-            if len(cnt[key]) > k:
-                res.extend(sorted(cnt[key])[: k])
-                break
+            if len(heap) == k:
+                heapq.heappushpop(heap, (-abs(num - x), -num))
             else:
-                res.extend(cnt[key])
-                k -= len(cnt[key])
-        res.sort()
-        return res
+                heapq.heappush(heap, (-abs(num - x), -num))
+        for diff, val in heap:
+            res.append(-val)
+        return sorted(res)
 
 print(Solution().findClosestElements(arr = [1,2,3,4,5], k = 4, x = 3))
